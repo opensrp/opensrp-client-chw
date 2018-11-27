34399a1f4ebfe22fcdb48b6871d8281eb94858c3
Ephraim Muhia <emuhia@ona.io>
2018-11-27 11:37:27 +0300

    Organized imports
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/FamilyDetailFragment.java
+++ opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/FamilyDetailFragment.java
@@ -1,8 +1,8 @@
 package org.smartgresiter.wcaro;
 
 import android.app.Activity;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
