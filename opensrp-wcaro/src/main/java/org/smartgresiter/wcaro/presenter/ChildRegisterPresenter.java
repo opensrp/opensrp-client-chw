ff71488ac36f0f66a0e7fb249150e44166a945ea
mpower <mahmud6390>
2018-12-09 16:06:41 +0600

    Implement Child Register page #17

---
+++ opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/presenter/ChildRegisterPresenter.java
@@ -0,0 +1,158 @@
+package org.smartgresiter.wcaro.presenter;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.util.Pair;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Triple;
+import org.json.JSONObject;
+import org.smartgresiter.wcaro.R;
+import org.smartgresiter.wcaro.contract.ChildRegisterContract;
+import org.smartgresiter.wcaro.interactor.ChildRegisterInteractor;
+import org.smartregister.clientandeventmodel.Client;
+import org.smartregister.clientandeventmodel.Event;
+import org.smartregister.domain.FetchStatus;
+import org.smartregister.repository.AllSharedPreferences;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+
+public class ChildRegisterPresenter implements ChildRegisterContract.Presenter, ChildRegisterContract.InteractorCallBack {
+    public static final String TAG = ChildRegisterPresenter.class.getName();
+
+    private WeakReference<ChildRegisterContract.View> viewReference;
+    private ChildRegisterContract.Interactor interactor;
+    private ChildRegisterContract.Model model;
+
+    public ChildRegisterPresenter(ChildRegisterContract.View view, ChildRegisterContract.Model model) {
+        viewReference = new WeakReference<>(view);
+        interactor = new ChildRegisterInteractor();
+        this.model = model;
+    }
+
+    public void setModel(ChildRegisterContract.Model model) {
+        this.model = model;
+    }
+
+    public void setInteractor(ChildRegisterContract.Interactor interactor) {
+        this.interactor = interactor;
+    }
+
+    @Override
+    public void registerViewConfigurations(List<String> viewIdentifiers) {
+        model.registerViewConfigurations(viewIdentifiers);
+    }
+
+    @Override
+    public void unregisterViewConfiguration(List<String> viewIdentifiers) {
+        model.unregisterViewConfiguration(viewIdentifiers);
+    }
+
+    @Override
+    public void saveLanguage(String language) {
+        model.saveLanguage(language);
+        getView().displayToast(language + " selected");
+    }
+
+    @Override
+    public void startForm(String formName, String entityId, String metadata, String currentLocationId) throws Exception {
+
+        if (StringUtils.isBlank(entityId)) {
+            Triple<String, String, String> triple = Triple.of(formName, metadata, currentLocationId);
+            interactor.getNextUniqueId(triple, this);
+            return;
+        }
+
+        JSONObject form = model.getFormAsJson(formName, entityId, currentLocationId);
+        getView().startFormActivity(form);
+
+    }
+
+    @Override
+    public void closeFamilyRecord(String jsonString) {
+
+        try {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getView().getContext());
+            AllSharedPreferences allSharedPreferences = new AllSharedPreferences(preferences);
+
+            Log.d("JSONResult", jsonString);
+            //getView().showProgressDialog(jsonString.contains(Constants.EventType.CLOSE) ? R.string.removing_dialog_title : R.string.saving_dialog_title);
+
+            interactor.removeChildFromRegister(jsonString, allSharedPreferences.fetchRegisteredANM());
+
+        } catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+
+        }
+    }
+
+    @Override
+    public void saveForm(String jsonString, boolean isEditMode) {
+
+        try {
+
+            getView().showProgressDialog(R.string.saving_dialog_title);
+
+            Pair<Client, Event> pair = model.processRegistration(jsonString);
+            if (pair == null) {
+                return;
+            }
+
+            interactor.saveRegistration(pair, jsonString, isEditMode, this);
+
+        } catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
+    }
+
+    @Override
+    public void onNoUniqueId() {
+        getView().displayShortToast(R.string.no_unique_id);
+    }
+
+    @Override
+    public void onUniqueIdFetched(Triple<String, String, String> triple, String entityId) {
+        try {
+            startForm(triple.getLeft(), entityId, triple.getMiddle(), triple.getRight());
+        } catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+            getView().displayToast(R.string.error_unable_to_start_form);
+        }
+    }
+
+    @Override
+    public void onRegistrationSaved(boolean isEdit) {
+        getView().refreshList(FetchStatus.fetched);
+        getView().hideProgressDialog();
+    }
+
+    @Override
+    public void onDestroy(boolean isChangingConfiguration) {
+
+        viewReference = null;//set to null on destroy
+        // Inform interactor
+        interactor.onDestroy(isChangingConfiguration);
+        // Activity destroyed set interactor to null
+        if (!isChangingConfiguration) {
+            interactor = null;
+            model = null;
+        }
+    }
+
+    @Override
+    public void updateInitials() {
+        String initials = model.getInitials();
+        if (initials != null) {
+            getView().updateInitialsText(initials);
+        }
+    }
+
+    private ChildRegisterContract.View getView() {
+        if (viewReference != null)
+            return viewReference.get();
+        else
+            return null;
+    }
+}
---
+++ opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/provider/ChildRegisterProvider.java
@@ -0,0 +1,245 @@
+package org.smartgresiter.wcaro.provider;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
+import org.smartregister.commonregistry.CommonPersonObject;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.commonregistry.CommonRepository;
+import org.smartregister.cursoradapter.RecyclerViewProvider;
+import org.smartregister.family.R;
+import org.smartregister.family.fragment.BaseFamilyRegisterFragment;
+import org.smartregister.family.provider.FamilyRegisterProvider;
+import org.smartregister.family.util.DBConstants;
+import org.smartregister.family.util.Utils;
+import org.smartregister.view.contract.SmartRegisterClient;
+import org.smartregister.view.contract.SmartRegisterClients;
+import org.smartregister.view.dialog.FilterOption;
+import org.smartregister.view.dialog.ServiceModeOption;
+import org.smartregister.view.dialog.SortOption;
+import org.smartregister.view.viewholder.OnClickFormLauncher;
+
+import java.text.MessageFormat;
+import java.util.Set;
+
+/**
+ * Created by keyman on 13/11/2018.
+ */
+
+public class ChildRegisterProvider implements RecyclerViewProvider<ChildRegisterProvider.RegisterViewHolder> {
+
+    private final LayoutInflater inflater;
+    private Set<org.smartregister.configurableviews.model.View> visibleColumns;
+
+    private View.OnClickListener onClickListener;
+    private View.OnClickListener paginationClickListener;
+
+    private Context context;
+    private CommonRepository commonRepository;
+
+    public ChildRegisterProvider(Context context, CommonRepository commonRepository, Set visibleColumns, View.OnClickListener onClickListener, View.OnClickListener paginationClickListener) {
+
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.visibleColumns = visibleColumns;
+
+        this.onClickListener = onClickListener;
+        this.paginationClickListener = paginationClickListener;
+
+        this.context = context;
+        this.commonRepository = commonRepository;
+    }
+
+    @Override
+    public void getView(Cursor cursor, SmartRegisterClient client, RegisterViewHolder viewHolder) {
+        CommonPersonObjectClient pc = (CommonPersonObjectClient) client;
+        if (visibleColumns.isEmpty()) {
+            populatePatientColumn(pc, client, viewHolder);
+            populateIdentifierColumn(pc, viewHolder);
+            populateLastColumn(pc, viewHolder);
+
+            return;
+        }
+    }
+
+    @Override
+    public void getFooterView(RecyclerView.ViewHolder viewHolder, int currentPageCount, int totalPageCount, boolean hasNext, boolean hasPrevious) {
+        FooterViewHolder footerViewHolder = (FooterViewHolder) viewHolder;
+        footerViewHolder.pageInfoView.setText(
+                MessageFormat.format(context.getString(org.smartregister.R.string.str_page_info), currentPageCount,
+                        totalPageCount));
+
+        footerViewHolder.nextPageView.setVisibility(hasNext ? View.VISIBLE : View.INVISIBLE);
+        footerViewHolder.previousPageView.setVisibility(hasPrevious ? View.VISIBLE : View.INVISIBLE);
+
+        footerViewHolder.nextPageView.setOnClickListener(paginationClickListener);
+        footerViewHolder.previousPageView.setOnClickListener(paginationClickListener);
+    }
+
+    private void populatePatientColumn(CommonPersonObjectClient pc, SmartRegisterClient client, RegisterViewHolder viewHolder) {
+
+        String firstName = Utils.getValue(pc.getColumnmaps(), DBConstants.KEY.FIRST_NAME, true);
+        String lastName = Utils.getValue(pc.getColumnmaps(), DBConstants.KEY.LAST_NAME, true);
+        String patientName = org.smartregister.util.Utils.getName(firstName, lastName);
+
+        fillValue(viewHolder.patientName, WordUtils.capitalize(patientName));
+
+        String dobString = Utils.getDuration(Utils.getValue(pc.getColumnmaps(), DBConstants.KEY.DOB, false));
+        dobString = dobString.contains("y") ? dobString.substring(0, dobString.indexOf("y")) : dobString;
+        fillValue((viewHolder.age), String.format(context.getString(R.string.age_text), dobString));
+
+        View patient = viewHolder.patientColumn;
+        attachPatientOnclickListener(patient, client);
+
+
+        View dueButton = viewHolder.dueButton;
+        attachDosageOnclickListener(dueButton, client);
+
+    }
+
+
+    private void populateIdentifierColumn(CommonPersonObjectClient pc, RegisterViewHolder viewHolder) {
+        String uniqueId = Utils.getValue(pc.getColumnmaps(), DBConstants.KEY.UNIQUE_ID, false);
+        fillValue(viewHolder.ancId, String.format(context.getString(R.string.unique_id_text), uniqueId));
+    }
+
+
+    private void populateLastColumn(CommonPersonObjectClient pc, RegisterViewHolder viewHolder) {
+
+        if (commonRepository != null) {
+            CommonPersonObject commonPersonObject = commonRepository.findByBaseEntityId(pc.entityId());
+            if (commonPersonObject != null) {
+                viewHolder.dueButton.setVisibility(View.VISIBLE);
+                viewHolder.dueButton.setText("Due\n02/05/2019");
+
+                String ga = Utils.getValue(pc.getColumnmaps(), DBConstants.KEY.CONTACT_STATUS, false);
+
+                if (StringUtils.isNotBlank(ga)) {
+
+                    viewHolder.dueButton.setBackgroundColor(context.getResources().getColor(R.color.progress_orange));
+                    viewHolder.dueButton.setTextColor(context.getResources().getColor(R.color.white));
+                }
+
+                //updateDoseButton();
+            } else {
+                viewHolder.dueButton.setVisibility(View.GONE);
+                //attachSyncOnclickListener(viewHolder.sync, pc);
+            }
+        }
+    }
+
+    private void attachPatientOnclickListener(View view, SmartRegisterClient client) {
+        view.setOnClickListener(onClickListener);
+        view.setTag(client);
+        view.setTag(R.id.VIEW_ID, BaseFamilyRegisterFragment.CLICK_VIEW_NORMAL);
+    }
+
+    private void attachDosageOnclickListener(View view, SmartRegisterClient client) {
+        view.setOnClickListener(onClickListener);
+        view.setTag(client);
+        view.setTag(R.id.VIEW_ID, BaseFamilyRegisterFragment.CLICK_VIEW_DOSAGE_STATUS);
+    }
+
+    @Override
+    public SmartRegisterClients updateClients(FilterOption villageFilter, ServiceModeOption serviceModeOption, FilterOption searchFilter, SortOption sortOption) {
+        return null;
+    }
+
+    @Override
+    public void onServiceModeSelected(ServiceModeOption serviceModeOption) {//Implement Abstract Method
+    }
+
+    @Override
+    public OnClickFormLauncher newFormLauncher(String formName, String entityId, String metaData) {
+        return null;
+    }
+
+    @Override
+    public LayoutInflater inflater() {
+        return inflater;
+    }
+
+    @Override
+    public RegisterViewHolder createViewHolder(ViewGroup parent) {
+        View view = inflater.inflate(R.layout.family_register_list_row, parent, false);
+
+        /*
+        ConfigurableViewsHelper helper = ConfigurableViewsLibrary.getInstance().getConfigurableViewsHelper();
+        if (helper.isJsonViewsEnabled()) {
+
+            ViewConfiguration viewConfiguration = helper.getViewConfiguration(Constants.CONFIGURATION.HOME_REGISTER_ROW);
+            ViewConfiguration commonConfiguration = helper.getViewConfiguration(COMMON_REGISTER_ROW);
+
+            if (viewConfiguration != null) {
+                return helper.inflateDynamicView(viewConfiguration, commonConfiguration, view, R.id.register_columns, false);
+            }
+        }*/
+
+        return new RegisterViewHolder(view);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder createFooterHolder(ViewGroup parent) {
+        View view = inflater.inflate(R.layout.smart_register_pagination, parent, false);
+        return new FooterViewHolder(view);
+    }
+
+    @Override
+    public boolean isFooterViewHolder(RecyclerView.ViewHolder viewHolder) {
+        return FooterViewHolder.class.isInstance(viewHolder);
+    }
+
+
+    public static void fillValue(TextView v, String value) {
+        if (v != null)
+            v.setText(value);
+
+    }
+
+    ////////////////////////////////////////////////////////////////
+    // Inner classes
+    ////////////////////////////////////////////////////////////////
+
+    public class RegisterViewHolder extends RecyclerView.ViewHolder {
+        public TextView patientName;
+        public TextView age;
+        public TextView ga;
+        public TextView ancId;
+        public Button dueButton;
+        public View patientColumn;
+
+        public RegisterViewHolder(View itemView) {
+            super(itemView);
+
+            patientName = itemView.findViewById(R.id.patient_name);
+            age = itemView.findViewById(R.id.age);
+            ga = itemView.findViewById(R.id.ga);
+            ancId = itemView.findViewById(R.id.anc_id);
+            dueButton = itemView.findViewById(R.id.due_button);
+
+            patientColumn = itemView.findViewById(R.id.patient_column);
+        }
+    }
+
+    public class FooterViewHolder extends RecyclerView.ViewHolder {
+        public TextView pageInfoView;
+        public Button nextPageView;
+        public Button previousPageView;
+
+        public FooterViewHolder(View view) {
+            super(view);
+
+            nextPageView = view.findViewById(org.smartregister.R.id.btn_next_page);
+            previousPageView = view.findViewById(org.smartregister.R.id.btn_previous_page);
+            pageInfoView = view.findViewById(org.smartregister.R.id.txt_page_info);
+        }
+    }
+
+}
