adcfd15000b4d54fc88413cbeebc3e252ee57db3
Ephraim Muhia <emuhia@ona.io>
2018-12-13 19:07:50 +0300

    updated model
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/presenter/FamilyProfileMemberPresenter.java
+++ opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/presenter/FamilyProfileMemberPresenter.java
@@ -1,12 +1,11 @@
 package org.smartgresiter.wcaro.presenter;
 
 import org.smartregister.family.contract.FamilyProfileMemberContract;
-import org.smartregister.family.contract.FamilyRegisterFragmentContract;
 import org.smartregister.family.presenter.BaseFamilyProfileMemberPresenter;
 
 public class FamilyProfileMemberPresenter extends BaseFamilyProfileMemberPresenter {
 
-    public FamilyProfileMemberPresenter(FamilyProfileMemberContract.View view, FamilyRegisterFragmentContract.Model model, String viewConfigurationIdentifier, String familyBaseEntityId) {
+    public FamilyProfileMemberPresenter(FamilyProfileMemberContract.View view, FamilyProfileMemberContract.Model model, String viewConfigurationIdentifier, String familyBaseEntityId) {
         super(view, model, viewConfigurationIdentifier, familyBaseEntityId);
     }
 }
