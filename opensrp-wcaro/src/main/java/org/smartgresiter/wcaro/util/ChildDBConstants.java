9970de2a3df20a95122478559b2b0adff69e9841
mpower <mahmud6390>
2018-12-24 18:00:09 +0600

    cg:primary caregevier added

--- opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/util/ChildDBConstants.java
+++ opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/util/ChildDBConstants.java
@@ -2,8 +2,6 @@ package org.smartgresiter.wcaro.util;
 
 public class ChildDBConstants {
     public static final class KEY {
-        public static final String TABLE_NAME = Constants.TABLE_NAME.CHILD;
-        public static final String PARENT_TABLE_NAME=Constants.TABLE_NAME.FAMILY;
         public static final String FAMILY_FIRST_NAME = "family_first_name";
         public static final String FAMILY_LAST_NAME = "family_last_name";
         public static final String FAMILY_HOME_ADDRESS = "family_home_address";
