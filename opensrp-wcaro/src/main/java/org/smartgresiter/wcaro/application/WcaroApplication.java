c9b0ba1ad45f3f7020218993c706dca3b2dd0b92
Ephraim Muhia <emuhia@ona.io>
2019-01-11 10:43:11 +0300

    Fixed failing tests
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/application/WcaroApplication.java
+++ opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/application/WcaroApplication.java
@@ -114,7 +114,7 @@ public class WcaroApplication extends DrishtiApplication {
         JobManager.create(this).addJobCreator(new WcaroJobCreator());
 
         // TODO FIXME remove when login is implemented
-        sampleUniqueIds();
+        // sampleUniqueIds();
 
         initOfflineSchedules();
     }
