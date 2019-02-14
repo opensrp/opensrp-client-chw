4914502450e7fbcca12e91951488bb8f8b4ddeb5
rkodev <43806892+rkodev@users.noreply.github.com>
2019-02-14 11:45:14 +0300

    Issue #27

--- opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/activity/CustomJsonFromActivity.java
+++ opensrp-wcaro/src/main/java/org/smartgresiter/wcaro/activity/CustomJsonFromActivity.java
@@ -1,6 +1,7 @@
 package org.smartgresiter.wcaro.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import org.smartgresiter.wcaro.R;
 import org.smartregister.family.activity.FamilyWizardFormActivity;
@@ -9,9 +10,20 @@ import org.smartregister.family.activity.FamilyWizardFormActivity;
  * Override to change custom close message and custom view design
  */
 public class CustomJsonFromActivity extends FamilyWizardFormActivity {
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setConfirmCloseMessage(getString(R.string.any_changes_you_make));
+        setContentView(R.layout.custom_native_form_activity_json_form);
+        mToolbar = findViewById(com.vijay.jsonwizard.R.id.tb_top);
+        setSupportActionBar(mToolbar);
     }
+
+    @Override
+    public Toolbar getToolbar() {
+        return mToolbar;
+    }
+
 }
