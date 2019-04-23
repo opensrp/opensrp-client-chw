c3bf6d7949b93e172d4f18ce62cb4b1e0da5e583
rkodev <43806892+rkodev@users.noreply.github.com>
2019-04-23 16:05:18 +0300

    Added Empty State graphic

---
+++ opensrp-chw/src/test/java/org/smartregister/chw/fragment/FamilyProfileDueFragmentTest.java
@@ -0,0 +1,63 @@
+package org.smartregister.chw.fragment;
+
+import android.view.View;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.reflect.Whitebox;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.smartregister.chw.BuildConfig;
+import org.smartregister.chw.application.ChwApplication;
+import org.smartregister.family.adapter.FamilyRecyclerViewCustomAdapter;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(application = ChwApplication.class, constants = BuildConfig.class, sdk = 22)
+public class FamilyProfileDueFragmentTest {
+
+    @Mock
+    FamilyRecyclerViewCustomAdapter clientAdapter;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testonEmptyRegisterCountIsCalled() {
+        FamilyProfileDueFragment spyFragment = Mockito.spy(FamilyProfileDueFragment.class);
+
+        View emptyView = Mockito.mock(View.class);
+        Whitebox.setInternalState(spyFragment, "emptyView", emptyView);
+        Whitebox.setInternalState(spyFragment, "clientAdapter", clientAdapter);
+        spyFragment.countExecute();
+
+        Mockito.verify(emptyView).setVisibility(Mockito.anyInt());
+    }
+
+    @Test
+    public void testonEmptyRegisterCountHidesViewHideView() {
+        FamilyProfileDueFragment spyFragment = Mockito.spy(FamilyProfileDueFragment.class);
+
+        View emptyView = Mockito.mock(View.class);
+        Whitebox.setInternalState(spyFragment, "emptyView", emptyView);
+        spyFragment.onEmptyRegisterCount(false);
+
+        Mockito.verify(emptyView).setVisibility(View.GONE);
+    }
+
+    @Test
+    public void testonEmptyRegisterCountHidesViewShowsView() {
+        FamilyProfileDueFragment spyFragment = Mockito.spy(FamilyProfileDueFragment.class);
+
+        View emptyView = Mockito.mock(View.class);
+        Whitebox.setInternalState(spyFragment, "emptyView", emptyView);
+        spyFragment.onEmptyRegisterCount(true);
+
+        Mockito.verify(emptyView).setVisibility(View.VISIBLE);
+    }
+}
