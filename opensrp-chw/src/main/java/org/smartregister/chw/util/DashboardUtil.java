f37ed82b0d3915000f89e65f17861ea342a864d4
Allan <a.onchuru@gmail.com>
2019-05-15 16:49:25 +0300

    Add Children 0-59 months with and without birth certificates Indicator definitions

--- opensrp-chw/src/main/java/org/smartregister/chw/util/DashboardUtil.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/util/DashboardUtil.java
@@ -11,4 +11,6 @@ public class DashboardUtil {
     public static String countOfChildrenUnder5 = "CHW_001";
     public static String deceasedChildren0_11Months = "CHW_002";
     public static String deceasedChildren12_59Months = "CHW_003";
+    public static String countOfChildren0_59WithBirthCert = "CHW_004";
+    public static String countOfChildren0_59WithNoBirthCert = "CHW_005";
 }
