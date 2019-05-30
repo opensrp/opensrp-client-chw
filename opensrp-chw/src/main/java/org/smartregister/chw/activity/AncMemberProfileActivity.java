d02ad19258e139c15c544c27af7b976f6e6b2292
Manu Tarus <manutarus@gmail.com>
2019-05-30 16:30:31 +0300

    add anc member profile activity

---
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/AncMemberProfileActivity.java
@@ -0,0 +1,15 @@
+package org.smartregister.chw.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import org.smartregister.chw.anc.activity.BaseAncMemberProfileActivity;
+import org.smartregister.chw.anc.util.MemberObject;
+
+public class AncMemberProfileActivity extends BaseAncMemberProfileActivity {
+    public static void startMe(Activity activity, MemberObject memberObject) {
+        Intent intent = new Intent(activity, AncMemberProfileActivity.class);
+        intent.putExtra("MemberObject", memberObject);
+        activity.startActivity(intent);
+    }
+}
