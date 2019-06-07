ab9cb978c7ffac2c59bea7fc34f5a395f4c4e2c1
Allan <a.onchuru@gmail.com>
2019-06-07 17:32:23 +0300

    Implement manual update/refresh indicators button in Dashboard
    Schedule indicator generating job to execute immediately and refresh display fragment

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/JobAidsActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/JobAidsActivity.java
@@ -8,16 +8,21 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import org.smartregister.chw.BuildConfig;
 import org.smartregister.chw.R;
 import org.smartregister.chw.fragment.JobAidsDashboardFragment;
 import org.smartregister.chw.fragment.JobAidsGuideBooksFragment;
+import org.smartregister.chw.job.ChwIndicatorGeneratingJob;
 import org.smartregister.chw.listener.JobsAidsBottomNavigationListener;
 import org.smartregister.helper.BottomNavigationHelper;
 
 public class JobAidsActivity extends FamilyRegisterActivity {
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,32 +30,6 @@ public class JobAidsActivity extends FamilyRegisterActivity {
         setContentView(R.layout.activity_job_aids);
         setUpView();
         registerBottomNavigation();
-    }
-
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return JobAidsDashboardFragment.newInstance();
-                case 1:
-                    return JobAidsGuideBooksFragment.newInstance();
-                default:
-                    return JobAidsDashboardFragment.newInstance();
-            }
-
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 2;
-        }
     }
 
     private void setUpView() {
@@ -63,13 +42,30 @@ public class JobAidsActivity extends FamilyRegisterActivity {
         // primary sections of the activity.
         SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.job_aids_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_refresh_indicators:
+                refreshIndicatorData();
+                return true;
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
     }
 
     @Override
@@ -99,6 +95,53 @@ public class JobAidsActivity extends FamilyRegisterActivity {
 
         if (!BuildConfig.SUPPORT_QR) {
             bottomNavigationView.getMenu().removeItem(org.smartregister.family.R.id.action_scan_qr);
+        }
+    }
+
+    /**
+     * Refresh the indicator data by clearing the view and scheduling the IndicatorGeneratingJob immediately
+     * then triggering the reloading of the Fragment
+     */
+    public void refreshIndicatorData() {
+        ChwIndicatorGeneratingJob.scheduleJobImmediately(ChwIndicatorGeneratingJob.TAG);
+        if (mViewPager != null) {
+            mViewPager.getAdapter().notifyDataSetChanged();
+        }
+        Log.d(TAG, "Refreshing indicators...");
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return JobAidsDashboardFragment.newInstance();
+                case 1:
+                    return JobAidsGuideBooksFragment.newInstance();
+                default:
+                    return JobAidsDashboardFragment.newInstance();
+            }
+
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            if (object instanceof JobAidsDashboardFragment) {
+                return POSITION_NONE;
+            } else {
+                return POSITION_UNCHANGED;
+            }
         }
     }
 }
