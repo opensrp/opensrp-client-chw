c2ba783bbc3b6245f60a171a62d9bd2e763757e2
mahmud6390 <mahmud6390@gmail.com>
2019-06-13 20:12:02 +0600

    test case update

--- opensrp-chw/src/test/java/org/smartregister/chw/presenter/ChildRegisterFragmentPresenterTest.java
+++ opensrp-chw/src/test/java/org/smartregister/chw/presenter/ChildRegisterFragmentPresenterTest.java
@@ -45,7 +45,7 @@ public class ChildRegisterFragmentPresenterTest extends BaseUnitTest {
 
     @Test
     public void testDueAndFilterCondition() {
-        Assert.assertEquals(" date_removed is null AND  (( strftime('%Y','now') - strftime('%Y',dob))<5) AND (( IFNULL(STRFTIME('%Y%m%d%H%M%S', datetime((last_home_visit)/1000,'unixepoch')),0) < STRFTIME('%Y%m%d%H%M%S', datetime('now','start of month')) AND IFNULL(STRFTIME('%Y%m%d%H%M%S', datetime((visit_not_done)/1000,'unixepoch')),0) < STRFTIME('%Y%m%d%H%M%S', datetime('now','start of month'))  ))", presenter.getDueFilterCondition());
+        Assert.assertEquals(" date_removed is null AND  ((( julianday('now') - julianday(dob))/365.25) <5) AND (( IFNULL(STRFTIME('%Y%m%d%H%M%S', datetime((last_home_visit)/1000,'unixepoch')),0) < STRFTIME('%Y%m%d%H%M%S', datetime('now','start of month')) AND IFNULL(STRFTIME('%Y%m%d%H%M%S', datetime((visit_not_done)/1000,'unixepoch')),0) < STRFTIME('%Y%m%d%H%M%S', datetime('now','start of month'))  ))", presenter.getDueFilterCondition());
 
     }
 }
