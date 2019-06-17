ef0f8ae947d1916f602744f5e25225c1ef7552dd
JafariUCC <jafarimwidadi@gmail.com>
2019-06-17 12:52:36 +0300

    removing view initialization codes

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/MalariaRegisterFragment.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/MalariaRegisterFragment.java
@@ -1,61 +1,16 @@
 package org.smartregister.chw.fragment;
 
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.LinearLayout;
-import org.smartregister.chw.R;
-import org.smartregister.chw.custom_view.NavigationMenu;
 import org.smartregister.chw.malaria.fragment.BaseMalariaRegisterFragment;
 import org.smartregister.chw.model.MalariaRegisterFragmentModel;
 import org.smartregister.chw.presenter.MalariaRegisterFragmentPresenter;
-import org.smartregister.chw.util.Utils;
 import org.smartregister.view.activity.BaseRegisterActivity;
-import org.smartregister.view.customcontrols.CustomFontTextView;
 
 public class MalariaRegisterFragment extends BaseMalariaRegisterFragment {
 
     @Override
     public void setupViews(View view) {
         super.setupViews(view);
-
-        Toolbar toolbar = view.findViewById(org.smartregister.R.id.register_toolbar);
-        toolbar.setContentInsetsAbsolute(0, 0);
-        toolbar.setContentInsetsRelative(0, 0);
-        toolbar.setContentInsetStartWithNavigation(0);
-
-        NavigationMenu.getInstance(getActivity(), null, toolbar);
-
-        View navbarContainer = view.findViewById(R.id.register_nav_bar_container);
-        navbarContainer.setFocusable(false);
-
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        View searchBarLayout = view.findViewById(R.id.search_bar_layout);
-        searchBarLayout.setLayoutParams(params);
-        searchBarLayout.setBackgroundResource(R.color.chw_primary);
-        searchBarLayout.setPadding(searchBarLayout.getPaddingLeft(), searchBarLayout.getPaddingTop(), searchBarLayout.getPaddingRight(), (int) Utils.convertDpToPixel(10, getActivity()));
-
-        CustomFontTextView titleView = view.findViewById(R.id.txt_title_label);
-        if (titleView != null) {
-            titleView.setPadding(0, titleView.getTop(), titleView.getPaddingRight(), titleView.getPaddingBottom());
-        }
-
-        View topLeftLayout = view.findViewById(R.id.top_left_layout);
-        topLeftLayout.setVisibility(View.GONE);
-
-        View topRightLayout = view.findViewById(R.id.top_right_layout);
-        topRightLayout.setVisibility(View.VISIBLE);
-
-        View sortFilterBarLayout = view.findViewById(R.id.register_sort_filter_bar_layout);
-        sortFilterBarLayout.setVisibility(View.GONE);
-
-        View filterSortLayout = view.findViewById(R.id.filter_sort_layout);
-        filterSortLayout.setVisibility(View.GONE);
-
-        if (getSearchView() != null) {
-            getSearchView().setBackgroundResource(org.smartregister.family.R.color.white);
-            getSearchView().setCompoundDrawablesWithIntrinsicBounds(org.smartregister.family.R.drawable.ic_action_search, 0, 0, 0);
-            getSearchView().setTextColor(getResources().getColor(R.color.text_black));
-        }
     }
 
     @Override
