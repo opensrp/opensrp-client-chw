68122f8e3834f65b52621e79f45770a82210276d
Manu Tarus <manutarus@gmail.com>
2019-06-18 10:20:21 +0300

    codacy fix

--- opensrp-chw/src/main/java/org/smartregister/chw/contract/AncRegisterContract.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/contract/AncRegisterContract.java
@@ -1,14 +1,6 @@
 package org.smartregister.chw.contract;
 
-import android.util.Pair;
-
 import org.apache.commons.lang3.tuple.Triple;
-import org.json.JSONObject;
-import org.smartregister.clientandeventmodel.Client;
-import org.smartregister.clientandeventmodel.Event;
-import org.smartregister.view.contract.BaseRegisterContract;
-
-import java.util.List;
 
 /**
  * Created by keyamn on 12/11/2018.
@@ -21,6 +13,7 @@ public interface AncRegisterContract {
         void getNextUniqueId(Triple<String, String, String> triple, AncRegisterContract.InteractorCallBack callBack, String familyID);
 
     }
+
     interface InteractorCallBack {
 
         void onNoUniqueId();
