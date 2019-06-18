6399f824994a5196313790adbce090afa6be3458
Denis Rwelamila <rwelamila.jr@gmail.com>
2019-06-18 11:14:51 +0300

    trouble bringing up malaria profile activity

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/MalariaProfileActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/MalariaProfileActivity.java
@@ -1,79 +1,18 @@
 package org.smartregister.chw.activity;
 
-import android.annotation.SuppressLint;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-import org.smartregister.chw.R;
-import org.smartregister.chw.anc.util.DBConstants;
+
+import android.app.Activity;
+import android.content.Intent;
+import org.smartregister.chw.malaria.activity.BaseMalariaProfileActivity;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
-import org.smartregister.helper.ImageRenderHelper;
-import org.smartregister.util.Utils;
-import org.smartregister.view.activity.BaseProfileActivity;
 
-public class MalariaProfileActivity extends BaseProfileActivity {
-    private String full_name;
-    private String location;
-    private String gender;
-    private String unique_id;
-    private CommonPersonObjectClient client;
-    private TextView textViewFullName;
-    private TextView textViewLocation;
-    private TextView textViewGender;
-    private TextView textViewUniqueId;
-    private TextView textViewTitle;
+public class MalariaProfileActivity extends BaseMalariaProfileActivity {
+    private static CommonPersonObjectClient client;
 
-    @SuppressLint("DefaultLocale")
-    protected void onCreation() {
-
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            full_name = String.format("%s ,%d", extras.getString("full_name"), extras.getInt("age"));
-            location = extras.getString("location");
-            gender = extras.getString("gender");
-            unique_id = extras.getString("unique_id");
-            client = (CommonPersonObjectClient) getIntent().getSerializableExtra("client");
-
-        }
-
-        textViewFullName = findViewById(R.id.textview_name);
-        textViewLocation = findViewById(R.id.textview_address);
-        textViewGender = findViewById(R.id.textview_gender);
-        textViewUniqueId = findViewById(R.id.textview_id);
-        fillValues(full_name, location, gender, unique_id);
-    }
-
-    @Override
-    protected void initializePresenter() {
+    public static void startMalariaActivity(Activity activity, Intent intent) {
+//        client = (CommonPersonObjectClient)intent.getSerializableExtra("client");
+        activity.startActivity(intent);
 
     }
 
-    @Override
-    protected ViewPager setupViewPager(ViewPager viewPager) {
-        return null;
-    }
-
-    @Override
-    protected void fetchProfileData() {
-
-    }
-
-    private void fillValues(String full_name, String location, String gender, String unique_id) {
-        String baseEntityId = Utils.getValue(client.getColumnmaps(), DBConstants.KEY.BASE_ENTITY_ID, true);
-        textViewFullName.setText(full_name);
-        textViewLocation.setText(location);
-        textViewLocation.setText(location);
-        textViewGender.setText(gender);
-        textViewUniqueId.setText(unique_id);
-
-        Log.e("COMMON PERSON", client.getColumnmaps() + "");
-        Log.e("COMMON PERSON", Utils.getValue(client.getColumnmaps(), DBConstants.KEY.FIRST_NAME, true));
-    }
 }
--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/MalariaRegisterFragment.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/MalariaRegisterFragment.java
@@ -1,18 +1,20 @@
 package org.smartregister.chw.fragment;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
 import android.view.View;
 
 import android.widget.Toast;
 import org.smartregister.chw.activity.MalariaProfileActivity;
+import org.smartregister.chw.malaria.contract.MalariaProfileContract;
 import org.smartregister.chw.malaria.fragment.BaseMalariaRegisterFragment;
 import org.smartregister.chw.model.MalariaRegisterFragmentModel;
 import org.smartregister.chw.presenter.MalariaRegisterFragmentPresenter;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 
+import java.lang.ref.WeakReference;
+
 public class MalariaRegisterFragment extends BaseMalariaRegisterFragment {
+    private WeakReference<MalariaProfileContract.View> view;
 
     @Override
     public void setupViews(View view) {
@@ -29,9 +31,9 @@ public class MalariaRegisterFragment extends BaseMalariaRegisterFragment {
 
     @Override
     protected void onViewClicked(android.view.View view) {
-        if (view.getTag() instanceof CommonPersonObjectClient && view.getId() == org.smartregister.malaria.R.id.due_button) {
+        if (view.getId() == org.smartregister.malaria.R.id.due_button) {
             Toast.makeText(getActivity().getApplicationContext(), "CLICKED", Toast.LENGTH_LONG).show();
-            goToPatientProfile(getActivity(), (CommonPersonObjectClient) view.getTag());
+            goToPatientProfile();
         } else {
             Boolean cpo = view.getTag() instanceof CommonPersonObjectClient;
             Toast.makeText(getActivity().getApplicationContext(), cpo.toString(), Toast.LENGTH_LONG).show();
@@ -40,16 +42,21 @@ public class MalariaRegisterFragment extends BaseMalariaRegisterFragment {
 
     }
 
-    private void goToPatientProfile(Activity activity, CommonPersonObjectClient client) {
-        Intent intent = new Intent(activity, MalariaProfileActivity.class);
+    private void goToPatientProfile() {
+        Intent intent = new Intent(getActivity(), MalariaProfileActivity.class);
         intent.putExtra("full_name", "Denis Rwelamila");
         intent.putExtra("age", 26);
         intent.putExtra("gender", "Male");
         intent.putExtra("location", "Tabata");
         intent.putExtra("unique_id", "ID#1231232");
-        intent.putExtra("client", client);
-        getActivity().startActivity(intent);
+        intent.putExtra("client", "client");
+        MalariaProfileActivity.startMalariaActivity(getActivity(), intent);
+//        malariaPresenter().startProfileActivity(intent);
     }
+
+//    private MalariaProfileContract.Presenter malariaPresenter() {
+//        return malariaPresenter();
+//    }
 
 
 }
