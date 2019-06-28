81a98d983af2b6b709f86923175328924c720983
rkodev <43806892+rkodev@users.noreply.github.com>
2019-06-28 10:58:31 +0300

    Wcaro Medical history

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/AncMedicalHistoryActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/AncMedicalHistoryActivity.java
@@ -1,18 +1,41 @@
 package org.smartregister.chw.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.view.View;
 
 import org.smartregister.chw.anc.activity.BaseAncMedicalHistoryActivity;
 import org.smartregister.chw.anc.domain.MemberObject;
+import org.smartregister.chw.anc.domain.Visit;
+
+import java.util.List;
 
 import static org.smartregister.chw.anc.util.Constants.ANC_MEMBER_OBJECTS.MEMBER_PROFILE_OBJECT;
 
 public class AncMedicalHistoryActivity extends BaseAncMedicalHistoryActivity {
 
+    private Flavor flavor = new AncMedicalHistoryActivityFlv();
+
     public static void startMe(Activity activity, MemberObject memberObject) {
         Intent intent = new Intent(activity, AncMedicalHistoryActivity.class);
         intent.putExtra(MEMBER_PROFILE_OBJECT, memberObject);
         activity.startActivity(intent);
+    }
+
+    @Override
+    public View renderView(List<Visit> visits) {
+        View view = flavor.bindViews(this);
+        displayLoadingState(true);
+        flavor.processViewData(visits, this);
+        displayLoadingState(false);
+        return view;
+    }
+
+    public interface Flavor {
+
+        View bindViews(Activity activity);
+
+        void processViewData(List<Visit> visits, Context context);
     }
 }
