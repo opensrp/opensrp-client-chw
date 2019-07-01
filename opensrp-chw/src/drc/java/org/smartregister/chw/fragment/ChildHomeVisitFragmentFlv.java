6d4a68b349e5f18c1887b798b6f9182d345b43ea
rkodev <43806892+rkodev@users.noreply.github.com>
2019-07-01 09:03:02 +0300

    Added drc changes

--- opensrp-chw/src/drc/java/org/smartregister/chw/fragment/ChildHomeVisitFragmentFlv.java
+++ opensrp-chw/src/drc/java/org/smartregister/chw/fragment/ChildHomeVisitFragmentFlv.java
@@ -1,6 +1,6 @@
 package org.smartregister.chw.fragment;
 
-public class ChildHomeVisitFragmentFlv implements ChildHomeVisitFragment.flavor {
+public class ChildHomeVisitFragmentFlv implements ChildHomeVisitFragment.Flavor {
     @Override
     public boolean onTaskVisibility() {
         return true;
@@ -9,5 +9,15 @@ public class ChildHomeVisitFragmentFlv implements ChildHomeVisitFragment.flavor 
     @Override
     public boolean onObsIllnessVisibility() {
         return false;
+    }
+
+    @Override
+    public boolean onSleepingUnderLLITNVisibility() {
+        return true;
+    }
+
+    @Override
+    public boolean onMUACVisibility() {
+        return true;
     }
 }
