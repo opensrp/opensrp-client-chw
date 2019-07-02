7eb633076901f8dd649a8a00f33058544682ed2b
rkodev <43806892+rkodev@users.noreply.github.com>
2019-07-02 11:22:53 +0300

    Fix French spelling

--- opensrp-chw/src/main/java/org/smartregister/chw/custom_view/DefaultNavigationMenuFlv.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/custom_view/DefaultNavigationMenuFlv.java
@@ -3,6 +3,6 @@ package org.smartregister.chw.custom_view;
 public abstract class DefaultNavigationMenuFlv implements NavigationMenu.Flavour {
     @Override
     public String[] getSupportedLanguages() {
-        return new String[]{"English", "Française"};
+        return new String[]{"English", "Français"};
     }
 }
