02f0855ed313a8da34a3189cf1616f4207fabdca
rkodev <43806892+rkodev@users.noreply.github.com>
2019-07-03 12:37:06 +0300

    - Added trasnlation fixes for togo - Added ANC Model differences for all flavors

--- opensrp-chw/src/main/java/org/smartregister/chw/model/DefaultAncRegisterFragmentModelFlv.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/model/DefaultAncRegisterFragmentModelFlv.java
@@ -3,7 +3,7 @@ package org.smartregister.chw.model;
 import java.util.HashSet;
 import java.util.Set;
 
-public class DefaultAncRegisterFragmentModelFlv implements AncRegisterFragmentModel.Flavor {
+public abstract class DefaultAncRegisterFragmentModelFlv implements AncRegisterFragmentModel.Flavor {
     @Override
     public Set<String> mainColumns(String tableName) {
         Set<String> columnList = new HashSet<>();
