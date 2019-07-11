5091ba0dcfa9dbf45cad9e52d446d0916c5ee78b
JafariUCC <jafarimwidadi@gmail.com>
2019-07-11 13:16:03 +0300

    qa register client fixes

--- opensrp-chw/src/main/java/org/smartregister/chw/model/MalariaRegisterFragmentModel.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/model/MalariaRegisterFragmentModel.java
@@ -16,7 +16,7 @@ public class MalariaRegisterFragmentModel extends BaseMalariaRegisterFragmentMod
         SmartRegisterQueryBuilder queryBuilder = new SmartRegisterQueryBuilder();
         queryBuilder.SelectInitiateMainTable(tableName, mainColumns(tableName));
         queryBuilder.customJoin("INNER JOIN " + Constants.TABLE_NAME.FAMILY_MEMBER + " ON  " + tableName + "." + DBConstants.KEY.BASE_ENTITY_ID + " = " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.BASE_ENTITY_ID + " COLLATE NOCASE ");
-        queryBuilder.customJoin("INNER JOIN " + Constants.TABLE_NAME.FAMILY + " ON  " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.RELATIONAL_ID + " = " + Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.BASE_ENTITY_ID + " COLLATE NOCASE ");
+        queryBuilder.customJoin("INNER JOIN " + Constants.TABLE_NAME.FAMILY + " ON  " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.RELATIONAL_ID + " = " + Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.BASE_ENTITY_ID + " COLLATE NOCASE WHERE ec_malaria_confirmation.malaria = 1");
 
         return queryBuilder.mainCondition(mainCondition);
     }
