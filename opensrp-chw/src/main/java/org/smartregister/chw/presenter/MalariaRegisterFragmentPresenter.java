a60a4e39a0bbdecdf2d3f20d4d993cccac6d32bf
Denis Rwelamila <rwelamila.jr@gmail.com>
2019-07-15 09:06:08 +0300

    fix table name not found

--- opensrp-chw/src/main/java/org/smartregister/chw/presenter/MalariaRegisterFragmentPresenter.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/presenter/MalariaRegisterFragmentPresenter.java
@@ -27,6 +27,7 @@ public class MalariaRegisterFragmentPresenter extends BaseMalariaRegisterFragmen
 
     @Override
     public String getMainCondition() {
-        return " " + DBConstants.TABLE_NAME + "." + DBConstants.KEY.DATE_REMOVED + " is null " + " COLLATE NOCASE WHERE ec_malaria_confirmation.malaria = 1";
+        return " " + Constants.TABLE_NAME.MALARIA_CONFIRMATION + "." + DBConstants.KEY.DATE_REMOVED + " is null" +
+                " " + " COLLATE NOCASE WHERE ec_malaria_confirmation.malaria = 1";
     }
 }
