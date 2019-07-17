357f90f4310b892563da5ff1e86bf01e266211e8
Elly Kitoto <ekitoto@ona.io>
2019-07-17 15:19:53 +0300

    Added health facility app module

---
+++ opensrp-chw-hf/src/main/java/com/opensrp/chw/hf/MainActivity.java
@@ -0,0 +1,15 @@
+package com.opensrp.chw.hf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.opensrp.hf.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
