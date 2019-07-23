767e92dd9394dae9ace63ef90b0cfc6b61f89968
Manu Tarus <manutarus@gmail.com>
2019-07-23 23:33:25 +0300

    class refactor

--- opensrp-chw/src/main/java/org/smartregister/chw/util/JsonFormUtils.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/util/JsonFormUtils.java
@@ -1046,8 +1046,6 @@ public class JsonFormUtils extends org.smartregister.family.util.JsonFormUtils {
             Event event = getEditAncLatestProperties(baseEntityID);
             final List<Obs> observations = event.getObs();
             JSONObject form = getFormWithMetaData(baseEntityID, context, formName, eventType);
-            LocationPickerView lpv = new LocationPickerView(context);
-            lpv.init();
             if (form != null) {
                 JSONObject stepOne = form.getJSONObject(org.smartregister.family.util.JsonFormUtils.STEP1);
 
