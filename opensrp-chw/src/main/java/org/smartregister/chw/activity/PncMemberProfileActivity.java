1a57ce9e7938ba2d8cf81211799af7cdad2f1e44
Manu Tarus <manutarus@gmail.com>
2019-07-30 14:52:50 +0300

    add pnc profile menu

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/PncMemberProfileActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/PncMemberProfileActivity.java
@@ -2,12 +2,21 @@ package org.smartregister.chw.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.view.MenuItem;
 
+import org.json.JSONObject;
+import org.smartregister.chw.R;
+import org.smartregister.chw.anc.AncLibrary;
 import org.smartregister.chw.anc.domain.MemberObject;
 import org.smartregister.chw.anc.presenter.BaseAncMemberProfilePresenter;
 import org.smartregister.chw.anc.util.Constants;
-import org.smartregister.chw.interactor.PncMemberProfileInteractor;
 import org.smartregister.chw.pnc.activity.BasePncMemberProfileActivity;
+import org.smartregister.commonregistry.CommonPersonObject;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.commonregistry.CommonRepository;
+import org.smartregister.family.util.JsonFormUtils;
+
+import timber.log.Timber;
 
 public class PncMemberProfileActivity extends BasePncMemberProfileActivity {
 
@@ -17,6 +26,40 @@ public class PncMemberProfileActivity extends BasePncMemberProfileActivity {
         intent.putExtra(Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_NAME, familyHeadName);
         intent.putExtra(Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_PHONE, familyHeadPhoneNumber);
         activity.startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.action_pnc_member_registration:
+                startFormForEdit(R.string.edit_member_form_title,
+                        org.smartregister.chw.util.Constants.JSON_FORM.getFamilyMemberRegister());
+                return true;
+            case R.id.action_pnc_registration:
+                startFormForEdit(R.string.edit_anc_registration_form_title,
+                        org.smartregister.chw.util.Constants.JSON_FORM.getAncRegistration());
+                return true;
+            case R.id.action__pnc_remove_member:
+                CommonRepository commonRepository = org.smartregister.chw.util.Utils.context().commonrepository(org.smartregister.chw.util.Utils.metadata().familyMemberRegister.tableName);
+
+                final CommonPersonObject commonPersonObject = commonRepository.findByBaseEntityId(MEMBER_OBJECT.getBaseEntityId());
+                final CommonPersonObjectClient client =
+                        new CommonPersonObjectClient(commonPersonObject.getCaseId(), commonPersonObject.getDetails(), "");
+                client.setColumnmaps(commonPersonObject.getColumnmaps());
+
+                IndividualProfileRemoveActivity.startIndividualProfileActivity(PncMemberProfileActivity.this, client, MEMBER_OBJECT.getFamilyBaseEntityId(), MEMBER_OBJECT.getFamilyHead(), MEMBER_OBJECT.getPrimaryCareGiver());
+                return true;
+            case R.id.action_pregnancy_out_come:
+                AncRegisterActivity.startAncRegistrationActivity(PncMemberProfileActivity.this, MEMBER_OBJECT.getBaseEntityId(), null,
+                        org.smartregister.chw.util.Constants.JSON_FORM.getPregnancyOutcome(), AncLibrary.getInstance().getUniqueIdRepository().getNextUniqueId().getOpenmrsId(), MEMBER_OBJECT.getFamilyBaseEntityId());
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     //TODO
@@ -39,5 +82,17 @@ public class PncMemberProfileActivity extends BasePncMemberProfileActivity {
     @Override
     public void openUpcomingService() {
         PncUpcomingServicesActivity.startMe(this, MEMBER_OBJECT);
+    }
+
+
+    public void startFormForEdit(Integer title_resource, String formName) {
+
+        try {
+            JSONObject form = org.smartregister.chw.util.JsonFormUtils.getAncPncForm(title_resource, formName, MEMBER_OBJECT, this);
+
+            startActivityForResult(org.smartregister.chw.util.JsonFormUtils.getAncPncStartFormIntent(form, this), JsonFormUtils.REQUEST_CODE_GET_JSON);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
     }
 }
