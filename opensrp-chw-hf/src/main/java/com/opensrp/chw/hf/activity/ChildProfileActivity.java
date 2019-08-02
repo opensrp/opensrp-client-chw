5ce3ab9c3f2dd546137de69220ef98761d138989
Elly Kitoto <ekitoto@ona.io>
2019-08-02 14:55:15 +0300

    Started refactor for FamilyMember fragment

--- opensrp-chw-hf/src/main/java/com/opensrp/chw/hf/activity/ChildProfileActivity.java
+++ opensrp-chw-hf/src/main/java/com/opensrp/chw/hf/activity/ChildProfileActivity.java
@@ -3,6 +3,4 @@ package com.opensrp.chw.hf.activity;
 import com.opensrp.chw.core.activity.CoreChildProfileActivity;
 
 public class ChildProfileActivity extends CoreChildProfileActivity {
-
-
 }
--- opensrp-chw/src/main/java/org/smartregister/chw/model/FamilyProfileMemberModel.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/model/FamilyProfileMemberModel.java
@@ -1,23 +1,11 @@
 package org.smartregister.chw.model;
 
+import com.opensrp.chw.core.model.CoreFamilyProfileMemberModel;
 import com.opensrp.chw.core.utils.ChildDBConstants;
 import com.opensrp.chw.core.utils.Constants;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.smartregister.family.model.BaseFamilyProfileMemberModel;
 
-public class FamilyProfileMemberModel extends BaseFamilyProfileMemberModel {
-
-    @Override
-    protected String[] mainColumns(String tableName) {
-        String[] columns = super.mainColumns(tableName);
-        String[] newColumns = new String[]{
-                tableName + "." + ChildDBConstants.KEY.ENTITY_TYPE,
-                tableName + "." + Constants.JsonAssets.FAMILY_MEMBER.PHONE_NUMBER
-        };
-
-        return ArrayUtils.addAll(columns, newColumns);
-
-
-    }
+public class FamilyProfileMemberModel extends CoreFamilyProfileMemberModel {
 }
