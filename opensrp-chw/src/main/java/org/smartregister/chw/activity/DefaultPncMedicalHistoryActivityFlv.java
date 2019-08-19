6e76b5aa1ad63afe459bbbefe935e24d1f526d0a
paulinembabu <pauline.mbabu@gmail.com>
2019-08-19 20:21:39 +0300

    togo nedical history

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/DefaultPncMedicalHistoryActivityFlv.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/DefaultPncMedicalHistoryActivityFlv.java
@@ -8,6 +8,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.smartregister.chw.R;
@@ -19,6 +21,7 @@ import java.text.DateFormat;
 import java.text.MessageFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -33,11 +36,10 @@ public abstract class DefaultPncMedicalHistoryActivityFlv implements PncMedicalH
     protected LinearLayout linearLayoutHealthFacilityVisitDetails;
     private TextView customFontTextViewLastVisit;
     private LinearLayout linearLayoutLastVisit;
-    private LinearLayout linearLayoutPncHomeVisit;
-    private LinearLayout linearLayoutPncHomeVisitDetails;
+    protected LinearLayout linearLayoutPncHomeVisit;
+    protected LinearLayout linearLayoutPncHomeVisitDetails;
 
-    private Date DeliveryDateformatted;
-    private String VisitDateformattedString;
+
 
     @Override
     public View bindViews(Activity activity) {
@@ -51,9 +53,7 @@ public abstract class DefaultPncMedicalHistoryActivityFlv implements PncMedicalH
         linearLayoutPncHomeVisit = view.findViewById(R.id.linearLayoutPncHomeVisit);
         linearLayoutPncHomeVisitDetails = view.findViewById(R.id.linearLayoutPncHomeVisitDetails);
 
-
         return view;
-
     }
 
     @Override
@@ -63,148 +63,71 @@ public abstract class DefaultPncMedicalHistoryActivityFlv implements PncMedicalH
             int days = 0;
             int x = 0;
             Map<String, String> hf_visits = new LinkedHashMap<>();
-            Map<Integer, String> home_visits = new LinkedHashMap<>();
             while (x < visits.size()) {
 
                 // the first object in this list is the days difference
                 if (x == 0) {
                     days = Days.daysBetween(new DateTime(visits.get(0).getDate()), new DateTime()).getDays();
                 }
-
-                exctractHFVisits(visits, hf_visits, x);
-
                 x++;
             }
-            processLastVisit(days, context);
-            processHealthFacilityVisit(hf_visits, context);
-            exctractHomeVisits(visits, home_visits);
-            processHomeVisits(home_visits, context);
-        }
 
+            processLastVisit(days, context);
+
+        }
     }
 
-
-    private void processLastVisit(int days, Context context) {
+    protected void processLastVisit(int days, Context context) {
         linearLayoutLastVisit.setVisibility(View.VISIBLE);
         customFontTextViewLastVisit.setText(StringUtils.capitalize(MessageFormat.format(context.getString(R.string.days_ago), String.valueOf(days))));
-
     }
 
-    private void exctractHFVisits(List<Visit> sourceVisits, Map<String, String> hf_visits, int iteration) {
-        int x = 1;
-        while (x <= 4) {
-            List<VisitDetail> hf_details = sourceVisits.get(iteration).getVisitDetails().get("pnc_visit_" + x);
-            if (hf_details != null) {
-                for (VisitDetail D : hf_details
-                ) {
-                    if (D.getHumanReadable().equalsIgnoreCase("Yes")) {
 
-                        List<VisitDetail> hf_details_dates = sourceVisits.get(iteration).getVisitDetails().get("pnc_hf_visit" + x + "_date");
+    /**
+     * Extract value from VisitDetail
+     *
+     * @return
+     */
+    @NotNull
+    protected String getText(@Nullable VisitDetail visitDetail) {
+        if (visitDetail == null)
+            return "";
 
-                        if (hf_details_dates != null) {
-                            for (VisitDetail hfDate : hf_details_dates
-                            ) {
+        String val = visitDetail.getHumanReadable();
+        if (StringUtils.isNotBlank(val))
+            return val.trim();
 
-                                hf_visits.put(D.getVisitKey(), hfDate.getDetails());
+        return (StringUtils.isNotBlank(visitDetail.getDetails()))? visitDetail.getDetails().trim() : "";
+    }
 
+    @NotNull
+    protected String getText(@Nullable List<VisitDetail> visitDetails) {
+        if (visitDetails == null)
+            return "";
 
-                            }
-                        }
-                    }
-                }
+        List<String> vals = new ArrayList<>();
+        for (VisitDetail vd : visitDetails) {
+            String val = getText(vd);
+            if (StringUtils.isNotBlank(val))
+                vals.add(val);
+        }
+
+        return toCSV(vals);
+    }
+
+    protected static String toCSV(List<String> list) {
+        String result = "";
+        if (list.size() > 0) {
+            StringBuilder sb = new StringBuilder();
+            for (String s : list) {
+                sb.append(s).append(",");
             }
-
-            x++;
+            result = sb.deleteCharAt(sb.length() - 1).toString();
         }
-
+        return result;
     }
 
 
-    protected void processHealthFacilityVisit(Map<String, String> hf_visits, Context context) {
-        if (hf_visits != null && hf_visits.size() > 0) {
-            linearLayoutHealthFacilityVisit.setVisibility(View.VISIBLE);
-
-            for (Map.Entry<String, String> entry : hf_visits.entrySet()) {
-                View view = inflater.inflate(R.layout.medical_history_pnc_visit, null);
-
-                TextView tvTitle = view.findViewById(R.id.pnctitle);
-                if (entry.getKey().equalsIgnoreCase("pnc_visit_3"))
-                    tvTitle.setText(MessageFormat.format(context.getString(R.string.pnc_visit_date), context.getString(R.string.pnc_eight_to_twenty_eight), entry.getValue()));
-                if (entry.getKey().equalsIgnoreCase("pnc_visit_1"))
-                    tvTitle.setText(MessageFormat.format(context.getString(R.string.pnc_visit_date), context.getString(R.string.pnc_48_hours), entry.getValue()));
-                if (entry.getKey().equalsIgnoreCase("pnc_visit_2"))
-                    tvTitle.setText(MessageFormat.format(context.getString(R.string.pnc_visit_date), context.getString(R.string.pnc_three_to_seven_days), entry.getValue()));
-                if (entry.getKey().equalsIgnoreCase("pnc_visit_4"))
-                    tvTitle.setText(MessageFormat.format(context.getString(R.string.pnc_visit_date), context.getString(R.string.pnc_twenty_nine_to_forty_two), entry.getValue()));
 
 
-                linearLayoutHealthFacilityVisitDetails.addView(view, 0);
-            }
-
-
-        }
-    }
-
-    private void exctractHomeVisits(List<Visit> sourceVisits, Map<Integer, String> home_visits) {
-
-        String id = sourceVisits.get(0).getBaseEntityId();
-        String DeliveryDateSql = "SELECT delivery_date FROM ec_pregnancy_outcome where base_entity_id = ? ";
-
-        List<Map<String, String>> valus = AbstractDao.readData(DeliveryDateSql, new String[]{id});
-
-        String deliveryDate = valus.get(0).get("delivery_date");
-
-
-        if (sourceVisits != null) {
-            for (Visit vst : sourceVisits
-            ) {
-
-                try {
-                    DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-                    DateFormat dateFormat1 = new SimpleDateFormat("yyyy-MM-dd");
-                    DeliveryDateformatted = dateFormat.parse(deliveryDate);
-                    VisitDateformattedString = dateFormat1.format(vst.getDate());
-                } catch (ParseException e) {
-                    Timber.e(e, e.toString());
-                }
-
-
-                int res = Days.daysBetween((new DateTime(DeliveryDateformatted)), new DateTime(vst.getDate())).getDays();
-
-                home_visits.put(res, VisitDateformattedString);
-
-
-                 }
-        }
-
-    }
-
-
-    protected void processHomeVisits(Map<Integer, String> home_visits, Context context) {
-        linearLayoutPncHomeVisit.setVisibility(View.VISIBLE);
-
-
-        for (Map.Entry<Integer, String> entry : home_visits.entrySet()
-        ) {
-            View view = inflater.inflate(R.layout.medical_history_pnc_visit, null);
-
-            TextView tvTitle = view.findViewById(R.id.pnctitle);
-            if (entry.getKey() <= 2)
-                tvTitle.setText(MessageFormat.format(context.getString(R.string.pnc_visit_date), context.getString(R.string.pnc_home_day_one_visit), entry.getValue()));
-            if ((entry.getKey() > 2) && (entry.getKey() <= 3))
-                tvTitle.setText(MessageFormat.format(context.getString(R.string.pnc_visit_date), context.getString(R.string.pnc_home_day_three_visit), entry.getValue()));
-            if ((entry.getKey() > 3) && (entry.getKey() <= 8))
-                tvTitle.setText(MessageFormat.format(context.getString(R.string.pnc_visit_date), context.getString(R.string.pnc_home_day_eight_visit), entry.getValue()));
-            if ((entry.getKey() > 8) && (entry.getKey() <= 27))
-                tvTitle.setText(MessageFormat.format(context.getString(R.string.pnc_visit_date), context.getString(R.string.pnc_home_day_twenty_one_to_twenty_seven_visit), entry.getValue()));
-            if ((entry.getKey() > 27) && (entry.getKey() <= 42))
-                tvTitle.setText(MessageFormat.format(context.getString(R.string.pnc_visit_date), context.getString(R.string.pnc_home_day_thirty_five_to_forty_one_visit), entry.getValue()));
-
-
-            linearLayoutPncHomeVisitDetails.addView(view, 0);
-
-        }
-
-
-    }
 }
