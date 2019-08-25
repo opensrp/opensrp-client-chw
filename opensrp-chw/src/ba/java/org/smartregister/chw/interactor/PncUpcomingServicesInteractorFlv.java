eaa5e5fbd36a0b3780ba6e1f333e54711d39c2aa
paulinembabu <pauline.mbabu@gmail.com>
2019-08-26 02:14:32 +0300

    Added BA upcoming services Page

--- opensrp-chw/src/ba/java/org/smartregister/chw/interactor/PncUpcomingServicesInteractorFlv.java
+++ opensrp-chw/src/ba/java/org/smartregister/chw/interactor/PncUpcomingServicesInteractorFlv.java
@@ -1,42 +1,39 @@
 package org.smartregister.chw.interactor;
 
-
 import android.content.Context;
 
 import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
 import org.smartregister.chw.R;
 import org.smartregister.chw.anc.domain.MemberObject;
 import org.smartregister.chw.anc.model.BaseUpcomingService;
-import org.smartregister.chw.core.dao.AlertDao;
 import org.smartregister.chw.core.rule.PNCHealthFacilityVisitRule;
 import org.smartregister.chw.core.utils.CoreConstants;
 import org.smartregister.chw.core.utils.VaccineScheduleUtil;
 import org.smartregister.chw.dao.PNCDao;
 import org.smartregister.chw.domain.PNCHealthFacilityVisitSummary;
 import org.smartregister.chw.util.PNCVisitUtil;
-import org.smartregister.domain.Alert;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DefaultPncUpcomingServiceInteractor implements PncUpcomingServiceInteractor.Flavor {
+public class PncUpcomingServicesInteractorFlv extends DefaultPncUpcomingServiceInteractor {
     protected MemberObject memberObject;
     protected Context context;
 
+
     @Override
     public List<BaseUpcomingService> getMemberServices(Context context, MemberObject memberObject) {
-         this.memberObject = memberObject;
-         this.context = context;
-        VaccineScheduleUtil.updateOfflineAlerts(memberObject.getBaseEntityId(),new DateTime(memberObject.getDob()), CoreConstants.SERVICE_GROUPS.CHILD);
+        this.memberObject = memberObject;
+        this.context = context;
+        VaccineScheduleUtil.updateOfflineAlerts(memberObject.getBaseEntityId(), new DateTime(memberObject.getDob()), CoreConstants.SERVICE_GROUPS.CHILD);
         List<BaseUpcomingService> serviceList = new ArrayList<>();
-
         evaluateHealthFacility(serviceList);
-        evaluateImmunization(serviceList);
+        return serviceList;
 
-        return  serviceList;
+
     }
+
 
     private void evaluateHealthFacility(List<BaseUpcomingService> serviceList) {
         // create data to display
@@ -46,19 +43,22 @@ public class DefaultPncUpcomingServiceInteractor implements PncUpcomingServiceIn
 
             if (visitRule != null && visitRule.getVisitName() != null) {
 
-                int visit_num;
+                String visit_num;
                 switch (visitRule.getVisitName()) {
                     case "1":
-                        visit_num = 1;
+                        visit_num = "48 hours";
                         break;
-                    case "7":
-                        visit_num = 2;
+                    case "3":
+                        visit_num = "days 3-7";
                         break;
-                    case "42":
-                        visit_num = 3;
+                    case "8":
+                        visit_num = "days 8-28";
+                        break;
+                    case "29":
+                        visit_num = "days 29-42";
                         break;
                     default:
-                        visit_num = 1;
+                        visit_num = "48 hours";
                         break;
                 }
                 BaseUpcomingService upcomingService = new BaseUpcomingService();
@@ -69,20 +69,6 @@ public class DefaultPncUpcomingServiceInteractor implements PncUpcomingServiceIn
 
         }
     }
-    private void evaluateImmunization(List<BaseUpcomingService> serviceList){
-        List<Alert> alerts = AlertDao.getActiveAlertsForVaccines(memberObject.getBaseEntityId());
-        if(alerts == null || alerts.size() == 0)
-            return;
 
-        BaseUpcomingService upcomingService = new BaseUpcomingService();
-        upcomingService.setServiceDate(new LocalDate(alerts.get(0).startDate()).toDate());
-        upcomingService.setServiceName(context.getString(R.string.immunizations, context.getString(R.string.at_birth)));
-        List<BaseUpcomingService> subService = new ArrayList<>();
-        for (Alert alert: alerts) {
-            subService.add(new BaseUpcomingService(alert.scheduleName()));
-        }
-        upcomingService.setUpcomingServiceList(subService);
-        serviceList.add(upcomingService);
 
-    }
 }
