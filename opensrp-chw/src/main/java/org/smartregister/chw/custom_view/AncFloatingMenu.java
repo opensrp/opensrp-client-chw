56d51716934c40d50f3c53133d8bb1f2f18eb445
Elly Kitoto <junkmailstoelly@gmail.com>
2019-08-28 12:28:51 +0300

    Added referral forms for anc and pnc

---
+++ opensrp-chw/src/main/java/org/smartregister/chw/custom_view/AncFloatingMenu.java
@@ -0,0 +1,11 @@
+package org.smartregister.chw.custom_view;
+
+import android.content.Context;
+
+import org.smartregister.chw.anc.custom_views.BaseAncFloatingMenu;
+
+public class AncFloatingMenu extends BaseAncFloatingMenu {
+    public AncFloatingMenu(Context context, String ancWomanName, String ancWomanPhone, String ancFamilyHeadName, String ancFamilyHeadPhone, String profileType) {
+        super(context, ancWomanName, ancWomanPhone, ancFamilyHeadName, ancFamilyHeadPhone, profileType);
+    }
+}
