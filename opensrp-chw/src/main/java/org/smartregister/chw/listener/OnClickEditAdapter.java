b5b54c04bebe2d1b7679d81a05f9bae43f5f0040
rkodev <43806892+rkodev@users.noreply.github.com>
2019-08-30 20:21:04 +0300

    Removed dead code

--- opensrp-chw/src/main/java/org/smartregister/chw/listener/OnClickEditAdapter.java
+++
@@ -1,7 +0,0 @@
-package org.smartregister.chw.listener;
-
-import org.smartregister.chw.core.utils.HomeVisitVaccineGroup;
-
-public interface OnClickEditAdapter {
-    void onClick(int position, HomeVisitVaccineGroup homeVisitVaccineGroup);
-}
