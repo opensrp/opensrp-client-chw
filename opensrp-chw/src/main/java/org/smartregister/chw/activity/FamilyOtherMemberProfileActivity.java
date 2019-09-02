1b1d9819211868eb27b1cfb47f8fd92f628f1d23
Allan O <a.onchuru@gmail.com>
2019-09-02 18:49:53 +0300

    WIP : Override onCreation and check which Profile to render

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/FamilyOtherMemberProfileActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/FamilyOtherMemberProfileActivity.java
@@ -7,6 +7,8 @@ import android.view.Menu;
 
 import org.json.JSONObject;
 import org.smartregister.chw.R;
+import org.smartregister.chw.anc.domain.MemberObject;
+import org.smartregister.chw.application.ChwApplication;
 import org.smartregister.chw.core.activity.CoreFamilyOtherMemberProfileActivity;
 import org.smartregister.chw.core.activity.CoreFamilyProfileActivity;
 import org.smartregister.chw.core.listener.OnClickFloatingMenu;
@@ -22,11 +24,21 @@ import org.smartregister.family.fragment.BaseFamilyOtherMemberProfileFragment;
 import org.smartregister.family.model.BaseFamilyOtherMemberProfileActivityModel;
 import org.smartregister.view.contract.BaseProfileContract;
 
+import java.util.HashMap;
+
 import timber.log.Timber;
 
 public class FamilyOtherMemberProfileActivity extends CoreFamilyOtherMemberProfileActivity {
     private FamilyMemberFloatingMenu familyFloatingMenu;
     private Flavor flavor = new FamilyOtherMemberProfileActivityFlv();
+
+    @Override
+    protected void onCreation() {
+        // TODO -> Umm do this here?
+        initializePresenter();
+        qualifyFamilyMember(commonPersonObject);
+        // TODO -> This has to go somewhere super.onCreation();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,6 +57,21 @@ public class FamilyOtherMemberProfileActivity extends CoreFamilyOtherMemberProfi
         menu.findItem(R.id.action_malaria_diagnosis).setVisible(false);
         menu.findItem(R.id.action_malaria_followup_visit).setVisible(false);
         return true;
+    }
+
+    // Qualify family member and show appropriate profile view
+    protected void qualifyFamilyMember(CommonPersonObjectClient commonPersonObjectClient) {
+        if (presenter().isWomanAlreadyRegisteredOnAnc(commonPersonObjectClient)) {
+            HashMap<String, String> detailsMap = ChwApplication.ancRegisterRepository().getFamilyNameAndPhone(Utils.getValue(commonPersonObjectClient.getColumnmaps(), org.smartregister.family.util.DBConstants.KEY.FAMILY_HEAD, false));
+
+            String familyName = "";
+            String familyHeadPhone = "";
+            if (detailsMap != null) {
+                familyName = detailsMap.get(org.smartregister.chw.anc.util.Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_NAME);
+                familyHeadPhone = detailsMap.get(org.smartregister.chw.anc.util.Constants.ANC_MEMBER_OBJECTS.FAMILY_HEAD_PHONE);
+            }
+            AncMemberProfileActivity.startMe(this, new MemberObject(commonPersonObjectClient), familyName, familyHeadPhone);
+        }
     }
 
     @Override
