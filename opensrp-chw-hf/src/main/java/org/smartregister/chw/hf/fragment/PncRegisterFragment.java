4ce8b36c387c757021dc5b11da559281b5d7a5e9
Elly Kitoto <junkmailstoelly@gmail.com>
2019-09-13 16:09:01 +0300

    Remove due services on PNC register HF

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/PncRegisterFragment.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/PncRegisterFragment.java
@@ -1,18 +1,37 @@
 package org.smartregister.chw.hf.fragment;
 
+import android.view.View;
+
 import org.smartregister.chw.core.fragment.CorePncRegisterFragment;
+import org.smartregister.chw.core.provider.ChwPncRegisterProvider;
+import org.smartregister.chw.hf.provider.HfPncRegisterProvider;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.cursoradapter.RecyclerViewPaginatedAdapter;
+
+import java.util.Set;
 
 public class PncRegisterFragment extends CorePncRegisterFragment {
 
     @Override
+    public void initializeAdapter(Set<org.smartregister.configurableviews.model.View> visibleColumns) {
+        ChwPncRegisterProvider provider = new HfPncRegisterProvider(getActivity(), commonRepository(), visibleColumns, registerActionHandler, paginationViewHandler);
+        clientAdapter = new RecyclerViewPaginatedAdapter(null, provider, context().commonrepository(this.tablename));
+        clientAdapter.setCurrentlimit(20);
+        clientsView.setAdapter(clientAdapter);
+    }
+
+    @Override
+    public void setupViews(View view) {
+        super.setupViews(view);
+    }
+
+    @Override
     protected void openHomeVisit(CommonPersonObjectClient client) {
-      //  PncHomeVisitActivity.startMe(getActivity(), new MemberObject(client), false);
+        //  PncHomeVisitActivity.startMe(getActivity(), new MemberObject(client), false);
     }
 
     @Override
     protected void openPncMemberProfile(CommonPersonObjectClient client) {
 //        PncMemberProfileActivity.startMe(getActivity(), new MemberObject(client), getFamilyName(), getFamilyHeadPhone());
     }
-
 }
