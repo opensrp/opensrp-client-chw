f4c25e2be27043c34a278f0e00c70304e29f6b75
Manu Tarus <manutarus@gmail.com>
2019-10-02 21:12:29 +0300

    codacy fix

--- opensrp-chw/src/main/java/org/smartregister/chw/util/Constants.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/util/Constants.java
@@ -3,6 +3,6 @@ package org.smartregister.chw.util;
 import org.smartregister.chw.core.utils.CoreConstants;
 
 public class Constants extends CoreConstants {
-    public enum familyRegisterOptionsUtil {Miscarriage, Other}
+    public enum FamilyRegisterOptionsUtil {Miscarriage, Other}
     public static String pregnancyOutcome = "preg_outcome";
 }
