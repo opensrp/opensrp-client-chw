075daa93e588652b618b00114ada9b91611c2ded
mahmud6390 <mahmud6390@gmail.com>
2019-10-06 17:10:11 +0600

    merge

--- opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/fragment/FamilyProfileMemberFragment.java
+++ opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/fragment/FamilyProfileMemberFragment.java
@@ -1,21 +1,28 @@
 package org.smartregister.brac.hnpp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import org.smartregister.brac.hnpp.activity.AboveFiveChildProfileActivity;
 import org.smartregister.brac.hnpp.activity.HnppChildProfileActivity;
 import org.smartregister.brac.hnpp.activity.HnppFamilyOtherMemberProfileActivity;
 import org.smartregister.brac.hnpp.model.HNPPFamilyProfileMemberModel;
+import org.smartregister.chw.anc.domain.MemberObject;
 import org.smartregister.chw.core.activity.CoreAboveFiveChildProfileActivity;
 import org.smartregister.chw.core.activity.CoreChildProfileActivity;
 import org.smartregister.chw.core.fragment.CoreFamilyProfileMemberFragment;
 import org.smartregister.chw.core.provider.CoreMemberRegisterProvider;
 import org.smartregister.brac.hnpp.provider.HNPPMemberRegisterProvider;
+import org.smartregister.chw.core.utils.CoreChildUtils;
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.configurableviews.model.View;
 import org.smartregister.cursoradapter.RecyclerViewPaginatedAdapter;
 import org.smartregister.family.fragment.BaseFamilyProfileMemberFragment;
 import org.smartregister.family.presenter.BaseFamilyProfileMemberPresenter;
 import org.smartregister.family.util.Constants;
+import org.smartregister.family.util.DBConstants;
+import org.smartregister.family.util.Utils;
 
 import java.util.Set;
 
@@ -45,6 +52,25 @@ public class FamilyProfileMemberFragment extends CoreFamilyProfileMemberFragment
         String familyHead = getArguments().getString(Constants.INTENT_KEY.FAMILY_HEAD);
         String primaryCareGiver = getArguments().getString(Constants.INTENT_KEY.PRIMARY_CAREGIVER);
         presenter = new BaseFamilyProfileMemberPresenter(this, new HNPPFamilyProfileMemberModel(), null, familyBaseEntityId, familyHead, primaryCareGiver);
+    }
+
+    @Override
+    public void goToChildProfileActivity(CommonPersonObjectClient patient) {
+        String dobString = Utils.getDuration(Utils.getValue(patient.getColumnmaps(), DBConstants.KEY.DOB, false));
+        Integer yearOfBirth = CoreChildUtils.dobStringToYear(dobString);
+        Intent intent;
+        if (yearOfBirth != null && yearOfBirth > 5) {
+            intent = new Intent(getActivity(), getAboveFiveChildProfileActivityClass());
+        } else {
+            intent = new Intent(getActivity(), getChildProfileActivityClass());
+        }
+        if (getArguments() != null) {
+            intent.putExtras(getArguments());
+        }
+        intent.putExtra(CoreConstants.INTENT_KEY.IS_COMES_FROM_FAMILY, true);
+        intent.putExtra(Constants.INTENT_KEY.BASE_ENTITY_ID, patient.getCaseId());
+        intent.putExtra(org.smartregister.chw.anc.util.Constants.ANC_MEMBER_OBJECTS.MEMBER_PROFILE_OBJECT, new MemberObject(patient));
+        startActivity(intent);
     }
 
     @Override
