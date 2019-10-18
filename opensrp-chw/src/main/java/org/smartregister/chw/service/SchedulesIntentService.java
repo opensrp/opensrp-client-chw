06c70d8ba97eb8166406959fd70cc59027e8c6d6
rkodev <43806892+rkodev@users.noreply.github.com>
2019-10-18 14:11:02 +0300

    Recomputes all alerts at login and periodically

--- opensrp-chw/src/main/java/org/smartregister/chw/service/SchedulesIntentService.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/service/SchedulesIntentService.java
@@ -4,6 +4,13 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.dao.ScheduleDao;
+import org.smartregister.chw.schedulers.ChwScheduleTaskExecutor;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * Recompute all schedules to adjust the new dates
  */
@@ -20,12 +27,51 @@ public class SchedulesIntentService extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         // execute all children schedules
+        executeChildVisitSchedules();
 
         // execute all anc schedules
+        executeAncVisitSchedules();
 
         // execute all pnc schedules
+        executePncVisitSchedules();
 
-        // execute all malaria schedules
+        // execute all wash check
+        executeWashCheckSchedules();
+    }
 
+    private void executeChildVisitSchedules() {
+        List<String> baseEntityIDs = ScheduleDao.getActiveChildren();
+        if (baseEntityIDs == null) return;
+
+        for (String baseID : baseEntityIDs) {
+            ChwScheduleTaskExecutor.getInstance().execute(baseID, CoreConstants.EventType.CHILD_HOME_VISIT, new Date());
+        }
+    }
+
+    private void executeAncVisitSchedules() {
+        List<String> baseEntityIDs = ScheduleDao.getActiveANCWomen();
+        if (baseEntityIDs == null) return;
+
+        for (String baseID : baseEntityIDs) {
+            ChwScheduleTaskExecutor.getInstance().execute(baseID, CoreConstants.EventType.ANC_REGISTRATION, new Date());
+        }
+    }
+
+    private void executePncVisitSchedules() {
+        List<String> baseEntityIDs = ScheduleDao.getActivePNCWomen();
+        if (baseEntityIDs == null) return;
+
+        for (String baseID : baseEntityIDs) {
+            ChwScheduleTaskExecutor.getInstance().execute(baseID, CoreConstants.EventType.PREGNANCY_OUTCOME, new Date());
+        }
+    }
+
+    private void executeWashCheckSchedules() {
+        List<String> baseEntityIDs = ScheduleDao.getActiveWashCheckFamilies();
+        if (baseEntityIDs == null) return;
+
+        for (String baseID : baseEntityIDs) {
+            ChwScheduleTaskExecutor.getInstance().execute(baseID, CoreConstants.EventType.WASH_CHECK, new Date());
+        }
     }
 }
