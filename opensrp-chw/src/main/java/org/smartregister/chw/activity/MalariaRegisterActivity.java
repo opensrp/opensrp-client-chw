91ce574960d1224f01a2ee377f1d06baaeade1cb
Manu Tarus <manutarus@gmail.com>
2019-10-28 18:19:51 +0300

    Updates on activity result for follo up form

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/MalariaRegisterActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/MalariaRegisterActivity.java
@@ -26,12 +26,6 @@ public class MalariaRegisterActivity extends CoreMalariaRegisterActivity {
     }
 
     @Override
-    protected void startMalariaRegisterActivity(Activity activity, String s) {
-        startMalariaRegistrationActivity(activity, s);
-    }
-
-
-    @Override
     protected void registerBottomNavigation() {
 
         bottomNavigationHelper = new BottomNavigationHelper();
