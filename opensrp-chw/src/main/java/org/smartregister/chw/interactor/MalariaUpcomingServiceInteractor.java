81927c423ca3b1563d0f66e6cd743be993e82c7a
rkodev <43806892+rkodev@users.noreply.github.com>
2019-10-29 16:47:32 +0300

    Updated malaria upcoming services

--- opensrp-chw/src/main/java/org/smartregister/chw/interactor/MalariaUpcomingServiceInteractor.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/interactor/MalariaUpcomingServiceInteractor.java
@@ -7,12 +7,20 @@ import org.smartregister.chw.anc.interactor.BaseAncUpcomingServicesInteractor;
 import org.smartregister.chw.anc.model.BaseUpcomingService;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MalariaUpcomingServiceInteractor extends BaseAncUpcomingServicesInteractor {
 
     @Override
     protected List<BaseUpcomingService> getMemberServices(Context context, MemberObject memberObject) {
-        return new ArrayList<>();
+        List<BaseUpcomingService> upcomingServices = new ArrayList<>();
+        upcomingServices.addAll(new AncUpcomingServicesInteractorFlv().getMemberServices(context, memberObject));
+        upcomingServices.addAll(new PncUpcomingServicesInteractorFlv().getMemberServices(context, memberObject));
+        //TODO add malaria and child
+
+        Collections.sort(upcomingServices, (s1, s2) -> s1.getServiceDate().compareTo(s2.getServiceDate()));
+        //
+        return upcomingServices;
     }
 }
