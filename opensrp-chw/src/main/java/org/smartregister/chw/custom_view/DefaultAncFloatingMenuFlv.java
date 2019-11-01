a34a78eb8a0ce526c8a2fa9ef3949108e9542d83
rkodev <43806892+rkodev@users.noreply.github.com>
2019-11-01 13:00:44 +0300

    Updated code accessibility on Floating Menu

---
+++ opensrp-chw/src/main/java/org/smartregister/chw/custom_view/DefaultAncFloatingMenuFlv.java
@@ -0,0 +1,8 @@
+package org.smartregister.chw.custom_view;
+
+public class DefaultAncFloatingMenuFlv implements AncFloatingMenu.Flavor {
+    @Override
+    public boolean hasReferral() {
+        return false;
+    }
+}
