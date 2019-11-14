6e001776900ed4704ee57e86d7ed45cc8103b1a5
Manu Tarus <manutarus@gmail.com>
2019-11-14 15:50:49 +0300

    add malaria upcoming service dates

--- opensrp-chw/src/main/java/org/smartregister/chw/interactor/MalariaUpcomingServiceInteractor.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/interactor/MalariaUpcomingServiceInteractor.java
@@ -2,12 +2,18 @@ package org.smartregister.chw.interactor;
 
 import android.content.Context;
 
+import org.joda.time.LocalDate;
+import org.smartregister.chw.R;
 import org.smartregister.chw.anc.domain.MemberObject;
 import org.smartregister.chw.anc.interactor.BaseAncUpcomingServicesInteractor;
 import org.smartregister.chw.anc.model.BaseUpcomingService;
+import org.smartregister.chw.core.dao.AncDao;
+import org.smartregister.chw.core.dao.ChildDao;
+import org.smartregister.chw.core.dao.PNCDao;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class MalariaUpcomingServiceInteractor extends BaseAncUpcomingServicesInteractor {
@@ -15,9 +21,23 @@ public class MalariaUpcomingServiceInteractor extends BaseAncUpcomingServicesInt
     @Override
     protected List<BaseUpcomingService> getMemberServices(Context context, MemberObject memberObject) {
         List<BaseUpcomingService> upcomingServices = new ArrayList<>();
-        upcomingServices.addAll(new AncUpcomingServicesInteractorFlv().getMemberServices(context, memberObject));
-        upcomingServices.addAll(new PncUpcomingServicesInteractorFlv().getMemberServices(context, memberObject));
-        //TODO add malaria and child
+
+        BaseUpcomingService followUP = new BaseUpcomingService();
+        followUP.setServiceName(context.getString(R.string.follow_up_visit));
+        followUP.setServiceDate(new Date());
+        followUP.setOverDueDate(LocalDate.now().plusDays(2).toDate());
+
+        upcomingServices.add(followUP);
+
+
+        String baseEntityID = memberObject.getBaseEntityId();
+        if (PNCDao.isPNCMember(baseEntityID)) {
+            upcomingServices.addAll(new PncUpcomingServicesInteractorFlv().getMemberServices(context, memberObject));
+        } else if (AncDao.isANCMember(baseEntityID)) {
+            upcomingServices.addAll(new AncUpcomingServicesInteractorFlv().getMemberServices(context, memberObject));
+        } else if (ChildDao.isChild(baseEntityID)) {
+            upcomingServices.addAll(new ChildUpcomingServicesInteractor().getMemberServices(context, memberObject));
+        }
 
         Collections.sort(upcomingServices, (s1, s2) -> s1.getServiceDate().compareTo(s2.getServiceDate()));
         //
