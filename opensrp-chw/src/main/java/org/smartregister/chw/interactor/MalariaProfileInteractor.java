baa0c4f9c6dec8fb4be2d561f6a94c3137793811
Manu Tarus <manutarus@gmail.com>
2019-11-14 15:49:21 +0300

    add upcoming services for malaria

--- opensrp-chw/src/main/java/org/smartregister/chw/interactor/MalariaProfileInteractor.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/interactor/MalariaProfileInteractor.java
@@ -3,7 +3,10 @@ package org.smartregister.chw.interactor;
 import android.content.Context;
 
 import org.joda.time.LocalDate;
+import org.smartregister.chw.R;
 import org.smartregister.chw.anc.model.BaseUpcomingService;
+import org.smartregister.chw.core.dao.AncDao;
+import org.smartregister.chw.core.dao.ChildDao;
 import org.smartregister.chw.core.dao.PNCDao;
 import org.smartregister.chw.core.dao.VisitDao;
 import org.smartregister.chw.core.interactor.CoreMalariaProfileInteractor;
@@ -45,30 +48,47 @@ public class MalariaProfileInteractor extends CoreMalariaProfileInteractor {
 
     private Alert getAlerts(Context context, String baseEntityID) {
         List<BaseUpcomingService> baseUpcomingServices = new ArrayList<>();
-        org.smartregister.chw.anc.domain.MemberObject memberObject = PNCDao.getMember(baseEntityID);
-        try {
-            baseUpcomingServices.addAll(new PncUpcomingServicesInteractorFlv().getMemberServices(context, memberObject));
-            baseUpcomingServices.addAll(new AncUpcomingServicesInteractorFlv().getMemberServices(context, memberObject));
-            //TODO add upcoming services for malaria and child
-            if (baseUpcomingServices.size() > 0) {
-                Comparator<BaseUpcomingService> comparator = (o1, o2) -> o1.getServiceDate().compareTo(o2.getServiceDate());
-                Collections.sort(baseUpcomingServices, comparator);
+        org.smartregister.chw.anc.domain.MemberObject memberObject;
 
-                BaseUpcomingService baseUpcomingService = baseUpcomingServices.get(0);
-                return new Alert(
-                        memberObject.getBaseEntityId(),
-                        baseUpcomingService.getServiceName(),
-                        baseUpcomingService.getServiceName(),
-                        baseUpcomingService.getServiceDate().before(new Date()) ? AlertStatus.urgent : AlertStatus.normal,
-                        AbstractDao.getDobDateFormat().format(baseUpcomingService.getServiceDate()),
-                        "",
-                        true
-                );
-            }
+        if (PNCDao.isPNCMember(baseEntityID)) {
+            memberObject = PNCDao.getMember(baseEntityID);
+            baseUpcomingServices.addAll(new PncUpcomingServicesInteractorFlv().getMemberServices(context, memberObject));
+        } else if (AncDao.isANCMember(baseEntityID)) {
+            memberObject = AncDao.getMember(baseEntityID);
+            baseUpcomingServices.addAll(new AncUpcomingServicesInteractorFlv().getMemberServices(context, memberObject));
+        } else if (ChildDao.isChild(baseEntityID)) {
+            memberObject = ChildDao.getMember(baseEntityID);
+            baseUpcomingServices.addAll(new ChildUpcomingServicesInteractor().getMemberServices(context, memberObject));
+        }
+
+        try {
+            // malaria follow up visit
+            BaseUpcomingService followUP = new BaseUpcomingService();
+            followUP.setServiceName(context.getString(R.string.follow_up_visit));
+            followUP.setServiceDate(new Date());
+            followUP.setOverDueDate(LocalDate.now().plusDays(2).toDate());
+
+            baseUpcomingServices.add(followUP);
         } catch (Exception e) {
             Timber.e(e);
         }
 
-        return null;
+        if (baseUpcomingServices.size() > 0) {
+            Comparator<BaseUpcomingService> comparator = (o1, o2) -> o1.getServiceDate().compareTo(o2.getServiceDate());
+            Collections.sort(baseUpcomingServices, comparator);
+
+            BaseUpcomingService baseUpcomingService = baseUpcomingServices.get(0);
+            return new Alert(
+                    baseEntityID,
+                    baseUpcomingService.getServiceName(),
+                    baseUpcomingService.getServiceName(),
+                    baseUpcomingService.getServiceDate().before(new Date()) ? AlertStatus.urgent : AlertStatus.normal,
+                    AbstractDao.getDobDateFormat().format(baseUpcomingService.getServiceDate()),
+                    "",
+                    true
+            );
+        } else {
+            return null;
+        }
     }
 }
