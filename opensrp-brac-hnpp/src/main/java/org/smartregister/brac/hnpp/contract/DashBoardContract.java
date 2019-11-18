af5e3a320de6042d92b6e0dfe0b03f896b7859f9
mahmud6390 <mahmud6390@gmail.com>
2019-11-18 12:58:25 +0600

    p2

---
+++ opensrp-brac-hnpp/src/main/java/org/smartregister/brac/hnpp/contract/DashBoardContract.java
@@ -0,0 +1,21 @@
+package org.smartregister.brac.hnpp.contract;
+
+public interface DashBoardContract {
+
+    public interface InteractorCallBack{
+
+    }
+    public interface Interactor{
+
+    }
+    public interface Model{
+
+    }
+    public interface Presenter{
+
+    }
+    public interface View{
+
+    }
+
+}
