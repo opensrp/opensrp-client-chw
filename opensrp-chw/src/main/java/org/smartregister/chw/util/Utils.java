c88176e00a82dd98c6d131af79280691f6b5ecb9
Manu Tarus <manutarus@gmail.com>
2019-11-20 11:43:26 +0300

    abstract malaria upcoming services

--- opensrp-chw/src/main/java/org/smartregister/chw/util/Utils.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/util/Utils.java
@@ -1,13 +1,20 @@
 package org.smartregister.chw.util;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.smartregister.chw.R;
 import org.smartregister.chw.activity.ClientReferralActivity;
+import org.smartregister.chw.anc.model.BaseUpcomingService;
+import org.smartregister.chw.core.dao.MalariaDao;
 import org.smartregister.chw.model.ReferralTypeModel;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Utils extends org.smartregister.chw.core.utils.Utils {
@@ -17,5 +24,17 @@ public class Utils extends org.smartregister.chw.core.utils.Utils {
         bundle.putString(Constants.ENTITY_ID, baseEntityId);
         bundle.putParcelableArrayList(Constants.REFERRAL_TYPES, (ArrayList<ReferralTypeModel>) referralTypeModels);
         activity.startActivity(new Intent(activity, ClientReferralActivity.class).putExtras(bundle));
+    }
+
+    public static void malariaUpcomingServices(String baseEntityID, Context context, List<BaseUpcomingService> baseUpcomingServices) {
+        Date malariaTestDate = MalariaDao.getMalariaTestDate(baseEntityID);
+        DateTime dateTime = new DateTime(malariaTestDate);
+        if (Days.daysBetween(dateTime, new DateTime()).getDays() <= 14) {
+            BaseUpcomingService followUP = new BaseUpcomingService();
+            followUP.setServiceName(context.getString(R.string.follow_up_visit));
+            followUP.setServiceDate(dateTime.plusDays(7).toDate());
+            followUP.setOverDueDate(dateTime.plusDays(10).toDate());
+            baseUpcomingServices.add(followUP);
+        }
     }
 }
