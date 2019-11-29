30fa414daede0bc9e51c40d29105f8da8c58da82
rkodev <43806892+rkodev@users.noreply.github.com>
2019-11-29 13:07:56 +0300

    Add home visit result to anc sort

--- opensrp-chw/src/main/java/org/smartregister/chw/model/AncRegisterFragmentModel.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/model/AncRegisterFragmentModel.java
@@ -1,9 +1,25 @@
 package org.smartregister.chw.model;
 
 import org.smartregister.chw.core.model.CoreAncRegisterFragmentModel;
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.cursoradapter.SmartRegisterQueryBuilder;
+import org.smartregister.family.util.DBConstants;
 
 public class AncRegisterFragmentModel extends CoreAncRegisterFragmentModel {
+
     public AncRegisterFragmentModel() {
         setFlavor(new AncRegisterFragmentModelFlv());
     }
+
+    @Override
+    public String mainSelect(String tableName, String mainCondition) {
+        SmartRegisterQueryBuilder queryBuilder = new SmartRegisterQueryBuilder();
+        queryBuilder.SelectInitiateMainTable(tableName, mainColumns(tableName));
+        queryBuilder.customJoin("INNER JOIN " + CoreConstants.TABLE_NAME.FAMILY_MEMBER + " ON  " + tableName + "." + DBConstants.KEY.BASE_ENTITY_ID + " = " + CoreConstants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.BASE_ENTITY_ID + " COLLATE NOCASE ");
+        queryBuilder.customJoin("INNER JOIN " + CoreConstants.TABLE_NAME.ANC_MEMBER_LOG + " ON  " + tableName + "." + DBConstants.KEY.BASE_ENTITY_ID + " = " + CoreConstants.TABLE_NAME.ANC_MEMBER_LOG + "." + DBConstants.KEY.BASE_ENTITY_ID + " COLLATE NOCASE ");
+        queryBuilder.customJoin("INNER JOIN " + CoreConstants.TABLE_NAME.FAMILY + " ON  " + CoreConstants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.RELATIONAL_ID + " = " + CoreConstants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.BASE_ENTITY_ID + " COLLATE NOCASE ");
+        queryBuilder.customJoin("LEFT JOIN (select base_entity_id , max(visit_date) visit_date from visits GROUP by base_entity_id) VISIT_SUMMARY ON VISIT_SUMMARY.base_entity_id = " + tableName + "." + DBConstants.KEY.BASE_ENTITY_ID);
+        return queryBuilder.mainCondition(mainCondition);
+    }
+
 }
