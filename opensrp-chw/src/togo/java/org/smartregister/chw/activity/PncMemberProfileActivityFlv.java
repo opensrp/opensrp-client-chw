d23a12919acd8708c38f9cf7ba8d878069ccf133
Manu Tarus <manutarus@gmail.com>
2019-12-05 20:10:51 +0300

    add default flv for pnc profile

--- opensrp-chw/src/togo/java/org/smartregister/chw/activity/PncMemberProfileActivityFlv.java
+++ opensrp-chw/src/togo/java/org/smartregister/chw/activity/PncMemberProfileActivityFlv.java
@@ -1,15 +1,5 @@
 package org.smartregister.chw.activity;
 
-import android.view.Menu;
-
-import org.smartregister.chw.R;
-
-public class PncMemberProfileActivityFlv implements PncMemberProfileActivity.Flavor {
-
-    @Override
-    public Boolean onCreateOptionsMenu(Menu menu) {
-        menu.findItem(R.id.action_malaria_confirmation).setVisible(false);
-        return true;
-    }
+public class PncMemberProfileActivityFlv extends DefaultPncMemberProfileActivityFlv {
 
 }
