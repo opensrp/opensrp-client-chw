2f2ec78841cb0e4c4c0beb29119e47f75981e207
Manu Tarus <manutarus@gmail.com>
2019-12-10 15:13:25 +0300

    update ba flavor with new menu items

--- opensrp-chw/src/ba/java/org/smartregister/chw/activity/PncMemberProfileActivityFlv.java
+++ opensrp-chw/src/ba/java/org/smartregister/chw/activity/PncMemberProfileActivityFlv.java
@@ -4,12 +4,13 @@ import android.view.Menu;
 
 import org.smartregister.chw.R;
 import org.smartregister.chw.fp.dao.FpDao;
+import org.smartregister.chw.util.UtilsFlv;
 
 public class PncMemberProfileActivityFlv implements PncMemberProfileActivity.Flavor {
 
     @Override
     public Boolean onCreateOptionsMenu(Menu menu, String baseEntityId) {
-        menu.findItem(R.id.action_malaria_confirmation).setVisible(true);
+        UtilsFlv.updateMalariaMenuItems(baseEntityId, menu);
         if (!FpDao.isRegisteredForFp(baseEntityId)) {
             menu.findItem(R.id.action_fp_initiation).setVisible(true);
         }
