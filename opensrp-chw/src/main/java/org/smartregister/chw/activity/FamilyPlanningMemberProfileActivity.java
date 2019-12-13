0f0ca8a15927bc2a9c5d68bc9ffc0a477fbf14b6
Manu Tarus <manutarus@gmail.com>
2019-12-13 18:15:50 +0300

    add flag to edit form

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/FamilyPlanningMemberProfileActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/FamilyPlanningMemberProfileActivity.java
@@ -131,7 +131,7 @@ public class FamilyPlanningMemberProfileActivity extends BaseFpProfileActivity
         super.onClick(view);
         int id = view.getId();
         if (id == R.id.record_fp_followup_visit) {
-            openFollowUpVisitForm();
+            openFollowUpVisitForm(false);
         }
     }
 
@@ -351,7 +351,7 @@ public class FamilyPlanningMemberProfileActivity extends BaseFpProfileActivity
     }
 
     @Override
-    public void openFollowUpVisitForm(boolean edit) {
+    public void openFollowUpVisitForm(boolean isEdit) {
         // Implement -> FamilyPlanningFollowUpActivity.startRegistrationActivity(this, fpMemberObject.getBaseEntityId());
     }
 
