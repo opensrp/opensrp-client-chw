b53ef1bff9b374bd17a15a794c1b43574b2ad61a
Manu Tarus <manutarus@gmail.com>
2019-12-19 20:29:38 +0300

    add tittle to form

--- opensrp-chw/src/main/java/org/smartregister/chw/dataloader/FPDataLoader.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/dataloader/FPDataLoader.java
@@ -1,5 +1,11 @@
 package org.smartregister.chw.dataloader;
 
+import android.content.Context;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.smartregister.chw.form_data.NativeFormsDataLoader;
 import org.smartregister.chw.fp.util.FamilyPlanningConstants;
 
@@ -7,6 +13,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FPDataLoader extends NativeFormsDataLoader {
+
+    private String title;
+
+    public FPDataLoader(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void bindNativeFormsMetaData(@NotNull JSONObject jsonObjectForm, Context context, String baseEntityID) throws JSONException {
+        super.bindNativeFormsMetaData(jsonObjectForm, context, baseEntityID);
+        JSONObject stepOne = jsonObjectForm.getJSONObject("step1");
+        if (StringUtils.isNotBlank(title))
+            stepOne.put("title", title);
+    }
 
     @Override
     protected List<String> getEventTypes() {
