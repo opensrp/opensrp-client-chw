82ed1029ebe581d29f7a80dab7c55457dc96e490
Elly Kitoto <junkmailstoelly@gmail.com>
2020-01-08 19:13:28 +0300

    Fix app crash on Samsung J5
    App was crashing when opening referral page from client
    profile. Cause the display format - apparently for API version 21,22
    the YYYY should be in lower case instead.

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/ClientReferralActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/ClientReferralActivity.java
@@ -1,12 +1,9 @@
 package org.smartregister.chw.activity;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,11 +13,8 @@ import org.smartregister.chw.R;
 import org.smartregister.chw.adapter.ReferralTypeAdapter;
 import org.smartregister.chw.contract.ClientReferralContract;
 import org.smartregister.chw.core.utils.CoreConstants;
-import org.smartregister.chw.core.utils.CoreReferralUtils;
 import org.smartregister.chw.model.ReferralTypeModel;
 import org.smartregister.chw.util.Constants;
-import org.smartregister.family.util.JsonFormUtils;
-import org.smartregister.family.util.Utils;
 import org.smartregister.util.FormUtils;
 
 import java.util.HashMap;
@@ -34,23 +28,14 @@ public class ClientReferralActivity extends AppCompatActivity implements ClientR
     private ReferralTypeAdapter referralTypeAdapter;
     private FormUtils formUtils;
     private String baseEntityId;
-    private Map<String, String> encounterTypeToTableMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_referral);
         referralTypeAdapter = new ReferralTypeAdapter();
-        encounterTypeToTableMap = new HashMap<>();
-        mapEncounterTypeToTable();
         referralTypeAdapter.setOnClickListener(this);
         setUpView();
-    }
-
-    private void mapEncounterTypeToTable() {
-        encounterTypeToTableMap.put(Constants.EncounterType.SICK_CHILD, CoreConstants.TABLE_NAME.CHILD_REFERRAL);
-        encounterTypeToTableMap.put(Constants.EncounterType.PNC_REFERRAL, CoreConstants.TABLE_NAME.PNC_REFERRAL);
-        encounterTypeToTableMap.put(Constants.EncounterType.ANC_REFERRAL, CoreConstants.TABLE_NAME.ANC_REFERRAL);
     }
 
     @Override
@@ -62,7 +47,7 @@ public class ClientReferralActivity extends AppCompatActivity implements ClientR
         closeImageView.setOnClickListener(this);
 
         if (getIntent().getExtras() != null) {
-            referralTypeModels = getIntent().getExtras().getParcelableArrayList(Constants.REFERRAL_TYPES);
+            referralTypeModels = getIntent().getParcelableArrayListExtra(Constants.REFERRAL_TYPES);
             baseEntityId = getIntent().getStringExtra(Constants.ENTITY_ID);
         }
 
@@ -80,21 +65,9 @@ public class ClientReferralActivity extends AppCompatActivity implements ClientR
     @Override
     public FormUtils getFormUtils() throws Exception {
         if (this.formUtils == null) {
-            this.formUtils = new FormUtils(this.getApplicationContext());
+            this.formUtils = new FormUtils(this);
         }
         return this.formUtils;
-    }
-
-    @Override
-    public boolean isReferralForm(String encounterType) {
-        switch (encounterType) {
-            case CoreConstants.EventType.CHILD_REFERRAL:
-            case CoreConstants.EventType.PNC_REFERRAL:
-            case CoreConstants.EventType.ANC_REFERRAL:
-                return true;
-            default:
-                return false;
-        }
     }
 
     @Override
@@ -118,25 +91,6 @@ public class ClientReferralActivity extends AppCompatActivity implements ClientR
                 Timber.e(e, "ClientReferralActivity --> onActivityResult");
             }
         }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != Activity.RESULT_OK) return;
-        if (requestCode == JsonFormUtils.REQUEST_CODE_GET_JSON)
-            try {
-                String jsonString = data.getStringExtra(org.smartregister.family.util.Constants.JSON_FORM_EXTRA.JSON);
-                JSONObject form = new JSONObject(jsonString);
-                String encounterType = form.getString(JsonFormUtils.ENCOUNTER_TYPE);
-                if (isReferralForm(encounterType)) {
-                    CoreReferralUtils.createReferralEvent(Utils.getAllSharedPreferences(),
-                            jsonString, encounterTypeToTableMap.get(encounterType), baseEntityId);
-                    Utils.showToast(this, this.getString(R.string.referral_submitted));
-                }
-            } catch (Exception e) {
-                Timber.e(e, "ClientReferralActivity --> onActivityResult");
-            }
     }
 
     @Override
--- opensrp-chw/src/main/java/org/smartregister/chw/contract/ClientReferralContract.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/contract/ClientReferralContract.java
@@ -5,12 +5,12 @@ import org.smartregister.util.FormUtils;
 
 public interface ClientReferralContract {
     interface View {
+
         void setUpView();
 
         void startReferralForm(JSONObject jsonObject);
 
         FormUtils getFormUtils() throws Exception;
 
-        boolean isReferralForm(String encounterType);
     }
 }
