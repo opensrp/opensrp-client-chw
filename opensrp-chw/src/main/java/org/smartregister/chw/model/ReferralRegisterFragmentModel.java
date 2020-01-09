f3740e998613388974ceb03504dd80a9f2db4ff1
cozej4 <ilakozejumanne@gmail.com>
2020-01-09 08:57:36 +0300

    Code refactoring

--- opensrp-chw/src/main/java/org/smartregister/chw/model/ReferralRegisterFragmentModel.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/model/ReferralRegisterFragmentModel.java
@@ -21,8 +21,6 @@ public class ReferralRegisterFragmentModel extends BaseReferralRegisterFragmentM
         queryBuilder.SelectInitiateMainTable(tableName, mainColumns(tableName));
         queryBuilder.customJoin("INNER JOIN " + Constants.TABLE_NAME.FAMILY_MEMBER + " ON  " + tableName + "." + DBConstants.KEY.BASE_ENTITY_ID + " = " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.BASE_ENTITY_ID + " COLLATE NOCASE ");
         queryBuilder.customJoin("INNER JOIN " + Constants.TABLE_NAME.FAMILY + " ON  " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.RELATIONAL_ID + " = " + Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.BASE_ENTITY_ID);
-        queryBuilder.customJoin("INNER JOIN " + org.smartregister.chw.referral.util.Constants.TABLES.REFERRAL_SERVICE + " ON  " + tableName + "." + org.smartregister.chw.referral.util.DBConstants.KEY.REFERRAL_SERVICE_ID + " = " + org.smartregister.chw.referral.util.Constants.TABLES.REFERRAL_SERVICE + "." + org.smartregister.chw.referral.util.DBConstants.KEY.ID + " COLLATE NOCASE ");
-        queryBuilder.customJoin("INNER JOIN location ON  " + tableName + "." + org.smartregister.chw.referral.util.DBConstants.KEY.REFERRAL_HF + " = location.uuid COLLATE NOCASE ");
         return queryBuilder.mainCondition(mainCondition);
     }
 
@@ -42,12 +40,11 @@ public class ReferralRegisterFragmentModel extends BaseReferralRegisterFragmentM
         columnList.add(Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.VILLAGE_TOWN);
         columnList.add(Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.FAMILY_HEAD);
         columnList.add(Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.PRIMARY_CAREGIVER);
-        columnList.add("location.name  AS " + org.smartregister.chw.referral.util.DBConstants.KEY.REFERRAL_HF);
-        columnList.add(org.smartregister.chw.referral.util.Constants.TABLES.REFERRAL_SERVICE + "." + org.smartregister.chw.referral.util.DBConstants.KEY.REFERRAL_SERVICE_IDENTIFIER + "  AS " + org.smartregister.chw.referral.util.DBConstants.KEY.REFERRAL_SERVICE);
+        columnList.add(org.smartregister.chw.referral.util.Constants.TABLES.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.KEY.REFERRAL_SERVICE+"  AS " + org.smartregister.chw.referral.util.DBConstants.KEY.REFERRAL_SERVICE);
         columnList.add(org.smartregister.chw.referral.util.Constants.TABLES.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.KEY.REFERRAL_DATE);
         columnList.add(org.smartregister.chw.referral.util.Constants.TABLES.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.KEY.REFERRAL_STATUS);
-        columnList.add(org.smartregister.chw.referral.util.Constants.TABLES.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.KEY.PROBLEMS_IDS);
-        columnList.add(org.smartregister.chw.referral.util.Constants.TABLES.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.KEY.REFERRAL_REASON);
+        columnList.add(org.smartregister.chw.referral.util.Constants.TABLES.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.KEY.PROBLEM);
+        columnList.add(org.smartregister.chw.referral.util.Constants.TABLES.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.KEY.PROBLEM_OTHER);
         columnList.add(org.smartregister.chw.referral.util.Constants.TABLES.REFERRAL + "." + org.smartregister.chw.referral.util.DBConstants.KEY.REFERRAL_APPOINTMENT_DATE);
         columnList.add(Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.FIRST_NAME + " as " + org.smartregister.chw.anc.util.DBConstants.KEY.FAMILY_NAME);
         return columnList.toArray(new String[columnList.size()]);
