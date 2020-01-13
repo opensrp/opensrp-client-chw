9bdfe1275a807606aeba75e0dd2c6e8c6db20528
cozej4 <ilakozejumanne@gmail.com>
2020-01-13 08:37:47 +0300

    Code refactoring

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/ReferralRegistrationActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/ReferralRegistrationActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 
 import org.json.JSONObject;
 import org.smartregister.chw.presenter.IssueReferralActivityPresenter;
-import org.smartregister.chw.referral.ReferralLibrary;
 import org.smartregister.chw.referral.activity.BaseIssueReferralActivity;
 import org.smartregister.chw.referral.contract.BaseIssueReferralContract;
 import org.smartregister.chw.referral.interactor.BaseIssueReferralInteractor;
@@ -17,10 +16,7 @@ import org.smartregister.family.util.JsonFormUtils;
 public class ReferralRegistrationActivity extends BaseIssueReferralActivity {
     private static String BASE_ENTITY_ID;
 
-    public static void startGeneralReferralFormActivityForResults(Activity activity, String baseEntityID, JSONObject formJsonObject,String referralServiceId) {
-        //TODO Coze: the following line is used to seed services and indicators only for testing purposes and should not be available for production version of the app
-        ReferralLibrary.getInstance().seedSampleReferralServicesAndIndicators();
-
+    public static void startGeneralReferralFormActivityForResults(Activity activity, String baseEntityID, JSONObject formJsonObject, String referralServiceId) {
         BASE_ENTITY_ID = baseEntityID;
         Intent intent = new Intent(activity, ReferralRegistrationActivity.class);
         intent.putExtra(org.smartregister.chw.referral.util.Constants.ACTIVITY_PAYLOAD.BASE_ENTITY_ID, baseEntityID);
