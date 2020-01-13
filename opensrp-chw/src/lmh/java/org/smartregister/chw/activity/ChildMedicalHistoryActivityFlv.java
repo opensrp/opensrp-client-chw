8f93481df8796f675751be319af95179c89b5670
rkodev <43806892+rkodev@users.noreply.github.com>
2020-01-13 15:15:42 +0300

    Set up LMH project

---
+++ opensrp-chw/src/lmh/java/org/smartregister/chw/activity/ChildMedicalHistoryActivityFlv.java
@@ -0,0 +1,6 @@
+package org.smartregister.chw.activity;
+
+import org.smartregister.chw.core.activity.DefaultChildMedicalHistoryActivityFlv;
+
+public class ChildMedicalHistoryActivityFlv extends DefaultChildMedicalHistoryActivityFlv {
+}
