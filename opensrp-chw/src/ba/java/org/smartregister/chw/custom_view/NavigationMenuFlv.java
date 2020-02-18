5789a30230fe386f6b3dcf2f6e9eff542065377a
Allan O <a.onchuru@gmail.com>
2020-02-18 15:48:15 +0300

    :boom: Implement getTableMapValues() in NavigationMenuFlv

--- opensrp-chw/src/ba/java/org/smartregister/chw/custom_view/NavigationMenuFlv.java
+++ opensrp-chw/src/ba/java/org/smartregister/chw/custom_view/NavigationMenuFlv.java
@@ -2,9 +2,16 @@ package org.smartregister.chw.custom_view;
 
 import org.smartregister.chw.core.custom_views.NavigationMenu;
 
+import java.util.HashMap;
+
 public class NavigationMenuFlv implements NavigationMenu.Flavour {
     @Override
     public String[] getSupportedLanguages() {
         return new String[]{"English", "Kiswahili"};
+    }
+
+    @Override
+    public HashMap<String, String> getTableMapValues() {
+        return null;
     }
 }
