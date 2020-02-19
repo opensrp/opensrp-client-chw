8da1df8b8b2fcc0c520a355fd54c1bc112748a2d
paulinembabu <pauline.mbabu@gmail.com>
2020-02-19 09:52:17 +0300

    Changes the Title for mother nutritional task to  be Nutritional status mother optional

--- opensrp-chw/src/main/java/org/smartregister/chw/interactor/FpUpcomingServicesInteractor.java
+++
@@ -1,6 +0,0 @@
-package org.smartregister.chw.interactor;
-
-import org.smartregister.chw.core.interactor.CoreFpUpcomingServicesInteractor;
-
-public class FpUpcomingServicesInteractor extends CoreFpUpcomingServicesInteractor {
-}
