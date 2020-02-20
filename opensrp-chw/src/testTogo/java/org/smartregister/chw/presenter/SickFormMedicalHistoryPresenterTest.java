acaa8249b3686e46ded370802460b2b3154e7191
paulinembabu <pauline.mbabu@gmail.com>
2020-02-20 12:07:05 +0300

    Fixes coverage issues by adding test

---
+++ opensrp-chw/src/testTogo/java/org/smartregister/chw/presenter/SickFormMedicalHistoryPresenterTest.java
@@ -0,0 +1,64 @@
+package org.smartregister.chw.presenter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.reflect.Whitebox;
+import org.smartregister.chw.anc.domain.MemberObject;
+import org.smartregister.chw.anc.domain.Visit;
+import org.smartregister.chw.contract.SickFormMedicalHistoryContract;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+
+public class SickFormMedicalHistoryPresenterTest {
+    @Mock
+    private SickFormMedicalHistoryContract.Interactor interactor;
+    @Mock
+    private SickFormMedicalHistoryContract.View view;
+
+    @Mock
+    private MemberObject memberObject;
+
+    private SickFormMedicalHistoryPresenter presenter;
+
+    @Mock
+    private List<Visit> serviceList;
+
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        presenter = Mockito.mock(SickFormMedicalHistoryPresenter.class, Mockito.CALLS_REAL_METHODS);
+        Whitebox.setInternalState(presenter, "interactor", interactor);
+        Whitebox.setInternalState(presenter, "view", new WeakReference<>(view));
+        Whitebox.setInternalState(presenter, "memberObject", memberObject);
+        initialize();
+    }
+
+
+    @Test
+    public void getView() {
+        Assert.assertEquals(view, presenter.getView());
+    }
+
+    @Test
+    public void initialize() {
+        presenter.initialize();
+        if (presenter.getView() != null) {
+        }
+    }
+
+    @Test
+    public void onDataFetched() {
+        presenter.onDataFetched(serviceList);
+        if (presenter.getView() != null) {
+            Mockito.verify(view).displayLoadingState(false);
+            Mockito.verify(view).refreshVisits(serviceList);
+        }
+    }
+
+}
