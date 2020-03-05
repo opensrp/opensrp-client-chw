526782a5b6b872cf51249b843853286d293e5a74
Allan O <a.onchuru@gmail.com>
2020-03-05 14:42:32 +0300

    :boom: Override check for whether child is over 2 months for LMH implementation

--- opensrp-chw/src/lmh/java/org/smartregister/chw/activity/ChildProfileActivityFlv.java
+++ opensrp-chw/src/lmh/java/org/smartregister/chw/activity/ChildProfileActivityFlv.java
@@ -1,4 +1,10 @@
 package org.smartregister.chw.activity;
 
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+
 public class ChildProfileActivityFlv extends DefaultChildProfileActivityFlv {
+    @Override
+    public boolean isChildOverTwoMonths(CommonPersonObjectClient client) {
+        return true; // This is because LMH isn't concerned with hiding the sick child form for children < 2 months
+    }
 }
