4b218949e6e8b5565ba79ff8f3c5533263c51ab1
Josphat <josphatmuindi@gmail.com>
2020-03-10 18:00:23 +0300

    Additional instrumentation tests

--- opensrp-chw/src/androidTest/java/org/smartregister/chw/activity/wcaro/RemoveMemberTests.java
+++ opensrp-chw/src/androidTest/java/org/smartregister/chw/activity/wcaro/RemoveMemberTests.java
@@ -8,6 +8,7 @@ import androidx.annotation.StringRes;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.core.internal.deps.guava.collect.Iterables;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
 import androidx.test.runner.lifecycle.Stage;
@@ -21,10 +22,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.smartregister.chw.R;
 import org.smartregister.chw.activity.LoginActivity;
 import org.smartregister.chw.activity.utils.Constants;
 import org.smartregister.chw.activity.utils.Order;
+import org.smartregister.chw.activity.utils.OrderedRunner;
 import org.smartregister.chw.activity.utils.Utils;
 
 import static androidx.test.espresso.Espresso.onView;
@@ -37,6 +40,9 @@ import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.r
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.smartregister.chw.activity.utils.Utils.getViewId;
 
+@LargeTest
+//@RunWith(AndroidJUnit4.class)
+@RunWith(OrderedRunner.class)
 public class RemoveMemberTests {
     @Rule
     public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
@@ -93,10 +99,10 @@ public class RemoveMemberTests {
         onView(androidx.test.espresso.matcher.ViewMatchers.withSubstring("Remove existing family member"))
                 .perform(click());
         Thread.sleep(500);
-        onView(androidx.test.espresso.matcher.ViewMatchers.withSubstring(Configs.AdditionalTestData.memberOneFirstname
-                + " " + Configs.AdditionalTestData.memberOneSecondname + " "
+        onView(androidx.test.espresso.matcher.ViewMatchers.withSubstring(Configs.AdditionalTestData.memberTwoFirstname
+                + " " + Configs.AdditionalTestData.memberTwoSecondname + " "
                 + Configs.TestConfigs.familyName
-                + ", " + Configs.AdditionalTestData.extraMemberAge1))
+                + ", " + Configs.AdditionalTestData.extraMemberAge2))
                 .perform(click());
         Activity activity = getCurrentActivity();
         onView(withId(getViewId((JsonFormActivity) activity, "step1:remove_reason")))
