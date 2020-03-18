1782a6ad0021c9b392c389dafe460f826c999bd7
cozej4 <ilakozejumanne@gmail.com>
2020-03-18 18:30:50 +0300

    Implemented All clients register

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/AllClientsRegisterFragment.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/AllClientsRegisterFragment.java
@@ -1,43 +1,50 @@
 package org.smartregister.chw.fragment;
 
+import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 
 import org.smartregister.chw.core.fragment.CoreAllClientsRegisterFragment;
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.dao.FamilyDao;
+import org.smartregister.chw.model.FamilyDetailsModel;
+import org.smartregister.chw.util.AllClientsUtils;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 
 public class AllClientsRegisterFragment extends CoreAllClientsRegisterFragment {
+    public static final String REGISTER_TYPE = "register_type";
 
     @Override
     protected void goToClientDetailActivity(@NonNull CommonPersonObjectClient commonPersonObjectClient) {
 
-//        String registerType = commonPersonObjectClient.getDetails().get(HfReferralUtils.REGISTER_TYPE);
-//
-//        Bundle bundle = new Bundle();
-//        FamilyDetailsModel familyDetailsModel = FamilyDao.getFamilyDetail(commonPersonObjectClient.entityId());
-//        bundle.putString(org.smartregister.family.util.Constants.INTENT_KEY.FAMILY_BASE_ENTITY_ID, familyDetailsModel.getBaseEntityId());
-//        bundle.putString(org.smartregister.family.util.Constants.INTENT_KEY.FAMILY_HEAD, familyDetailsModel.getFamilyHead());
-//        bundle.putString(org.smartregister.family.util.Constants.INTENT_KEY.PRIMARY_CAREGIVER, familyDetailsModel.getPrimaryCareGiver());
-//        bundle.putString(org.smartregister.family.util.Constants.INTENT_KEY.FAMILY_NAME, familyDetailsModel.getFamilyName());
-//
-//        if (registerType != null) {
-//            switch (registerType) {
-//                case Constants.CHILD:
-//                    AllClientsUtils.goToChildProfile(this.getActivity(), commonPersonObjectClient, bundle);
-//                    break;
-//                case Constants.ANC:
-//                case Constants.PNC:
-//                case Constants.MALARIA:
-//                case Constants.FAMILY_PLANNING:
-//                    AllClientsUtils.goToAdultMemberProfile(this.getActivity(), commonPersonObjectClient, bundle);
-//                    break;
-//                default:
-//                    AllClientsUtils.goToOtherMemberProfile(this.getActivity(), commonPersonObjectClient, bundle,
-//                            familyDetailsModel.getFamilyHead(), familyDetailsModel.getPrimaryCareGiver());
-//                    break;
-//            }
-//        } else {
-//            AllClientsUtils.goToOtherMemberProfile(this.getActivity(), commonPersonObjectClient, bundle,
-//                    familyDetailsModel.getFamilyHead(), familyDetailsModel.getPrimaryCareGiver());
-//        }
+        String registerType = commonPersonObjectClient.getDetails().get(REGISTER_TYPE);
+
+        Bundle bundle = new Bundle();
+        FamilyDetailsModel familyDetailsModel = FamilyDao.getFamilyDetail(commonPersonObjectClient.entityId());
+        bundle.putString(org.smartregister.family.util.Constants.INTENT_KEY.FAMILY_BASE_ENTITY_ID, familyDetailsModel.getBaseEntityId());
+        bundle.putString(org.smartregister.family.util.Constants.INTENT_KEY.FAMILY_HEAD, familyDetailsModel.getFamilyHead());
+        bundle.putString(org.smartregister.family.util.Constants.INTENT_KEY.PRIMARY_CAREGIVER, familyDetailsModel.getPrimaryCareGiver());
+        bundle.putString(org.smartregister.family.util.Constants.INTENT_KEY.FAMILY_NAME, familyDetailsModel.getFamilyName());
+
+        if (registerType != null) {
+            switch (registerType) {
+                case CoreConstants.REGISTER_TYPE.CHILD:
+                    AllClientsUtils.goToChildProfile(this.getActivity(), commonPersonObjectClient, bundle);
+                    break;
+                case CoreConstants.REGISTER_TYPE.ANC:
+                case CoreConstants.REGISTER_TYPE.PNC:
+                case CoreConstants.REGISTER_TYPE.MALARIA:
+                case CoreConstants.REGISTER_TYPE.FAMILY_PLANNING:
+                    AllClientsUtils.goToAdultMemberProfile(this.getActivity(), commonPersonObjectClient, bundle);
+                    break;
+                default:
+                    AllClientsUtils.goToOtherMemberProfile(this.getActivity(), commonPersonObjectClient, bundle,
+                            familyDetailsModel.getFamilyHead(), familyDetailsModel.getPrimaryCareGiver());
+                    break;
+            }
+        } else {
+            AllClientsUtils.goToOtherMemberProfile(this.getActivity(), commonPersonObjectClient, bundle,
+                    familyDetailsModel.getFamilyHead(), familyDetailsModel.getPrimaryCareGiver());
+        }
     }
 }
