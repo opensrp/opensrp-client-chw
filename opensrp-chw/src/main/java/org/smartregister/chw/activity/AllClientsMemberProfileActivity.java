feb02dec1251647ebcdf94e4fb3ce406cce3f5fc
Elly Kitoto <junkmailstoelly@gmail.com>
2020-04-03 13:51:43 +0300

    Add all clients member profile

---
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/AllClientsMemberProfileActivity.java
@@ -0,0 +1,191 @@
+package org.smartregister.chw.activity;
+
+import android.content.Context;
+import android.view.Menu;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import org.json.JSONObject;
+import org.smartregister.chw.R;
+import org.smartregister.chw.core.activity.CoreFamilyOtherMemberProfileActivity;
+import org.smartregister.chw.core.activity.CoreFamilyProfileActivity;
+import org.smartregister.chw.core.form_data.NativeFormsDataBinder;
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.custom_view.FamilyMemberFloatingMenu;
+import org.smartregister.chw.dataloader.FamilyMemberDataLoader;
+import org.smartregister.chw.fp.util.FamilyPlanningConstants;
+import org.smartregister.chw.fragment.FamilyOtherMemberProfileFragment;
+import org.smartregister.chw.presenter.FamilyOtherMemberActivityPresenter;
+import org.smartregister.chw.util.Constants;
+import org.smartregister.chw.util.Utils;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.family.adapter.ViewPagerAdapter;
+import org.smartregister.family.fragment.BaseFamilyOtherMemberProfileFragment;
+import org.smartregister.family.model.BaseFamilyOtherMemberProfileActivityModel;
+import org.smartregister.family.util.DBConstants;
+import org.smartregister.helper.ImageRenderHelper;
+import org.smartregister.view.contract.BaseProfileContract;
+
+import timber.log.Timber;
+
+public class AllClientsMemberProfileActivity extends CoreFamilyOtherMemberProfileActivity {
+
+    private FamilyMemberFloatingMenu familyFloatingMenu;
+    private RelativeLayout layoutFamilyHasRow;
+
+    @Override
+    protected void onCreation() {
+        setContentView(R.layout.activity_all_clients_member_profile);
+
+        Toolbar toolbar = findViewById(org.smartregister.family.R.id.family_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("");
+        }
+
+        appBarLayout = findViewById(org.smartregister.family.R.id.toolbar_appbarlayout);
+
+        imageRenderHelper = new ImageRenderHelper(this);
+
+        initializePresenter();
+
+        setupViews();
+    }
+
+    @Override
+    public void setFamilyServiceStatus(String status) {
+        layoutFamilyHasRow.setVisibility(View.GONE);
+    }
+
+    @Override
+    protected void setupViews() {
+        super.setupViews();
+        TextView toolbarTitle = findViewById(R.id.toolbar_title);
+        toolbarTitle.setText(getString(R.string.return_to_all_client));
+        layoutFamilyHasRow = findViewById(R.id.family_has_row);
+        layoutFamilyHasRow.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.findItem(R.id.action_anc_registration).setVisible(false);
+        menu.findItem(R.id.action_sick_child_follow_up).setVisible(false);
+        menu.findItem(R.id.action_malaria_diagnosis).setVisible(false);
+        return true;
+    }
+
+    @Override
+    public FamilyOtherMemberActivityPresenter presenter() {
+        return (FamilyOtherMemberActivityPresenter) presenter;
+    }
+
+    @Override
+    protected void startAncRegister() {
+        AncRegisterActivity.startAncRegistrationActivity(AllClientsMemberProfileActivity.this, baseEntityId, PhoneNumber,
+                Constants.JSON_FORM.getAncRegistration(), null, familyBaseEntityId, familyName);
+    }
+
+    @Override
+    protected void startMalariaRegister() {
+        MalariaRegisterActivity.startMalariaRegistrationActivity(AllClientsMemberProfileActivity.this, baseEntityId);
+    }
+
+    @Override
+    protected void startFpRegister() {
+        String dob = org.smartregister.family.util.Utils.getValue(commonPersonObject.getColumnmaps(), DBConstants.KEY.DOB, false);
+        FpRegisterActivity.startFpRegistrationActivity(AllClientsMemberProfileActivity.this, baseEntityId, dob, CoreConstants.JSON_FORM.getFpRegistrationForm(), FamilyPlanningConstants.ActivityPayload.REGISTRATION_PAYLOAD_TYPE);
+    }
+
+    @Override
+    protected void startFpChangeMethod() {
+        String dob = org.smartregister.family.util.Utils.getValue(commonPersonObject.getColumnmaps(), DBConstants.KEY.DOB, false);
+        FpRegisterActivity.startFpRegistrationActivity(this, baseEntityId, dob, CoreConstants.JSON_FORM.getFpChengeMethodForm(), FamilyPlanningConstants.ActivityPayload.CHANGE_METHOD_PAYLOAD_TYPE);
+    }
+
+    @Override
+    protected void removeIndividualProfile() {
+        IndividualProfileRemoveActivity.startIndividualProfileActivity(AllClientsMemberProfileActivity.this,
+                commonPersonObject, familyBaseEntityId, familyHead, primaryCaregiver, FamilyRegisterActivity.class.getCanonicalName());
+    }
+
+    @Override
+    protected void startEditMemberJsonForm(Integer title_resource, CommonPersonObjectClient client) {
+        String titleString = title_resource != null ? getResources().getString(title_resource) : null;
+        boolean isPrimaryCareGiver = commonPersonObject.getCaseId().equalsIgnoreCase(primaryCaregiver);
+        String eventName = Utils.metadata().familyMemberRegister.updateEventType;
+
+        String uniqueID = commonPersonObject.getColumnmaps().get(DBConstants.KEY.UNIQUE_ID);
+
+        NativeFormsDataBinder binder = new NativeFormsDataBinder(getContext(), client.getCaseId());
+        binder.setDataLoader(new FamilyMemberDataLoader(familyName, isPrimaryCareGiver, titleString, eventName, uniqueID));
+        JSONObject jsonObject = binder.getPrePopulatedForm(Constants.ALL_CLIENT_REGISTRATION_FORM);
+
+        try {
+            if (jsonObject != null)
+                startFormActivity(jsonObject);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+    }
+
+    @Override
+    protected BaseProfileContract.Presenter getFamilyOtherMemberActivityPresenter(
+            String familyBaseEntityId, String baseEntityId, String familyHead, String primaryCaregiver, String villageTown, String familyName) {
+        return new FamilyOtherMemberActivityPresenter(this, new BaseFamilyOtherMemberProfileActivityModel(),
+                null, familyBaseEntityId, baseEntityId, familyHead, primaryCaregiver, villageTown, familyName);
+    }
+
+    @Override
+    protected FamilyMemberFloatingMenu getFamilyMemberFloatingMenu() {
+        if (familyFloatingMenu == null) {
+            familyFloatingMenu = new FamilyMemberFloatingMenu(this);
+        }
+        return familyFloatingMenu;
+    }
+
+    @Override
+    protected Context getFamilyOtherMemberProfileActivity() {
+        return AllClientsMemberProfileActivity.this;
+    }
+
+    @Override
+    protected Class<? extends CoreFamilyProfileActivity> getFamilyProfileActivity() {
+        return FamilyProfileActivity.class;
+    }
+
+    @Override
+    protected void initializePresenter() {
+        super.initializePresenter();
+    }
+
+    @Override
+    protected ViewPager setupViewPager(ViewPager viewPager) {
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        BaseFamilyOtherMemberProfileFragment profileOtherMemberFragment = FamilyOtherMemberProfileFragment.newInstance(this.getIntent().getExtras());
+        adapter.addFragment(profileOtherMemberFragment, "");
+
+        viewPager.setAdapter(adapter);
+
+        return viewPager;
+    }
+
+    @Override
+    protected BaseFamilyOtherMemberProfileFragment getFamilyOtherMemberProfileFragment() {
+        return FamilyOtherMemberProfileFragment.newInstance(getIntent().getExtras());
+    }
+
+    @Override
+    protected void startMalariaFollowUpVisit() {
+        MalariaFollowUpVisitActivity.startMalariaFollowUpActivity(this, baseEntityId);
+    }
+
+}
