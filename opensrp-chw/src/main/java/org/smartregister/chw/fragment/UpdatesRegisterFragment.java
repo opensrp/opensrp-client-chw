5da6fe5f5ce95b011ba8ee1ae1e6dff559a09011
Elly Kitoto <junkmailstoelly@gmail.com>
2020-04-09 20:31:24 +0300

    Implement updates register details page

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/UpdatesRegisterFragment.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/UpdatesRegisterFragment.java
@@ -5,8 +5,12 @@ import android.view.View;
 import org.smartregister.chw.core.fragment.BaseReferralNotificationFragment;
 import org.smartregister.chw.presenter.UpdatesFragmentPresenter;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.family.util.Utils;
 
 import java.util.HashMap;
+
+import static org.smartregister.chw.core.utils.CoreConstants.DB_CONSTANTS.NOTIFICATION_TYPE;
+import static org.smartregister.chw.core.utils.CoreConstants.DB_CONSTANTS.REFERRAL_TASK_ID;
 
 public class UpdatesRegisterFragment extends BaseReferralNotificationFragment {
 
@@ -33,11 +37,17 @@ public class UpdatesRegisterFragment extends BaseReferralNotificationFragment {
     @Override
     protected void onViewClicked(View view) {
         CommonPersonObjectClient client = (CommonPersonObjectClient) view.getTag();
-
+        String notificationType = Utils.getValue(client.getColumnmaps(), NOTIFICATION_TYPE, true);
+        String referralTaskId = Utils.getValue(client.getColumnmaps(), REFERRAL_TASK_ID, true);
+        getFragmentPresenter().displayDetailsActivity(referralTaskId, notificationType);
     }
 
     @Override
     public void showNotFoundPopup(String opensrpId) {
         // Overridden not required
+    }
+
+    private UpdatesFragmentPresenter getFragmentPresenter() {
+        return (UpdatesFragmentPresenter) presenter;
     }
 }
