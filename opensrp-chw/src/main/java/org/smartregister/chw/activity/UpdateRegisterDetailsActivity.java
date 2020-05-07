b5595c33d64af87d649dc491d3b19a93d0a7902c
Allan O <a.onchuru@gmail.com>
2020-05-07 18:57:24 +0300

    :recycle: Refactor get notification details to use baseEntityId and notificationType

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/UpdateRegisterDetailsActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/UpdateRegisterDetailsActivity.java
@@ -3,16 +3,17 @@ package org.smartregister.chw.activity;
 import android.app.Activity;
 import android.content.Intent;
 
-import org.smartregister.chw.core.activity.BaseReferralNotificationDetailsActivity;
 
+import org.smartregister.chw.core.activity.BaseChwNotificationDetailsActivity;
+
+import static org.smartregister.chw.core.utils.CoreConstants.DB_CONSTANTS.BASE_ENTITY_ID;
 import static org.smartregister.chw.core.utils.CoreConstants.DB_CONSTANTS.NOTIFICATION_TYPE;
-import static org.smartregister.chw.core.utils.CoreConstants.DB_CONSTANTS.REFERRAL_TASK_ID;
 
-public class UpdateRegisterDetailsActivity extends BaseReferralNotificationDetailsActivity {
+public class UpdateRegisterDetailsActivity extends BaseChwNotificationDetailsActivity {
 
-    public static void startActivity(Activity launcherActivity, String referralTaskId, String notificationType) {
+    public static void startActivity(Activity launcherActivity, String baseEntityId, String notificationType) {
         Intent intent = new Intent(launcherActivity, UpdateRegisterDetailsActivity.class);
-        intent.putExtra(REFERRAL_TASK_ID, referralTaskId);
+        intent.putExtra(BASE_ENTITY_ID, baseEntityId);
         intent.putExtra(NOTIFICATION_TYPE, notificationType);
         launcherActivity.startActivity(intent);
     }
