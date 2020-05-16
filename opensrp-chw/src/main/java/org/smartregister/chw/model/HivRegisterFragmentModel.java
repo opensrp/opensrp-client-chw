caf0666ee580984adca2f1acc5435bf5446b1ab0
cozej4 <ilakozejumanne@gmail.com>
2020-05-16 10:33:49 +0300

    Code refactoring

---
+++ opensrp-chw/src/main/java/org/smartregister/chw/model/HivRegisterFragmentModel.java
@@ -0,0 +1,67 @@
+package org.smartregister.chw.model;
+
+
+import androidx.annotation.NonNull;
+
+import org.jetbrains.annotations.NotNull;
+import org.smartregister.chw.core.utils.ChildDBConstants;
+import org.smartregister.chw.referral.model.BaseReferralRegisterFragmentModel;
+import org.smartregister.chw.util.ChwDBConstants;
+import org.smartregister.chw.util.Constants;
+import org.smartregister.cursoradapter.SmartRegisterQueryBuilder;
+import org.smartregister.family.util.DBConstants;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.smartregister.chw.referral.util.Constants.Tables;
+import static org.smartregister.chw.referral.util.DBConstants.Key;
+
+public class HivRegisterFragmentModel extends BaseReferralRegisterFragmentModel {
+
+    @NonNull
+    @Override
+    public String mainSelect(@NonNull String tableName, @NonNull String mainCondition) {
+        SmartRegisterQueryBuilder queryBuilder = new SmartRegisterQueryBuilder();
+        queryBuilder.SelectInitiateMainTable(tableName, mainColumns(tableName));
+        queryBuilder.customJoin("INNER JOIN " + Constants.TABLE_NAME.FAMILY_MEMBER + " ON  " + tableName + "." + Constants.ENTITY_ID + " = " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.BASE_ENTITY_ID + " COLLATE NOCASE ");
+        queryBuilder.customJoin("INNER JOIN " + Constants.TABLE_NAME.FAMILY + " ON  " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.RELATIONAL_ID + " = " + Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.BASE_ENTITY_ID);
+        queryBuilder.customJoin("INNER JOIN " + Constants.TABLE_NAME.TASK + " ON  " + tableName + ".id  = " + Constants.TABLE_NAME.TASK + "." + ChwDBConstants.TaskTable.REASON_REFERENCE);
+        queryBuilder.customJoin("LEFT JOIN " + Constants.TABLE_NAME.FAMILY_MEMBER + " as T1 ON  " + Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.PRIMARY_CAREGIVER + " = T1." + DBConstants.KEY.BASE_ENTITY_ID);
+        queryBuilder.customJoin("LEFT JOIN " + Constants.TABLE_NAME.FAMILY_MEMBER + " as T2 ON  " + Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.FAMILY_HEAD + " = T2." + DBConstants.KEY.BASE_ENTITY_ID);
+        return queryBuilder.mainCondition(mainCondition);
+    }
+
+    @Override
+    @NotNull
+    public String[] mainColumns(String tableName) {
+        Set<String> columnList = new HashSet<>();
+
+        columnList.add(tableName + "." + DBConstants.KEY.BASE_ENTITY_ID);
+        columnList.add(Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.RELATIONAL_ID + " as " + ChildDBConstants.KEY.RELATIONAL_ID);
+        columnList.add(Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.FIRST_NAME);
+        columnList.add(Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.MIDDLE_NAME);
+        columnList.add(Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.LAST_NAME);
+        columnList.add(Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.DOB);
+        columnList.add(Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.GENDER);
+        columnList.add(Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.UNIQUE_ID);
+        columnList.add(Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.RELATIONAL_ID);
+        columnList.add(Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.PHONE_NUMBER);
+        columnList.add(Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.OTHER_PHONE_NUMBER);
+        columnList.add("T2." + DBConstants.KEY.PHONE_NUMBER + " AS " + Key.FAMILY_HEAD_PHONE_NUMBER);
+        columnList.add(Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.VILLAGE_TOWN);
+        columnList.add("T1." + DBConstants.KEY.FIRST_NAME + " || " + "' '" + " || " + "T1." + DBConstants.KEY.MIDDLE_NAME + " || " + "' '" + " || " + "T1." + DBConstants.KEY.LAST_NAME + " AS " + DBConstants.KEY.PRIMARY_CAREGIVER);
+        columnList.add("T2." + DBConstants.KEY.FIRST_NAME + " || " + "' '" + " || " + "T2." + DBConstants.KEY.MIDDLE_NAME + " || " + "' '" + " || " + "T2." + DBConstants.KEY.LAST_NAME + " AS " + DBConstants.KEY.FAMILY_HEAD);
+        columnList.add(Tables.REFERRAL + "." + Key.REFERRAL_SERVICE + "  AS " + Key.REFERRAL_SERVICE);
+        columnList.add(Tables.REFERRAL + "." + Key.REFERRAL_DATE);
+        columnList.add(Constants.TABLE_NAME.TASK + "." + ChwDBConstants.TaskTable.BUSINESS_STATUS + " AS " + Key.REFERRAL_STATUS);
+        columnList.add(Tables.REFERRAL + "." + Key.PROBLEM);
+        columnList.add(Tables.REFERRAL + "." + Key.SERVICE_BEFORE_REFERRAL);
+        columnList.add(Tables.REFERRAL + "." + Key.SERVICE_BEFORE_REFERRAL_OTHER);
+        columnList.add(Tables.REFERRAL + "." + Key.PROBLEM_OTHER);
+        columnList.add(Tables.REFERRAL + "." + Key.REFERRAL_APPOINTMENT_DATE);
+        columnList.add(Tables.REFERRAL + "." + Key.REFERRAL_HF);
+        columnList.add(Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.FIRST_NAME + " as " + org.smartregister.chw.anc.util.DBConstants.KEY.FAMILY_NAME);
+        return columnList.toArray(new String[columnList.size()]);
+    }
+}
---
+++ opensrp-chw/src/main/java/org/smartregister/chw/presenter/HivRegisterFragmentPresenter.java
@@ -0,0 +1,50 @@
+package org.smartregister.chw.presenter;
+
+import org.jetbrains.annotations.NotNull;
+import org.smartregister.chw.R;
+import org.smartregister.chw.core.utils.ChwDBConstants;
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.referral.contract.BaseReferralRegisterFragmentContract;
+import org.smartregister.chw.referral.presenter.BaseReferralRegisterFragmentPresenter;
+import org.smartregister.chw.referral.util.DBConstants;
+import org.smartregister.chw.util.Constants;
+
+import static org.smartregister.chw.referral.util.Constants.ReferralType;
+import static org.smartregister.chw.referral.util.Constants.Tables;
+
+public class HivRegisterFragmentPresenter extends BaseReferralRegisterFragmentPresenter {
+
+    public HivRegisterFragmentPresenter(BaseReferralRegisterFragmentContract.View view, BaseReferralRegisterFragmentContract.Model model, String viewConfigurationIdentifier) {
+        super(view, model, viewConfigurationIdentifier);
+    }
+
+    @Override
+    @NotNull
+    public String getMainCondition() {
+        return " " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.Key.DATE_REMOVED + " is null " +
+                "AND " + Tables.REFERRAL + "." + DBConstants.Key.REFERRAL_TYPE + " = '" + ReferralType.COMMUNITY_TO_FACILITY_REFERRAL + "' ";
+
+    }
+
+    @Override
+    @NotNull
+    public String getDueFilterCondition() {
+        return " " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.Key.DATE_REMOVED + " is null " +
+                "AND " + Constants.TABLE_NAME.TASK + "." + ChwDBConstants.TaskTable.BUSINESS_STATUS + " = '" + CoreConstants.BUSINESS_STATUS.REFERRED + "' " +
+                "AND " + Tables.REFERRAL + "." + DBConstants.Key.REFERRAL_TYPE + " = '" + ReferralType.COMMUNITY_TO_FACILITY_REFERRAL + "' ";
+
+    }
+
+    @Override
+    public void processViewConfigurations() {
+        super.processViewConfigurations();
+        if (getConfig().getSearchBarText() != null && getView() != null) {
+            getView().updateSearchBarHint(getView().getContext().getString(R.string.search_name_or_id));
+        }
+    }
+
+    @Override
+    public String getMainTable() {
+        return Tables.REFERRAL;
+    }
+}
