bde65fa348108a3d764cd3cbef2f59f48704977e
Allan O <a.onchuru@gmail.com>
2020-05-17 14:07:12 +0300

    :construction: Implement get Member Profile Activities to navigate to

--- opensrp-chw/src/main/java/org/smartregister/chw/task/ChwGoToMemberProfileBasedOnRegisterTask.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/task/ChwGoToMemberProfileBasedOnRegisterTask.java
@@ -3,12 +3,18 @@ package org.smartregister.chw.task;
 import android.app.Activity;
 import android.os.Bundle;
 
+import org.smartregister.chw.activity.AboveFiveChildProfileActivity;
+import org.smartregister.chw.activity.AncMemberProfileActivity;
+import org.smartregister.chw.activity.ChildProfileActivity;
 import org.smartregister.chw.activity.FamilyPlanningMemberProfileActivity;
+import org.smartregister.chw.activity.MalariaProfileActivity;
+import org.smartregister.chw.activity.PncMemberProfileActivity;
 import org.smartregister.chw.anc.activity.BaseAncMemberProfileActivity;
 import org.smartregister.chw.core.activity.CoreAboveFiveChildProfileActivity;
 import org.smartregister.chw.core.activity.CoreChildProfileActivity;
 import org.smartregister.chw.core.task.CoreChwNotificationGoToMemberProfileTask;
 import org.smartregister.chw.fp.dao.FpDao;
+import org.smartregister.chw.malaria.activity.BaseMalariaProfileActivity;
 import org.smartregister.chw.pnc.activity.BasePncMemberProfileActivity;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 
@@ -26,26 +32,26 @@ public class ChwGoToMemberProfileBasedOnRegisterTask extends CoreChwNotification
 
     @Override
     protected Class<? extends CoreAboveFiveChildProfileActivity> getAboveFiveChildProfileActivityClass() {
-        return null;
+        return AboveFiveChildProfileActivity.class;
     }
 
     @Override
     protected Class<? extends CoreChildProfileActivity> getChildProfileActivityClass() {
-        return null;
+        return ChildProfileActivity.class;
     }
 
     @Override
     protected Class<? extends BaseAncMemberProfileActivity> getAncMemberProfileActivityClass() {
-        return null;
+        return AncMemberProfileActivity.class;
     }
 
     @Override
     protected Class<? extends BasePncMemberProfileActivity> getPncMemberProfileActivityClass() {
-        return null;
+        return PncMemberProfileActivity.class;
     }
 
     @Override
-    protected Class<?> getFamilyOtherMemberProfileActivityClass() {
-        return null;
+    protected Class<? extends BaseMalariaProfileActivity> getMalariaProfileActivityClass() {
+        return MalariaProfileActivity.class;
     }
 }
