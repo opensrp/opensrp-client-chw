c2adb72605c2644ee3145242e87237449c2b8b04
cozej4 <ilakozejumanne@gmail.com>
2020-05-19 13:24:43 +0300

    Updated hiv queries

--- opensrp-chw/src/main/java/org/smartregister/chw/model/HivRegisterFragmentModel.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/model/HivRegisterFragmentModel.java
@@ -6,16 +6,14 @@ import androidx.annotation.NonNull;
 import org.jetbrains.annotations.NotNull;
 import org.smartregister.chw.core.utils.ChildDBConstants;
 import org.smartregister.chw.hiv.model.BaseHivRegisterFragmentModel;
-import org.smartregister.chw.util.ChwDBConstants;
+import org.smartregister.chw.hiv.util.Constants.Tables;
+import org.smartregister.chw.hiv.util.DBConstants.Key;
 import org.smartregister.chw.util.Constants;
 import org.smartregister.cursoradapter.SmartRegisterQueryBuilder;
 import org.smartregister.family.util.DBConstants;
 
 import java.util.HashSet;
 import java.util.Set;
-
-import static org.smartregister.chw.referral.util.Constants.Tables;
-import static org.smartregister.chw.referral.util.DBConstants.Key;
 
 public class HivRegisterFragmentModel extends BaseHivRegisterFragmentModel {
 
@@ -24,7 +22,7 @@ public class HivRegisterFragmentModel extends BaseHivRegisterFragmentModel {
     public String mainSelect(@NonNull String tableName, @NonNull String mainCondition) {
         SmartRegisterQueryBuilder queryBuilder = new SmartRegisterQueryBuilder();
         queryBuilder.SelectInitiateMainTable(tableName, mainColumns(tableName));
-        queryBuilder.customJoin("INNER JOIN " + Constants.TABLE_NAME.FAMILY_MEMBER + " ON  " + tableName + "." + Constants.ENTITY_ID + " = " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.BASE_ENTITY_ID + " COLLATE NOCASE ");
+        queryBuilder.customJoin("INNER JOIN " + Constants.TABLE_NAME.FAMILY_MEMBER + " ON  " + tableName + "." + DBConstants.KEY.BASE_ENTITY_ID + " = " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.BASE_ENTITY_ID + " COLLATE NOCASE ");
         queryBuilder.customJoin("INNER JOIN " + Constants.TABLE_NAME.FAMILY + " ON  " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.KEY.RELATIONAL_ID + " = " + Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.BASE_ENTITY_ID);
         queryBuilder.customJoin("LEFT JOIN " + Constants.TABLE_NAME.FAMILY_MEMBER + " as T1 ON  " + Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.PRIMARY_CAREGIVER + " = T1." + DBConstants.KEY.BASE_ENTITY_ID);
         queryBuilder.customJoin("LEFT JOIN " + Constants.TABLE_NAME.FAMILY_MEMBER + " as T2 ON  " + Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.FAMILY_HEAD + " = T2." + DBConstants.KEY.BASE_ENTITY_ID);
@@ -52,6 +50,11 @@ public class HivRegisterFragmentModel extends BaseHivRegisterFragmentModel {
         columnList.add("T1." + DBConstants.KEY.FIRST_NAME + " || " + "' '" + " || " + "T1." + DBConstants.KEY.MIDDLE_NAME + " || " + "' '" + " || " + "T1." + DBConstants.KEY.LAST_NAME + " AS " + DBConstants.KEY.PRIMARY_CAREGIVER);
         columnList.add("T2." + DBConstants.KEY.FIRST_NAME + " || " + "' '" + " || " + "T2." + DBConstants.KEY.MIDDLE_NAME + " || " + "' '" + " || " + "T2." + DBConstants.KEY.LAST_NAME + " AS " + DBConstants.KEY.FAMILY_HEAD);
         columnList.add(Constants.TABLE_NAME.FAMILY + "." + DBConstants.KEY.FIRST_NAME + " as " + org.smartregister.chw.anc.util.DBConstants.KEY.FAMILY_NAME);
+        columnList.add(Tables.HIV + "." + Key.CTC_NUMBER);
+        columnList.add(Tables.HIV + "." + Key.CBHS_NUMBER);
+        columnList.add(Tables.HIV + "." + Key.CLIENT_HIV_STATUS_DURING_REGISTRATION);
+        columnList.add(Tables.HIV + "." + Key.CLIENT_HIV_STATUS_AFTER_TESTING);
+
         return columnList.toArray(new String[columnList.size()]);
     }
 }
--- opensrp-chw/src/main/java/org/smartregister/chw/presenter/HivRegisterFragmentPresenter.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/presenter/HivRegisterFragmentPresenter.java
@@ -2,17 +2,11 @@ package org.smartregister.chw.presenter;
 
 import org.jetbrains.annotations.NotNull;
 import org.smartregister.chw.R;
-import org.smartregister.chw.core.utils.ChwDBConstants;
-import org.smartregister.chw.core.utils.CoreConstants;
 import org.smartregister.chw.hiv.contract.BaseHivRegisterFragmentContract;
 import org.smartregister.chw.hiv.presenter.BaseHivRegisterFragmentPresenter;
-import org.smartregister.chw.referral.contract.BaseReferralRegisterFragmentContract;
-import org.smartregister.chw.referral.presenter.BaseReferralRegisterFragmentPresenter;
-import org.smartregister.chw.referral.util.DBConstants;
+import org.smartregister.chw.hiv.util.Constants.Tables;
+import org.smartregister.chw.hiv.util.DBConstants;
 import org.smartregister.chw.util.Constants;
-
-import static org.smartregister.chw.referral.util.Constants.ReferralType;
-import static org.smartregister.chw.referral.util.Constants.Tables;
 
 public class HivRegisterFragmentPresenter extends BaseHivRegisterFragmentPresenter {
 
@@ -23,19 +17,16 @@ public class HivRegisterFragmentPresenter extends BaseHivRegisterFragmentPresent
     @Override
     @NotNull
     public String getMainCondition() {
-        //TODO Coze update this
         return " " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.Key.DATE_REMOVED + " is null " +
-                "AND " + Tables.REFERRAL + "." + DBConstants.Key.REFERRAL_TYPE + " = '" + ReferralType.COMMUNITY_TO_FACILITY_REFERRAL + "' ";
+                "AND " + Tables.HIV + "." + DBConstants.Key.IS_CLOSED + " = '0' ";
 
     }
 
     @Override
     @NotNull
     public String getDueFilterCondition() {
-        //TODO Coze update this
         return " " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.Key.DATE_REMOVED + " is null " +
-                "AND " + Constants.TABLE_NAME.TASK + "." + ChwDBConstants.TaskTable.BUSINESS_STATUS + " = '" + CoreConstants.BUSINESS_STATUS.REFERRED + "' " +
-                "AND " + Tables.REFERRAL + "." + DBConstants.Key.REFERRAL_TYPE + " = '" + ReferralType.COMMUNITY_TO_FACILITY_REFERRAL + "' ";
+                "AND " + Tables.HIV + "." + DBConstants.Key.IS_CLOSED + " = '0' ";
 
     }
 
@@ -49,6 +40,6 @@ public class HivRegisterFragmentPresenter extends BaseHivRegisterFragmentPresent
 
     @Override
     public String getMainTable() {
-        return Tables.REFERRAL;
+        return Tables.HIV;
     }
 }
