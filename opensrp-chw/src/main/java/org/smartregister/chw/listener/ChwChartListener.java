00855a7244d77e47dce50127a882c8ad784601c1
paulinembabu <pauline.mbabu@gmail.com>
2020-06-19 20:58:23 +0300

    Removes unused imports

--- opensrp-chw/src/main/java/org/smartregister/chw/listener/ChwChartListener.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/listener/ChwChartListener.java
@@ -7,7 +7,6 @@ import android.widget.Toast;
 import org.smartregister.chw.R;
 import org.smartregister.chw.activity.FragmentBaseActivity;
 import org.smartregister.chw.application.ChwApplication;
-import org.smartregister.chw.application.ChwApplicationFlv;
 import org.smartregister.chw.fragment.MyCommunityActivityDetailsFragment;
 import org.smartregister.chw.util.Constants;
 import org.smartregister.reporting.domain.PieChartSlice;
