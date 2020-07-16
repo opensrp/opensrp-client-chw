ef3fce5c7cea890a2c4a4923e34290edff95913a
cozej4 <ilakozejumanne@gmail.com>
2020-07-16 17:48:42 +0300

    Fixed a bug in loading aboveFiveChild profile Activity

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/AboveFiveChildProfileActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/AboveFiveChildProfileActivity.java
@@ -1,6 +1,7 @@
 package org.smartregister.chw.activity;
 
 import android.view.Menu;
+import android.view.View;
 
 import org.smartregister.chw.R;
 import org.smartregister.chw.core.activity.CoreAboveFiveChildProfileActivity;
@@ -12,5 +13,21 @@ public class AboveFiveChildProfileActivity extends CoreAboveFiveChildProfileActi
         super.onCreateOptionsMenu(menu);
         menu.findItem(R.id.action_malaria_followup_visit).setVisible(false);
         return true;
+    }
+
+    @Override
+    protected void onCreation() {
+        super.onCreation();
+        initializePresenter();
+        setupViews();
+        setUpToolbar();
+        registerReceiver(mDateTimeChangedReceiver, sIntentFilter);
+        invisibleRecordVisitPanel();
+    }
+
+    private void invisibleRecordVisitPanel() {
+        if (recordVisitPanel != null) {
+            recordVisitPanel.setVisibility(View.GONE);
+        }
     }
 }
