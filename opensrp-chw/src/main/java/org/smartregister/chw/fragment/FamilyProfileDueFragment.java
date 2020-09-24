2aac3f1d74b92ca5ae70484d0cbf2b2b7eba90b3
paulinembabu <pauline.mbabu@gmail.com>
2020-09-24 18:12:12 +0300

    Converted childbaseEntityId to a local variable

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/FamilyProfileDueFragment.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/FamilyProfileDueFragment.java
@@ -39,7 +39,6 @@ public class FamilyProfileDueFragment extends BaseFamilyProfileDueFragment {
     private int dueCount = 0;
     private View emptyView;
     private String familyBaseEntityId;
-    private String childBaseEntityId;
 
     public static BaseFamilyProfileDueFragment newInstance(Bundle bundle) {
         Bundle args = bundle;
@@ -54,7 +53,7 @@ public class FamilyProfileDueFragment extends BaseFamilyProfileDueFragment {
     @Override
     protected void initializePresenter() {
         familyBaseEntityId = getArguments().getString(Constants.INTENT_KEY.FAMILY_BASE_ENTITY_ID);
-        childBaseEntityId = getArguments().getString(Constants.INTENT_KEY.BASE_ENTITY_ID);
+        String childBaseEntityId = getArguments().getString(Constants.INTENT_KEY.BASE_ENTITY_ID);
         presenter = new FamilyProfileDuePresenter(this, new FamilyProfileDueModel(), null, familyBaseEntityId, childBaseEntityId);
     }
 
