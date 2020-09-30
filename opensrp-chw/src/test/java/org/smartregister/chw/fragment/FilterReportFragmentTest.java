908009b5312c410119fbc19f870b2a68f179bb78
zzainulabidin <zain.ulabidin@venturedive.com>
2020-09-30 16:35:05 +0500

    Add unit tests

--- opensrp-chw/src/test/java/org/smartregister/chw/fragment/FilterReportFragmentTest.java
+++ opensrp-chw/src/test/java/org/smartregister/chw/fragment/FilterReportFragmentTest.java
@@ -7,12 +7,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.powermock.reflect.Whitebox;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.chw.application.ChwApplication;
+import org.smartregister.chw.contract.FindReportContract;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @RunWith(RobolectricTestRunner.class)
@@ -35,12 +37,34 @@ public class FilterReportFragmentTest {
         List<String> communityList = new ArrayList<>();
         communityList.add("CHW 1");
         communityList.add("CHW 2");
-        Whitebox.setInternalState(spyFragment, "checkedCommunities", checkedCommunities);
-        Whitebox.setInternalState(spyFragment, "communityList", communityList);
-        Whitebox.setInternalState(spyFragment, "selectedCommunitiesTV", selectedCommunitiesTV);
+        ReflectionHelpers.setField(spyFragment, "checkedCommunities", checkedCommunities);
+        ReflectionHelpers.setField(spyFragment, "communityList", communityList);
+        ReflectionHelpers.setField(spyFragment, "selectedCommunitiesTV", selectedCommunitiesTV);
 
         spyFragment.updateSelectedCommunitiesView();
 
         Mockito.verify(spyFragment).selectedCommunitiesTV.setText("CHW 1 \n CHW 2");
+    }
+
+    @Test
+    public void testRunReport() {
+        FilterReportFragment spyFragment = Mockito.spy(FilterReportFragment.class);
+        FindReportContract.Presenter presenter = Mockito.spy(FindReportContract.Presenter.class);
+        ReflectionHelpers.setField(spyFragment, "presenter", presenter);
+
+        LinkedHashMap<String, String> communityIDList = new LinkedHashMap<>();
+        communityIDList.put("CHW 1", "123");
+        communityIDList.put("CHW 2", "456");
+        boolean[] checkedCommunities = new boolean[2];
+        checkedCommunities[0] = true;
+        checkedCommunities[1] = true;
+        List<String> communityList = new ArrayList<>();
+        communityList.add("CHW 1");
+        communityList.add("CHW 2");
+        ReflectionHelpers.setField(spyFragment, "checkedCommunities", checkedCommunities);
+        ReflectionHelpers.setField(spyFragment, "communityList", communityList);
+        ReflectionHelpers.setField(spyFragment, "communityIDList", communityIDList);
+        spyFragment.runReport();
+        Mockito.verify(presenter).runReport(Mockito.anyMap());
     }
 }
