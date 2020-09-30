9b774f55a1715ae6a964114e9ef98221f485dfb1
zzainulabidin <zain.ulabidin@venturedive.com>
2020-09-30 17:10:11 +0500

    Fix FilterReportFragmentTest.java

--- opensrp-chw/src/test/java/org/smartregister/chw/fragment/FilterReportFragmentTest.java
+++ opensrp-chw/src/test/java/org/smartregister/chw/fragment/FilterReportFragmentTest.java
@@ -2,6 +2,8 @@ package org.smartregister.chw.fragment;
 
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,10 +14,16 @@ import org.robolectric.annotation.Config;
 import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.chw.application.ChwApplication;
 import org.smartregister.chw.contract.FindReportContract;
+import org.smartregister.chw.util.Constants;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(application = ChwApplication.class, sdk = 22)
@@ -53,18 +61,30 @@ public class FilterReportFragmentTest {
         ReflectionHelpers.setField(spyFragment, "presenter", presenter);
 
         LinkedHashMap<String, String> communityIDList = new LinkedHashMap<>();
-        communityIDList.put("CHW 1", "123");
-        communityIDList.put("CHW 2", "456");
+        communityIDList.put("All communities", "");
+        communityIDList.put("CHW 1", "456");
         boolean[] checkedCommunities = new boolean[2];
         checkedCommunities[0] = true;
-        checkedCommunities[1] = true;
         List<String> communityList = new ArrayList<>();
+        communityList.add("All communities");
         communityList.add("CHW 1");
-        communityList.add("CHW 2");
         ReflectionHelpers.setField(spyFragment, "checkedCommunities", checkedCommunities);
         ReflectionHelpers.setField(spyFragment, "communityList", communityList);
         ReflectionHelpers.setField(spyFragment, "communityIDList", communityIDList);
         spyFragment.runReport();
+
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy", Locale.US);
+        Calendar myCalendar = Calendar.getInstance();
+        List<String> communityIds = new ArrayList<>();
+        communityIds.add("");
+        List<String> communities = new ArrayList<>();
+        communities.add("All communities");
+        Map<String, String> map = new HashMap<>();
+        Gson gson = new Gson();
+        map.put(Constants.ReportParameters.COMMUNITY, gson.toJson(communities));
+        map.put(Constants.ReportParameters.COMMUNITY_ID, gson.toJson(communityIds));
+        map.put(Constants.ReportParameters.REPORT_DATE, dateFormat.format(myCalendar.getTime()));
         Mockito.verify(presenter).runReport(Mockito.anyMap());
     }
 }
