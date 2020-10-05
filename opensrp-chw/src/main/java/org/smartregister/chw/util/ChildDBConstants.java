a324c0f38b0c9da0a9c5ddcd17a21b35811fbb01
paulinembabu <pauline.mbabu@gmail.com>
2020-10-05 20:27:01 +0300

    Fixes lmh child register query

--- opensrp-chw/src/main/java/org/smartregister/chw/util/ChildDBConstants.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/util/ChildDBConstants.java
@@ -25,13 +25,11 @@ public class ChildDBConstants extends org.smartregister.chw.core.utils.ChildDBCo
                 " and (SELECT is_closed FROM ec_family_member WHERE base_entity_id = ec_child.mother_entity_id ) = 1)) " +
                 " and CASE WHEN ec_child.gender = 'Male' \n" +
                 " THEN (\n" +
-                " ((( julianday('now') - julianday(ec_child.dob))/365.25) < +1.8) OR ( (((( julianday('now') - julianday(ec_child.dob))/365.25) >= +1.8) AND ((( julianday('now') - julianday(ec_child.dob))/365.25) <= +2)) AND\n" +
-                "  ((SELECT count(*) FROM alerts WHERE alerts.caseID = ec_child.base_entity_id and alerts.status in ('normal','urgent', 'upcoming')) > 0))\n" +
-                "  )\n" +
-                "  WHEN ec_child.gender = 'Female' \n" +
+                " (( julianday('now') - julianday(ec_child.dob))/365.25) < 2\n" +
+                " )\n" +
+                " WHEN ec_child.gender = 'Female' \n" +
                 " THEN (\n" +
-                " ((( julianday('now') - julianday(ec_child.dob))/365.25) < +1.8) OR ( (((( julianday('now') - julianday(ec_child.dob))/365.25) >= +1.8) AND ((( julianday('now') - julianday(ec_child.dob))/365.25) <= +2)) AND\n" +
-                "  ((SELECT count(*) FROM alerts WHERE alerts.caseID = ec_child.base_entity_id and alerts.status in ('normal','urgent', 'upcoming')) > 0))  OR (((julianday('now') - julianday(ec_child.dob))/365.25) BETWEEN 9 AND 11)\n" +
+                " ((( julianday('now') - julianday(ec_child.dob))/365.25) < 2) OR (((julianday('now') - julianday(ec_child.dob))/365.25) BETWEEN 9 AND 11)\n" +
                 "  ) END ";
     }
 
