b6ece9e5bbdfad677eb77fd24c5887d765da2760
zzainulabidin <zain.ulabidin@venturedive.com>
2020-10-29 11:39:11 +0500

    Implement new delivery kit task for pregnant women

--- opensrp-chw/src/drc/java/org/smartregister/chw/interactor/AncHomeVisitInteractorFlv.java
+++ opensrp-chw/src/drc/java/org/smartregister/chw/interactor/AncHomeVisitInteractorFlv.java
@@ -1,5 +1,95 @@
 package org.smartregister.chw.interactor;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.smartregister.chw.R;
+import org.smartregister.chw.actionhelper.DeliveryKitAction;
+import org.smartregister.chw.anc.AncLibrary;
+import org.smartregister.chw.anc.contract.BaseAncHomeVisitContract;
+import org.smartregister.chw.anc.domain.MemberObject;
+import org.smartregister.chw.anc.domain.Visit;
+import org.smartregister.chw.anc.fragment.BaseAncHomeVisitFragment;
+import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
+import org.smartregister.chw.anc.util.VisitUtils;
+import org.smartregister.chw.application.ChwApplication;
+import org.smartregister.chw.core.model.VaccineTaskModel;
+import org.smartregister.chw.util.Constants;
+import org.smartregister.chw.util.ContactUtil;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import timber.log.Timber;
+
+import static org.smartregister.chw.util.Constants.ANC_HOME_VISIT.getDeliveryKitReceived;
+
 public class AncHomeVisitInteractorFlv extends DefaultAncHomeVisitInteractorFlv {
+
+    @Override
+    public LinkedHashMap<String, BaseAncHomeVisitAction> calculateActions(BaseAncHomeVisitContract.View view, MemberObject memberObject, BaseAncHomeVisitContract.InteractorCallBack callBack) throws BaseAncHomeVisitAction.ValidationException {
+        this.memberObject = memberObject;
+        this.view = view;
+        actionList = new LinkedHashMap<>();
+        context = view.getContext();
+        editMode = view.getEditMode();
+        // get the preloaded data
+        if (view.getEditMode()) {
+            Visit lastVisit = AncLibrary.getInstance().visitRepository().getLatestVisit(memberObject.getBaseEntityId(), Constants.EventType.ANC_HOME_VISIT);
+            if (lastVisit != null) {
+                details = VisitUtils.getVisitGroups(AncLibrary.getInstance().visitDetailsRepository().getVisits(lastVisit.getVisitId()));
+            }
+        }
+
+        // get contact schedule
+        Map<Integer, LocalDate> dateMap = ContactUtil.getContactSchedule(memberObject);
+
+        // get vaccine schedule if ga > 13
+        VaccineTaskModel vaccineTaskModel = null;
+
+        DateTime lastMenstrualPeriod = DateTimeFormat.forPattern("dd-MM-yyyy").parseDateTime(memberObject.getLastMenstrualPeriod());
+        int ga = Days.daysBetween(lastMenstrualPeriod, new DateTime()).getDays() / 7;
+
+        if (ga >= 13) {
+            vaccineTaskModel = getWomanVaccine(memberObject.getBaseEntityId(), lastMenstrualPeriod, getNotGivenVaccines());
+        }
+
+        try {
+            Constants.JSON_FORM.setLocaleAndAssetManager(ChwApplication.getCurrentLocale(), ChwApplication.getInstance().getApplicationContext().getAssets());
+            evaluateDangerSigns();
+            evaluateANCCounseling(dateMap);
+            evaluateSleepingUnderLLITN();
+            evaluateANCCard();
+            evaluateDeliveryKit();
+            evaluateHealthFacilityVisit(dateMap);
+            evaluateTTImmunization(vaccineTaskModel);
+            evaluateIPTP();
+            evaluateObservation();
+        } catch (BaseAncHomeVisitAction.ValidationException e) {
+            throw (e);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+
+        return actionList;
+    }
+
+
+    private void evaluateDeliveryKit() throws BaseAncHomeVisitAction.ValidationException {
+        if (memberObject.getDeliveryKit() != null && memberObject.getDeliveryKit().equalsIgnoreCase("Yes")) {
+            return;
+        }
+
+        BaseAncHomeVisitAction anc_card = new BaseAncHomeVisitAction.Builder(view.getContext(), view.getContext().getString(R.string.anc_woman_delivery_kit_received))
+                .withOptional(false)
+                .withDetails(details)
+                .withHelper(new DeliveryKitAction())
+                .withDestinationFragment(BaseAncHomeVisitFragment.getInstance(view, getDeliveryKitReceived(), null, details, null))
+                .build();
+
+        actionList.put(view.getContext().getString(R.string.anc_woman_delivery_kit_received), anc_card);
+
+    }
 }
 
