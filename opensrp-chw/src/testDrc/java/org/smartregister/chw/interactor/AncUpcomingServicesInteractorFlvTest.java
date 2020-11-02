4dad252f196da17399589fb7f6d4624373d9ccca
zzainulabidin <zain.ulabidin@venturedive.com>
2020-11-02 15:52:58 +0500

    Update AncUpcomingServicesInteractorFlvTest.java

--- opensrp-chw/src/testDrc/java/org/smartregister/chw/interactor/AncUpcomingServicesInteractorFlvTest.java
+++ opensrp-chw/src/testDrc/java/org/smartregister/chw/interactor/AncUpcomingServicesInteractorFlvTest.java
@@ -6,45 +6,24 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.util.ReflectionHelpers;
-import org.smartregister.Context;
 import org.smartregister.chw.anc.domain.MemberObject;
-import org.smartregister.chw.anc.domain.VisitDetail;
 import org.smartregister.chw.anc.model.BaseUpcomingService;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 public class AncUpcomingServicesInteractorFlvTest extends BaseHomeVisitInteractorFlvTest {
 
     private AncUpcomingServicesInteractorFlv interactor;
     @Mock
-    private Map<String, List<VisitDetail>> details;
-
-    @Mock
-    protected Context mContext;
+    private List<BaseUpcomingService> services;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
         interactor = Mockito.mock(AncUpcomingServicesInteractorFlv.class, Mockito.CALLS_REAL_METHODS);
-
-/*        Mockito.doReturn(ancHomeVisitAction).when(builder).build();
-        ReflectionHelpers.setField(interactor, "context", context);
-        ReflectionHelpers.setField(interactor, "view", view);
-        ReflectionHelpers.setField(interactor, "details", details);
-
-
-        Mockito.doReturn(ancHomeVisitAction).when(builder).build();
-        Constants.JSON_FORM.setLocaleAndAssetManager(locale, assetManager);
-        Mockito.doReturn(title).when(context).getString(Mockito.anyInt());
-
-
-        Mockito.doReturn(ancHomeVisitAction).when(builder).build();*/
     }
 
-    @Mock
-    List<BaseUpcomingService> services;
 
     @Test
     public void testEvaluateDeliveryKit() {
