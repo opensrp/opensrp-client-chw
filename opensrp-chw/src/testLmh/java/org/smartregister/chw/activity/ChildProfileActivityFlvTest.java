c8da178ab3dd8e5aaed7fa127f08aa5a50c13fb3
Allan O <a.onchuru@gmail.com>
2020-11-06 11:53:02 +0300

    :white_check_mark: Update ChildProfileActivityFlv tests

--- opensrp-chw/src/testLmh/java/org/smartregister/chw/activity/ChildProfileActivityFlvTest.java
+++ opensrp-chw/src/testLmh/java/org/smartregister/chw/activity/ChildProfileActivityFlvTest.java
@@ -1,5 +1,6 @@
 package org.smartregister.chw.activity;
 
+import org.joda.time.LocalDate;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,30 +8,50 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.smartregister.chw.anc.domain.MemberObject;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.family.util.DBConstants;
+
+import java.util.HashMap;
 
 public class ChildProfileActivityFlvTest {
 
     private ChildProfileActivity.Flavor childProfileActivityFlv;
+    private CommonPersonObjectClient commonPersonObjectClient;
+    HashMap<String, String> columnMaps = new HashMap<>();
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        childProfileActivityFlv = Mockito.mock(ChildProfileActivityFlv.class, Mockito.CALLS_REAL_METHODS);
+        childProfileActivityFlv = Mockito.mock(DefaultChildProfileActivityFlv.class, Mockito.CALLS_REAL_METHODS);
+        HashMap<String, String> clientDetails = new HashMap<>();
+        commonPersonObjectClient = new CommonPersonObjectClient(
+                "test-case-id",
+                clientDetails,
+                "tester");
     }
 
     @Test
-    public void getToolBarTitleNameReturnsFamilyName() {
+    public void getToolBarTitleNameReturnsFirstName() {
         MemberObject memberObject = new MemberObject();
-        memberObject.setFamilyName("Obafemi");
+        memberObject.setFirstName("Obafemi");
         Assert.assertEquals("Obafemi", childProfileActivityFlv.getToolbarTitleName(memberObject));
     }
 
     @Test
-    public void isChildOverTwoMonthsReturnsTrue() {
-        CommonPersonObjectClient commonPersonObjectClient = new CommonPersonObjectClient(
-                "test-case-id",
-                null,
-                "tester");
+    public void isChildOverTwoMonthsReturnsTrueWhenChildIsOver() {
+        LocalDate localDate = LocalDate.now().minusMonths(3);
+        String dateString = localDate.toString("yyyy-MM-dd");
+        columnMaps.put(DBConstants.KEY.DOB, dateString);
+        commonPersonObjectClient.setColumnmaps(columnMaps);
         Assert.assertTrue(childProfileActivityFlv.isChildOverTwoMonths(commonPersonObjectClient));
+    }
+
+
+    @Test
+    public void isChildOverTwoMonthsReturnsTWhenChildIsUnder() {
+        LocalDate localDate = LocalDate.now();
+        String dateString = localDate.toString("yyyy-MM-dd");
+        columnMaps.put(DBConstants.KEY.DOB, dateString);
+        commonPersonObjectClient.setColumnmaps(columnMaps);
+        Assert.assertFalse(childProfileActivityFlv.isChildOverTwoMonths(commonPersonObjectClient));
     }
 }
