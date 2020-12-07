8bb792b855e25c897ca72f92a1fbbe7a994ae07e
Hamza Ahmed Khan <hamza.khan@venturedive.com>
2020-12-07 18:11:20 +0500

    Fix ANC Registration edits

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/AncMemberProfileActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/AncMemberProfileActivity.java
@@ -253,6 +253,10 @@ public class AncMemberProfileActivity extends CoreAncMemberProfileActivity imple
                 NativeFormsDataBinder binder = new NativeFormsDataBinder(this, memberObject.getBaseEntityId());
                 binder.setDataLoader(new AncMemberDataLoader(titleString));
                 form = binder.getPrePopulatedForm(formName);
+                if (form != null) {
+                    form.put(JsonFormUtils.ENCOUNTER_TYPE, CoreConstants.EventType.UPDATE_ANC_REGISTRATION);
+                }
+
 
             } else if (formName.equals(CoreConstants.JSON_FORM.getFamilyMemberRegister())) {
 
