27ebbf83f09fb8c50280c08c6d5d7b3a0c8945a4
Qazi Abubakar <qma_2007@yahoo.com>
2021-02-11 18:22:12 +0500

    Fixed codacy issues

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/JobAidsGuideBooksFragment1.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/JobAidsGuideBooksFragment1.java
@@ -15,9 +15,6 @@ import org.smartregister.chw.R;
 
 public class JobAidsGuideBooksFragment1 extends Fragment {
 
-    private ConstraintLayout layoutTutorials;
-    private ConstraintLayout layoutCounseling;
-
     public static JobAidsGuideBooksFragment1 newInstance() {
         JobAidsGuideBooksFragment1 fragment = new JobAidsGuideBooksFragment1();
         Bundle args = new Bundle();
@@ -30,8 +27,8 @@ public class JobAidsGuideBooksFragment1 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_job_aids_guide_books1, container, false);
-        layoutTutorials = rootView.findViewById(R.id.layoutTutorials);
-        layoutCounseling = rootView.findViewById(R.id.layoutCounseling);
+        ConstraintLayout layoutTutorials = rootView.findViewById(R.id.layoutTutorials);
+        ConstraintLayout layoutCounseling = rootView.findViewById(R.id.layoutCounseling);
         layoutCounseling.setOnClickListener(view -> {
             Intent intent = new Intent(getActivity(), JobAidsPDFActivity.class);
             getActivity().startActivity(intent);
