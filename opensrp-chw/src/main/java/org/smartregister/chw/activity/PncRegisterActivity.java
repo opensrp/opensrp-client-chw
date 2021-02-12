2349ba12811af68912a86e97a2782caee5644967
rkodev <43806892+rkodev@users.noreply.github.com>
2021-02-12 16:02:25 +0300

    Close app on exit

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/PncRegisterActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/PncRegisterActivity.java
@@ -3,6 +3,7 @@ package org.smartregister.chw.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 
 import org.apache.commons.lang3.EnumUtils;
 import org.json.JSONArray;
@@ -25,6 +26,8 @@ import timber.log.Timber;
 
 public class PncRegisterActivity extends CorePncRegisterActivity {
 
+    private boolean closeOnCancel = false;
+
     public static void startPncRegistrationActivity(Activity activity, String memberBaseEntityID, String phoneNumber, String formName,
                                                     String uniqueId, String familyBaseID, String family_name, String last_menstrual_period) {
         Intent intent = new Intent(activity, PncRegisterActivity.class);
@@ -37,8 +40,18 @@ public class PncRegisterActivity extends CorePncRegisterActivity {
         lastMenstrualPeriod = last_menstrual_period;
         intent.putExtra(org.smartregister.chw.anc.util.Constants.ACTIVITY_PAYLOAD.ACTION, org.smartregister.chw.anc.util.Constants.ACTIVITY_PAYLOAD_TYPE.REGISTRATION);
         intent.putExtra(Constants.ACTIVITY_PAYLOAD.TABLE_NAME, getFormTable());
+        intent.putExtra("closeOnCancel", true);
         activity.startActivity(intent);
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getIntent() != null && getIntent().getExtras() != null && getIntent().getExtras().getBoolean("closeOnCancel")) {
+            closeOnCancel = true;
+        }
+    }
+
 
     @Override
     public void onRegistrationSaved(String encounterType, boolean isEdit, boolean hasChildren) {
@@ -90,6 +103,8 @@ public class PncRegisterActivity extends CorePncRegisterActivity {
             } catch (Exception e) {
                 Timber.e(e);
             }
+        } else if (resultCode == Activity.RESULT_CANCELED && closeOnCancel) {
+            this.finish();
         }
     }
 }
