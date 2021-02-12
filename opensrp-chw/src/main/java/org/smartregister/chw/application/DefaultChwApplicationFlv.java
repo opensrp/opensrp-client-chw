65e4da0f145ca8f5694b25cb0ce5191e06c9ed47
rkodev <43806892+rkodev@users.noreply.github.com>
2021-02-12 17:26:06 +0300

    Disable submit on save

--- opensrp-chw/src/main/java/org/smartregister/chw/application/DefaultChwApplicationFlv.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/application/DefaultChwApplicationFlv.java
@@ -204,7 +204,7 @@ public abstract class DefaultChwApplicationFlv implements ChwApplication.Flavor 
 
     @Override
     public boolean saveOnSubmission() {
-        return true;
+        return false;
     }
 
     @Override
