689feee93c6ee626b9f02aea36b77e0bec67ca42
Qazi Abubakar <qma_2007@yahoo.com>
2021-02-24 16:46:05 +0500

    Removed deprecated assert call

--- opensrp-chw/src/testDrc/java/org/smartregister/chw/util/ChildUtilsTest.java
+++ opensrp-chw/src/testDrc/java/org/smartregister/chw/util/ChildUtilsTest.java
@@ -65,7 +65,7 @@ public class ChildUtilsTest extends BaseUnitTest {
     public void testGetOneYearVaccines() {
         String[] expected = new String[]{"bcg", "opv1", "penta1", "pcv1", "rota1", "opv2", "penta2", "pcv2", "rota2", "opv3", "penta3", "pcv3", "ipv", "mcv1",
                 "yellowfever", "rota3", "mena", "rubella1"};
-        Assert.assertEquals(expected, childUtilsFlv.getOneYearVaccines());
+        Assert.assertArrayEquals(expected, childUtilsFlv.getOneYearVaccines());
     }
 
     @Test
