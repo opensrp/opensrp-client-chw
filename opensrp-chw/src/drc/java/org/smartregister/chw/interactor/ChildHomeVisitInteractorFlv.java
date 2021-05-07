4c43975fcccf1d095f9322d72fa47b0b1aed60a8
bitbucket <62241922+zzainulabidin@users.noreply.github.com>
2021-05-07 15:32:56 +0500

    Update child home visit

--- opensrp-chw/src/drc/java/org/smartregister/chw/interactor/ChildHomeVisitInteractorFlv.java
+++ opensrp-chw/src/drc/java/org/smartregister/chw/interactor/ChildHomeVisitInteractorFlv.java
@@ -10,6 +10,7 @@ import org.smartregister.chw.anc.fragment.BaseAncHomeVisitFragment;
 import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
 import org.smartregister.chw.anc.util.JsonFormUtils;
 import org.smartregister.chw.util.Constants;
+import org.smartregister.immunization.domain.ServiceWrapper;
 
 import java.util.List;
 import java.util.Map;
@@ -17,6 +18,24 @@ import java.util.Map;
 import timber.log.Timber;
 
 public class ChildHomeVisitInteractorFlv extends DefaultChildHomeVisitInteractorFlv {
+
+    @Override
+    protected void bindEvents(Map<String, ServiceWrapper> serviceWrapperMap) throws BaseAncHomeVisitAction.ValidationException {
+        try {
+            evaluateChildVaccineCard();
+            evaluateImmunization();
+            evaluateMNP(serviceWrapperMap);
+            evaluateVitaminA(serviceWrapperMap);
+            evaluateDeworming(serviceWrapperMap);
+            evaluateBirthCertForm();
+        } catch (BaseAncHomeVisitAction.ValidationException e) {
+            throw (e);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+    }
+
+
 
     protected void evaluateDietary() throws Exception {
         int age = getAgeInMonths();
