b089b804fdd7ec476b87813892c758c2295d201b
bitbucket <62241922+zzainulabidin@users.noreply.github.com>
2021-05-21 19:16:59 +0500

    add support for disability; fix pregnancy outcome translation issue

--- opensrp-chw/src/nigeria/java/org/smartregister/chw/interactor/ChildHomeVisitInteractorFlv.java
+++ opensrp-chw/src/nigeria/java/org/smartregister/chw/interactor/ChildHomeVisitInteractorFlv.java
@@ -1,8 +1,13 @@
 package org.smartregister.chw.interactor;
 
+import org.smartregister.chw.R;
+import org.smartregister.chw.actionhelper.DisabilityAction;
+import org.smartregister.chw.anc.domain.VisitDetail;
 import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
+import org.smartregister.chw.util.Constants;
 import org.smartregister.immunization.domain.ServiceWrapper;
 
+import java.util.List;
 import java.util.Map;
 
 import timber.log.Timber;
@@ -18,11 +23,28 @@ public class ChildHomeVisitInteractorFlv extends DefaultChildHomeVisitInteractor
             evaluateVitaminA(serviceWrapperMap);
             evaluateDeworming(serviceWrapperMap);
             evaluateBirthCertForm();
+            evaluateDisability();
         } catch (BaseAncHomeVisitAction.ValidationException e) {
             throw (e);
         } catch (Exception e) {
             Timber.e(e);
         }
+    }
+
+    private void evaluateDisability() throws Exception {
+
+        Map<String, List<VisitDetail>> details = getDetails(Constants.EventType.DISABILITY);
+
+        BaseAncHomeVisitAction action = getBuilder(context.getString(R.string.disability))
+                .withOptional(false)
+                .withDetails(details)
+                .withBaseEntityID(memberObject.getBaseEntityId())
+                .withProcessingMode(BaseAncHomeVisitAction.ProcessingMode.SEPARATE)
+                .withHelper(new DisabilityAction())
+                .withFormName(Constants.JSON_FORM.getDisability())
+                .build();
+
+        actionList.put(context.getString(R.string.disability), action);
     }
 
 }
