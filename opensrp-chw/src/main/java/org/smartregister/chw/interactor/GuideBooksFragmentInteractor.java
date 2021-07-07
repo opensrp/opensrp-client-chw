59621d8be1fe5cc325defa8c86ee3f4ba6e2ac03
bitbucket <62241922+zzainulabidin@users.noreply.github.com>
2021-07-07 19:09:07 +0500

    Add Missing Hyphens Separators in Job Aids Counselling

--- opensrp-chw/src/main/java/org/smartregister/chw/interactor/GuideBooksFragmentInteractor.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/interactor/GuideBooksFragmentInteractor.java
@@ -1,7 +1,6 @@
 package org.smartregister.chw.interactor;
 
 import android.content.Context;
-import android.os.Environment;
 
 import androidx.annotation.VisibleForTesting;
 
@@ -10,7 +9,6 @@ import com.google.gson.reflect.TypeToken;
 
 import org.apache.commons.lang3.StringUtils;
 import org.smartregister.chw.BuildConfig;
-import org.smartregister.chw.application.ChwApplication;
 import org.smartregister.chw.contract.GuideBooksFragmentContract;
 import org.smartregister.chw.domain.GuideBooksFragmentVideo;
 import org.smartregister.chw.util.DownloadUtil;
@@ -21,6 +19,8 @@ import java.io.File;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import timber.log.Timber;
 
@@ -124,7 +124,19 @@ public class GuideBooksFragmentInteractor implements GuideBooksFragmentContract.
     }
 
     private String toTitle(String s) {
-        return StringUtils.join(s.split("\\.")[0].split("_"), " ");
+        String smallCaps = StringUtils.join(s.split("\\.")[0].split("_"), " ");
+        return addHyphens(smallCaps);
+//        return smallCaps;
+    }
+
+    private String addHyphens(String str) {
+        Pattern compile = Pattern.compile("(?<=\\d) (?=\\d)");
+        Matcher matcher = compile.matcher(str);
+        boolean isFound = matcher.find();
+        if (isFound) {
+            int index = matcher.start();
+            return str.substring(0, index) + "-" + str.substring(index + 1);
+        } else return str;
     }
 
     private static class ServerFile {
