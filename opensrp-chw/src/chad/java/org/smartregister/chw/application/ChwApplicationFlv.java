774e33dc23eb9b0566fb82f07b0752fbf836cb21
bitbucket <62241922+zzainulabidin@users.noreply.github.com>
2021-09-20 17:55:56 +0500

    Disable ThinkMD health assessment

--- opensrp-chw/src/chad/java/org/smartregister/chw/application/ChwApplicationFlv.java
+++ opensrp-chw/src/chad/java/org/smartregister/chw/application/ChwApplicationFlv.java
@@ -3,6 +3,6 @@ package org.smartregister.chw.application;
 public class ChwApplicationFlv extends DefaultChwApplicationFlv {
     @Override
     public boolean useThinkMd() {
-        return true;
+        return false;
     }
 }
