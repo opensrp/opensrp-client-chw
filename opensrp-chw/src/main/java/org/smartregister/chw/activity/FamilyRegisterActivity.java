3d2ec9349910668e71be30a67ba0fc85e17bff65
paulinembabu <pauline.mbabu@gmail.com>
2021-09-30 12:47:50 +0300

    Show Progress Dialog on Sync

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/FamilyRegisterActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/FamilyRegisterActivity.java
@@ -1,11 +1,14 @@
 package org.smartregister.chw.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.smartregister.chw.R;
 import org.smartregister.chw.application.ChwApplication;
 import org.smartregister.chw.core.activity.CoreFamilyRegisterActivity;
 import org.smartregister.chw.core.custom_views.NavigationMenu;
@@ -13,10 +16,15 @@ import org.smartregister.chw.fragment.FamilyRegisterFragment;
 import org.smartregister.chw.listener.ChwBottomNavigationListener;
 import org.smartregister.chw.util.Constants;
 import org.smartregister.chw.util.Utils;
+import org.smartregister.domain.FetchStatus;
 import org.smartregister.helper.BottomNavigationHelper;
+import org.smartregister.receiver.SyncStatusBroadcastReceiver;
 import org.smartregister.view.fragment.BaseRegisterFragment;
 
-public class FamilyRegisterActivity extends CoreFamilyRegisterActivity {
+import timber.log.Timber;
+
+public class FamilyRegisterActivity extends CoreFamilyRegisterActivity implements SyncStatusBroadcastReceiver.SyncStatusListener {
+    private ProgressDialog progressDialog;
 
     public static void startFamilyRegisterForm(Activity activity) {
         Intent intent = new Intent(activity, FamilyRegisterActivity.class);
@@ -46,10 +54,65 @@ public class FamilyRegisterActivity extends CoreFamilyRegisterActivity {
         if (action != null && action.equals(Constants.ACTION.START_REGISTRATION)) {
             startRegistration();
         }
+        try {
+            SyncStatusBroadcastReceiver.getInstance().addSyncStatusListener(this);
+        }
+        catch (Exception e){
+            Timber.e(e);
+        }
     }
 
     @Override
     protected BaseRegisterFragment getRegisterFragment() {
         return new FamilyRegisterFragment();
+    }
+
+    private void initializeProgressDialog() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(true);
+        progressDialog.setTitle(getString(R.string.syncing_records));
+    }
+
+    private void showProgressDialog() {
+        try {
+            if (progressDialog == null) {
+                initializeProgressDialog();
+            }
+
+            progressDialog.show();
+        } catch (Exception e) {
+            Timber.e(Log.getStackTraceString(e));
+        }
+    }
+
+    public void hideProgressDialog() {
+        try {
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+            }
+        } catch (Exception e) {
+            Timber.e(Log.getStackTraceString(e));
+        }
+    }
+
+    @Override
+    public void onSyncStart() {
+        showProgressDialog();
+    }
+
+    @Override
+    public void onSyncInProgress(FetchStatus fetchStatus) {
+        showProgressDialog();
+    }
+
+    @Override
+    public void onSyncComplete(FetchStatus fetchStatus) {
+        hideProgressDialog();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SyncStatusBroadcastReceiver.getInstance().removeSyncStatusListener(this);
     }
 }
