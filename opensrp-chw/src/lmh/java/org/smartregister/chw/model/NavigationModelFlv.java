7acf282e9527757e36538139c540a0a2a97290fd
LZRS <12814349+LZRS@users.noreply.github.com>
2021-10-05 10:19:24 +0300

    change icons for reports and 'add new family'

--- opensrp-chw/src/lmh/java/org/smartregister/chw/model/NavigationModelFlv.java
+++ opensrp-chw/src/lmh/java/org/smartregister/chw/model/NavigationModelFlv.java
@@ -17,9 +17,9 @@ public class NavigationModelFlv extends DefaultNavigationModelFlv {
 
         if (navigationOptions.size() == 0) {
             navigationOptions.add(new NavigationOption(R.mipmap.sidemenu_families, R.mipmap.sidemenu_families, R.string.menu_all_families, Constants.DrawerMenu.ALL_FAMILIES, 0));
-            navigationOptions.add(new NavigationOption(R.drawable.ic_input_add, R.drawable.ic_input_add, R.string.menu_add_new_family, "Add New Family", -1));
+            navigationOptions.add(new NavigationOption(R.drawable.ic_add_new_family_white, R.drawable.ic_add_new_family_white, R.string.menu_add_new_family, "Add New Family", -1));
             navigationOptions.add(new NavigationOption(R.mipmap.sidemenu_children, R.mipmap.sidemenu_children, R.string.menu_child_clients, Constants.DrawerMenu.CHILD_CLIENTS, 0));
-            navigationOptions.add(new NavigationOption(R.mipmap.ic_jobaids, R.mipmap.ic_jobaids, R.string.reports, "Reports", -1));
+            navigationOptions.add(new NavigationOption(R.drawable.ic_reports, R.drawable.ic_reports, R.string.reports, "Reports", -1));
         }
 
         return navigationOptions;
