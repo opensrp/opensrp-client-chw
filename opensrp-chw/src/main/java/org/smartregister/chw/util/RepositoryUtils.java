5987677321548c5401245558f7aa51a1fafbeeae
Allan O <a.onchuru@gmail.com>
2021-10-07 21:42:53 +0300

    :recycle: Refactor RepositoryUtils interface
    Converted it to a class

--- opensrp-chw/src/main/java/org/smartregister/chw/util/RepositoryUtils.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/util/RepositoryUtils.java
@@ -22,32 +22,32 @@ import java.util.List;
 
 import timber.log.Timber;
 
-public interface RepositoryUtils {
+public class RepositoryUtils {
 
-    String EVENT_ID = "id";
-    String _ID = "_id";
+    public static final String EVENT_ID = "id";
+    public static final String _ID = "_id";
 
-    String ADD_MISSING_REPORTING_COLUMN = "ALTER TABLE 'indicator_queries' ADD COLUMN expected_indicators TEXT NULL;";
-    String FAMILY_MEMBER_ADD_REASON_FOR_REGISTRATION = "ALTER TABLE 'ec_family_member' ADD COLUMN reasons_for_registration TEXT NULL;";
-    String EC_REFERRAL_ADD_FP_METHOD_COLUMN = "ALTER TABLE 'ec_referral' ADD COLUMN fp_method_accepted_referral TEXT NULL;";
+    public static String ADD_MISSING_REPORTING_COLUMN = "ALTER TABLE 'indicator_queries' ADD COLUMN expected_indicators TEXT NULL;";
+    public static String FAMILY_MEMBER_ADD_REASON_FOR_REGISTRATION = "ALTER TABLE 'ec_family_member' ADD COLUMN reasons_for_registration TEXT NULL;";
+    public static String EC_REFERRAL_ADD_FP_METHOD_COLUMN = "ALTER TABLE 'ec_referral' ADD COLUMN fp_method_accepted_referral TEXT NULL;";
 
-    String[] UPDATE_REPOSITORY_TYPES = {
+    public static String[] UPDATE_REPOSITORY_TYPES = {
             "UPDATE recurring_service_types SET service_group = 'woman' WHERE type = 'IPTp-SP';",
             "UPDATE recurring_service_types SET service_group = 'child' WHERE type != 'IPTp-SP';",
     };
 
-    String[] UPGRADE_V10 = {
+    public static String[] UPGRADE_V10 = {
             "ALTER TABLE ec_child ADD COLUMN mother_entity_id VARCHAR;",
             "ALTER TABLE ec_child ADD COLUMN entry_point VARCHAR;"
     };
 
-    String DELETE_DUPLICATE_SCHEDULES = "delete from schedule_service where id not in ( " +
+    public static String DELETE_DUPLICATE_SCHEDULES = "delete from schedule_service where id not in ( " +
             "select max(id) from schedule_service " +
             "group by base_entity_id , schedule_group_name , schedule_name " +
             "having count(*) > 1 " +
             ")";
 
-    static void addDetailsColumnToFamilySearchTable(SQLiteDatabase db) {
+    public static void addDetailsColumnToFamilySearchTable(SQLiteDatabase db) {
         try {
 
             db.execSQL("ALTER TABLE ec_family ADD COLUMN entity_type VARCHAR; " +
@@ -63,7 +63,7 @@ public interface RepositoryUtils {
         }
     }
 
-    static void updateNullEventIds(SQLiteDatabase db) {
+    public static void updateNullEventIds(SQLiteDatabase db) {
         List<Event> events = new ArrayList<>();
         String eventTableName = EventClientRepository.Table.event.name();
         String eventIdCol = EventClientRepository.event_column.eventId.name();
