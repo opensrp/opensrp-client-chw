fa6c0d598334013183ab36a9a2f32365666bb7b6
LZRS <12814349+LZRS@users.noreply.github.com>
2021-11-10 13:29:49 +0300

    LMH1-116: rename reports menu title to Dashboard

--- opensrp-chw/src/lmh/java/org/smartregister/chw/model/NavigationModelFlv.java
+++ opensrp-chw/src/lmh/java/org/smartregister/chw/model/NavigationModelFlv.java
@@ -19,7 +19,7 @@ public class NavigationModelFlv extends DefaultNavigationModelFlv {
             navigationOptions.add(new NavigationOption(R.mipmap.sidemenu_families, R.mipmap.sidemenu_families, R.string.menu_all_families, Constants.DrawerMenu.ALL_FAMILIES, 0));
             navigationOptions.add(new NavigationOption(R.drawable.ic_add_new_family_white, R.drawable.ic_add_new_family_white, R.string.menu_add_new_family, Constants.DrawerMenu.ADD_NEW_FAMILY, -1));
             navigationOptions.add(new NavigationOption(R.mipmap.sidemenu_children, R.mipmap.sidemenu_children, R.string.menu_child_clients, Constants.DrawerMenu.CHILD_CLIENTS, 0));
-            navigationOptions.add(new NavigationOption(R.drawable.ic_reports, R.drawable.ic_reports, R.string.reports, Constants.DrawerMenu.REPORTS, -1));
+            navigationOptions.add(new NavigationOption(R.drawable.ic_reports, R.drawable.ic_reports, R.string.reports_dashboard, Constants.DrawerMenu.REPORTS, -1));
         }
 
         return navigationOptions;
