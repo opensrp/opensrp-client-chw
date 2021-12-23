768a68b0776a6fd7a756923a14bfa2948059eb68
LZRS <12814349+LZRS@users.noreply.github.com>
2021-12-23 17:22:29 +0300

    LMH1-142: Vaccine dates not saving correctly

--- opensrp-chw/src/lmh/java/org/smartregister/chw/fragment/BaseHomeVisitImmunizationFragmentFlv.java
+++ opensrp-chw/src/lmh/java/org/smartregister/chw/fragment/BaseHomeVisitImmunizationFragmentFlv.java
@@ -15,6 +15,7 @@ import org.smartregister.chw.anc.util.JsonFormUtils;
 import org.smartregister.chw.anc.util.NCUtils;
 import org.smartregister.util.DatePickerUtils;
 
+import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 
@@ -31,6 +32,16 @@ public class BaseHomeVisitImmunizationFragmentFlv extends DefaultBaseHomeVisitIm
         fragment.details = details;
         fragment.vaccinesDefaultChecked = defaultChecked;
         for (VaccineDisplay vaccineDisplay : vaccineDisplays) {
+            String name = NCUtils.removeSpaces(vaccineDisplay.getVaccineWrapper().getName());
+            if (details != null && details.containsKey(name)) {
+                String value = NCUtils.getText(details.get(name));
+
+                try {
+                    vaccineDisplay.setDateGiven(dateFormat.parse(value));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
             fragment.vaccineDisplays.put(vaccineDisplay.getVaccineWrapper().getName(), vaccineDisplay);
         }
 
