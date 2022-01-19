2d679ad9d0dadd702acf50eec85f98e44ff1d958
Zameer Khan <58561265+zk497@users.noreply.github.com>
2022-01-19 15:24:49 +0300

    Removed unused imports

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/HivCommunityFollowupDetailsActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/HivCommunityFollowupDetailsActivity.java
@@ -1,13 +1,9 @@
 package org.smartregister.chw.activity;
 
-import com.vijay.jsonwizard.utils.FormUtils;
-
 import org.json.JSONException;
 import org.smartregister.chw.hiv.activity.BaseHivCommunityFollowupDetailsActivity;
 
 import java.util.Objects;
-
-import timber.log.Timber;
 
 public class HivCommunityFollowupDetailsActivity extends BaseHivCommunityFollowupDetailsActivity {
     @Override
