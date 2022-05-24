9a1355a5eb68f8d5a56d576a17176c2613bb1590
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2022-05-24 13:23:53 +0300

    :hammer: added ChwHivOutcome Dao for querying HIV outcome details

--- opensrp-chw/src/main/java/org/smartregister/chw/dao/ChwHivOutcomeDao.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/dao/ChwHivOutcomeDao.java
@@ -1,35 +1,44 @@
 package org.smartregister.chw.dao;
 
-import org.joda.time.DateTime;
-import org.joda.time.Days;
-import org.smartregister.chw.application.ChwApplication;
-import org.smartregister.chw.core.dao.ChildDao;
-import org.smartregister.chw.core.domain.Child;
 import org.smartregister.dao.AbstractDao;
-import org.smartregister.domain.Alert;
-import org.smartregister.domain.AlertStatus;
-import org.smartregister.immunization.domain.Vaccine;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-public class ChwHivDao extends AbstractDao {
+public class ChwHivOutcomeDao extends AbstractDao {
 
-    public static boolean hasDueVaccines(String baseEntityID) {
-        String sql = "select * FROM  count \n" +
-                "FROM alerts  " +
-                "where caseID = '" + baseEntityID + "'";
+    public static String servicesProvided(String baseEntityID, long timestamp) {
+        String sql = "SELECT action_taken, test_results\n" +
+                "FROM ec_hiv_outcome\n" +
+                "WHERE entity_id = '" + baseEntityID + "'\n" +
+                "AND date(visit_date) = date(substr(strftime('%Y-%m-%d', datetime(" + timestamp + " / 1000, 'unixepoch', 'localtime')), 1, 4) ||\n" +
+                "                                 '-' ||\n" +
+                "                                 substr(strftime('%Y-%m-%d', datetime(" + timestamp + "/ 1000, 'unixepoch', 'localtime')), 6, 2) ||\n" +
+                "                                 '-' ||  substr(strftime('%Y-%m-%d', datetime(" + timestamp + "/ 1000, 'unixepoch', 'localtime')), 9, 2))\n" +
+                "ORDER BY visit_date DESC\n" +
+                "LIMIT 1;";
 
-        DataMap<Integer> dataMap = cursor -> getCursorIntValue(cursor, "count");
+        DataMap<String> dataMap = cursor -> getCursorValue(cursor, "action_taken");
+        List<String> res = readData(sql, dataMap);
+        if (res != null && res.size() > 0 && res.get(0) != null)
+            return res.get(0);
+        return null;
+    }
 
-        List<Integer> res = readData(sql, dataMap);
-        if (res == null || res.get(0) == 0)
-            return false;
+    public static String hivStatus(String baseEntityID, long timestamp) {
+        String sql = "SELECT test_results\n" +
+                "FROM ec_hiv_outcome\n" +
+                "WHERE entity_id = '" + baseEntityID + "'\n" +
+                "AND date(visit_date) = date(substr(strftime('%Y-%m-%d', datetime(" + timestamp + " / 1000, 'unixepoch', 'localtime')), 1, 4) ||\n" +
+                "                                 '-' ||\n" +
+                "                                 substr(strftime('%Y-%m-%d', datetime(" + timestamp + "/ 1000, 'unixepoch', 'localtime')), 6, 2) ||\n" +
+                "                                 '-' ||  substr(strftime('%Y-%m-%d', datetime(" + timestamp + "/ 1000, 'unixepoch', 'localtime')), 9, 2))\n" +
+                "ORDER BY visit_date DESC\n" +
+                "LIMIT 1;";
 
-        res.size();
-        return true;
+        DataMap<String> dataMap = cursor -> getCursorValue(cursor, "test_results");
+        List<String> res = readData(sql, dataMap);
+        if (res != null && res.size() > 0 && res.get(0) != null)
+            return res.get(0);
+        return null;
     }
 }
