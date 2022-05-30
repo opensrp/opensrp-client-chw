40eb92a989f0ae49fddf58a9106a41624158a55e
Billy <billypatrick767@gmail.com>
2022-05-30 15:16:52 +0300

    add: query for next visit

--- opensrp-chw/src/main/java/org/smartregister/chw/dao/ChwCBHSDao.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/dao/ChwCBHSDao.java
@@ -2,7 +2,15 @@ package org.smartregister.chw.dao;
 
 import org.smartregister.dao.AbstractDao;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
+
+import timber.log.Timber;
 
 public class ChwCBHSDao extends AbstractDao {
     public static boolean tbStatusAfterTestingDone(String baseEntityID) {
@@ -14,5 +22,29 @@ public class ChwCBHSDao extends AbstractDao {
         if (res != null && res.size() > 0 && res.get(0) != null)
             return !res.get(0).equalsIgnoreCase("unknown");
         return false;
+    }
+
+    public static Date getNextVisitDate(String baseEntityId){
+        String sql = "Select next_appointment_date from ec_cbhs_register where base_entity_id = '" + baseEntityId + "'";
+
+        DataMap<String> dataMap = cursor -> getCursorValue(cursor, "next_appointment_date");
+        List<String> res = readData(sql, dataMap);
+
+        if (res != null && res.size() > 0 && res.get(0) != null){
+            Calendar cal = Calendar.getInstance();
+            try{
+                cal.setTimeInMillis(new BigDecimal(res.get(0)).longValue());
+            }catch (Exception e){
+                //NEEDED FOR THE ISSUE IN SOME TABLETS FAILING TO CREATE A TIMESTAMP
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                try {
+                    cal.setTime(sdf.parse(res.get(0)));
+                } catch (ParseException parseException) {
+                    Timber.e(parseException);
+                }
+            }
+            return new Date(cal.getTimeInMillis());
+        }
+        return null;
     }
 }
