db3edc07858e1087bfdf34e8d81a2c2366d34633
Billy <billypatrick767@gmail.com>
2022-06-01 18:13:48 +0300

    updated: layout for referral with feedback views

--- opensrp-chw/src/main/java/org/smartregister/chw/dao/ChwHivOutcomeDao.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/dao/ChwHivOutcomeDao.java
@@ -41,4 +41,59 @@ public class ChwHivOutcomeDao extends AbstractDao {
             return res.get(0);
         return null;
     }
+
+    public static String hivEnrolledToCTC(String forEntity, long millis) {
+        String sql = "SELECT enrolled_to_clinic\n" +
+                "FROM ec_hiv_outcome\n" +
+                "WHERE entity_id = '" + forEntity + "'\n" +
+                "AND date(visit_date) = date(substr(strftime('%Y-%m-%d', datetime(" + millis + " / 1000, 'unixepoch', 'localtime')), 1, 4) ||\n" +
+                "                                 '-' ||\n" +
+                "                                 substr(strftime('%Y-%m-%d', datetime(" + millis + "/ 1000, 'unixepoch', 'localtime')), 6, 2) ||\n" +
+                "                                 '-' ||  substr(strftime('%Y-%m-%d', datetime(" + millis + "/ 1000, 'unixepoch', 'localtime')), 9, 2))\n" +
+                "ORDER BY visit_date DESC\n" +
+                "LIMIT 1;";
+        DataMap<String> dataMap = cursor -> getCursorValue(cursor, "enrolled_to_clinic");
+        List<String> res = readData(sql, dataMap);
+        if (res != null && res.size() > 0 && res.get(0) != null)
+            return res.get(0);
+        return null;
+    }
+
+    public static String hivCommentsFromHF(String forEntity, long millis) {
+        String sql = "SELECT comment\n" +
+                "FROM ec_hiv_outcome\n" +
+                "WHERE entity_id = '" + forEntity + "'\n" +
+                "AND date(visit_date) = date(substr(strftime('%Y-%m-%d', datetime(" + millis + " / 1000, 'unixepoch', 'localtime')), 1, 4) ||\n" +
+                "                                 '-' ||\n" +
+                "                                 substr(strftime('%Y-%m-%d', datetime(" + millis + "/ 1000, 'unixepoch', 'localtime')), 6, 2) ||\n" +
+                "                                 '-' ||  substr(strftime('%Y-%m-%d', datetime(" + millis + "/ 1000, 'unixepoch', 'localtime')), 9, 2))\n" +
+                "ORDER BY visit_date DESC\n" +
+                "LIMIT 1;";
+        DataMap<String> dataMap = cursor -> getCursorValue(cursor, "comment");
+        List<String> res = readData(sql, dataMap);
+        if (res != null && res.size() > 0 && res.get(0) != null)
+            return res.get(0);
+        return null;
+    }
+
+    public static String ctcNumber(String forEntity, long millis) {
+        String sql = "SELECT ctc_number\n" +
+                "FROM ec_hiv_outcome\n" +
+                "WHERE entity_id = '" + forEntity + "'\n" +
+                "AND date(visit_date) = date(substr(strftime('%Y-%m-%d', datetime(" + millis + " / 1000, 'unixepoch', 'localtime')), 1, 4) ||\n" +
+                "                                 '-' ||\n" +
+                "                                 substr(strftime('%Y-%m-%d', datetime(" + millis + "/ 1000, 'unixepoch', 'localtime')), 6, 2) ||\n" +
+                "                                 '-' ||  substr(strftime('%Y-%m-%d', datetime(" + millis + "/ 1000, 'unixepoch', 'localtime')), 9, 2))\n" +
+                "ORDER BY visit_date DESC\n" +
+                "LIMIT 1;";
+        DataMap<String> dataMap = cursor -> getCursorValue(cursor, "ctc_number");
+        List<String> res = readData(sql, dataMap);
+        if (res != null && res.size() > 0 && res.get(0) != null)
+            return res.get(0);
+        return null;
+    }
+
+    public static String reasonsForNotEnrolling(String forEntity, long millis) {
+        return null;
+    }
 }
