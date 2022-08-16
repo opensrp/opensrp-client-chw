7f97d1a2f234c3ea597f58598cca0540d799af10
Billy <billypatrick767@gmail.com>
2022-08-16 11:33:35 +0300

    fix: linking to fragment

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/HivstRegisterActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/HivstRegisterActivity.java
@@ -1,6 +1,12 @@
 package org.smartregister.chw.activity;
 
 import org.smartregister.chw.core.activity.CoreHivstRegisterActivity;
+import org.smartregister.chw.core.fragment.CoreHivstRegisterFragment;
+import org.smartregister.view.fragment.BaseRegisterFragment;
 
 public class HivstRegisterActivity extends CoreHivstRegisterActivity {
+    @Override
+    protected BaseRegisterFragment getRegisterFragment() {
+        return new CoreHivstRegisterFragment();
+    }
 }
