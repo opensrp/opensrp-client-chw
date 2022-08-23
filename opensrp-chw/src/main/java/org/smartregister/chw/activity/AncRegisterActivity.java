f8f86affc3cc3e3fa650ef29f310c1cbd23b971e
Billy <billypatrick767@gmail.com>
2022-08-23 14:05:33 +0300

    fix: update the anc registration form to ask for medical_history

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/AncRegisterActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/AncRegisterActivity.java
@@ -10,20 +10,31 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.vijay.jsonwizard.constants.JsonFormConstants;
+import com.vijay.jsonwizard.domain.Form;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.smartregister.chw.R;
 import org.smartregister.chw.anc.util.Constants;
+import org.smartregister.chw.anc.util.DBConstants;
 import org.smartregister.chw.core.activity.CoreAncRegisterActivity;
 import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.core.utils.CoreJsonFormUtils;
+import org.smartregister.chw.core.utils.FormUtils;
 import org.smartregister.chw.fragment.AncPartnerFollowupRegisterFragment;
 import org.smartregister.chw.fragment.AncRegisterFragment;
 import org.smartregister.chw.schedulers.ChwScheduleTaskExecutor;
+import org.smartregister.family.util.JsonFormUtils;
+import org.smartregister.family.util.Utils;
 import org.smartregister.helper.BottomNavigationHelper;
 import org.smartregister.job.SyncServiceJob;
 import org.smartregister.view.fragment.BaseRegisterFragment;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import timber.log.Timber;
 
@@ -64,6 +75,45 @@ public class AncRegisterActivity extends CoreAncRegisterActivity implements Bott
     @Override
     protected BaseRegisterFragment getRegisterFragment() {
         return new AncRegisterFragment();
+    }
+
+    @Override
+    public void startFormActivity(JSONObject jsonForm) {
+
+        try {
+            JSONObject stepOne = jsonForm.getJSONObject(JsonFormUtils.STEP1);
+            JSONArray jsonArray = stepOne.getJSONArray(JsonFormUtils.FIELDS);
+
+            Map<String, String> values = new HashMap<>();
+
+            values.put(DBConstants.KEY.TEMP_UNIQUE_ID, unique_id);
+            values.put(CoreConstants.JsonAssets.FAM_NAME, familyName);
+            values.put(CoreConstants.JsonAssets.FAMILY_MEMBER.PHONE_NUMBER, phone_number);
+            values.put(org.smartregister.family.util.DBConstants.KEY.RELATIONAL_ID, familyBaseEntityId);
+            values.put(DBConstants.KEY.LAST_MENSTRUAL_PERIOD, lastMenstrualPeriod);
+            try {
+                JSONObject min_date = CoreJsonFormUtils.getFieldJSONObject(jsonArray, "delivery_date");
+                min_date.put("min_date", lastMenstrualPeriod);
+            }catch (Exception e){
+                Timber.e(e);
+            }
+
+            FormUtils.updateFormField(jsonArray, values);
+
+            Intent intent = new Intent(this, Utils.metadata().familyMemberFormActivity);
+            intent.putExtra(org.smartregister.family.util.Constants.JSON_FORM_EXTRA.JSON, jsonForm.toString());
+
+            Form form = new Form();
+            form.setActionBarBackground(org.smartregister.chw.core.R.color.family_actionbar);
+            form.setNavigationBackground(org.smartregister.chw.core.R.color.family_navigation);
+            form.setName(getString(R.string.anc_registration));
+            form.setWizard(true);
+            intent.putExtra(JsonFormConstants.JSON_FORM_KEY.FORM, form);
+
+            startActivityForResult(intent, JsonFormUtils.REQUEST_CODE_GET_JSON);
+        } catch (JSONException e) {
+            Timber.e(e);
+        }
     }
 
     @Override
