123ae851f0a6da2ffd54931940499b76415e074d
Billy <billypatrick767@gmail.com>
2022-08-26 17:09:20 +0300

    fix: loading outlet forms

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/CdpRegisterActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/CdpRegisterActivity.java
@@ -1,8 +1,11 @@
 package org.smartregister.chw.activity;
 
+import org.json.JSONObject;
+import org.smartregister.chw.cdp.util.Constants;
 import org.smartregister.chw.core.activity.CoreCdpRegisterActivity;
 import org.smartregister.chw.core.fragment.CoreCdpRegisterFragment;
 import org.smartregister.chw.core.fragment.CoreOrdersRegisterFragment;
+import org.smartregister.chw.core.utils.FormUtils;
 import org.smartregister.view.fragment.BaseRegisterFragment;
 
 import androidx.fragment.app.Fragment;
@@ -19,5 +22,11 @@ public class CdpRegisterActivity extends CoreCdpRegisterActivity {
         return new Fragment[]{
                 new CoreOrdersRegisterFragment()
         };
+    }
+
+    @Override
+    public void startOutletForm() {
+        JSONObject form = FormUtils.getFormUtils().getFormJson(Constants.FORMS.CDP_OUTLET_REGISTRATION);
+        startFormActivity(form);
     }
 }
