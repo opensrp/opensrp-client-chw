18f19e8be2814483fe87f496e7d517eb9244f731
Billy <billypatrick767@gmail.com>
2022-09-12 15:27:35 +0300

    fix: loading mark as received

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/OrderDetailsActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/OrderDetailsActivity.java
@@ -3,15 +3,23 @@ package org.smartregister.chw.activity;
 import android.app.Activity;
 import android.content.Intent;
 
+import org.json.JSONObject;
 import org.smartregister.chw.cdp.activity.BaseOrderDetailsActivity;
 import org.smartregister.chw.cdp.util.Constants;
+import org.smartregister.chw.core.utils.FormUtils;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.family.util.JsonFormUtils;
 
 public class OrderDetailsActivity extends BaseOrderDetailsActivity {
 
-    public static void startMe(Activity activity, CommonPersonObjectClient pc){
+    public static void startMe(Activity activity, CommonPersonObjectClient pc) {
         Intent intent = new Intent(activity, OrderDetailsActivity.class);
         intent.putExtra(Constants.ACTIVITY_PAYLOAD.CLIENT, pc);
         activity.startActivity(intent);
+    }
+
+    @Override
+    public void startFormActivity(JSONObject jsonForm) {
+        startActivityForResult(FormUtils.getStartFormActivity(jsonForm, null, this), JsonFormUtils.REQUEST_CODE_GET_JSON);
     }
 }
