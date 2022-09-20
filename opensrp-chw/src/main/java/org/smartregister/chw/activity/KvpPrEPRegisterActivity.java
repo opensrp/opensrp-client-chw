110e1a5521654d1a44f0b840f28752d8e0f0e55b
Billy <billypatrick767@gmail.com>
2022-09-20 16:20:45 +0300

    fix: loading kvp register activity

---
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/KvpPrEPRegisterActivity.java
@@ -0,0 +1,7 @@
+package org.smartregister.chw.activity;
+
+import org.smartregister.chw.core.activity.CoreKvpRegisterActivity;
+
+public class KvpPrEPRegisterActivity extends CoreKvpRegisterActivity {
+
+}
