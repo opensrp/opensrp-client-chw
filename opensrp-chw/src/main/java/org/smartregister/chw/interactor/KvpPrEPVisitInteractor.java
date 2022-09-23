f503ff0786d619da08ec2b9c929a31232d6f5ece
Billy <billypatrick767@gmail.com>
2022-09-23 18:26:22 +0300

    fix: moved repeatitive implementation to lib

--- opensrp-chw/src/main/java/org/smartregister/chw/interactor/KvpPrEPVisitInteractor.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/interactor/KvpPrEPVisitInteractor.java
@@ -1,54 +1,25 @@
 package org.smartregister.chw.interactor;
 
-import android.content.Context;
-
 import org.smartregister.chw.R;
 import org.smartregister.chw.actionhelper.KvpPrEPPreventiveServicesActionHelper;
 import org.smartregister.chw.actionhelper.KvpPrEPReferralServicesActionHelper;
 import org.smartregister.chw.actionhelper.KvpPrEPSbccServicesActionHelper;
 import org.smartregister.chw.actionhelper.KvpPrEPStructuralServicesActionHelper;
 import org.smartregister.chw.actionhelper.KvpPrEPVisitTypeActionHelper;
-import org.smartregister.chw.kvp.KvpLibrary;
 import org.smartregister.chw.kvp.contract.BaseKvpVisitContract;
-import org.smartregister.chw.kvp.domain.MemberObject;
-import org.smartregister.chw.kvp.domain.Visit;
 import org.smartregister.chw.kvp.domain.VisitDetail;
 import org.smartregister.chw.kvp.interactor.BaseKvpVisitInteractor;
 import org.smartregister.chw.kvp.model.BaseKvpVisitAction;
 import org.smartregister.chw.kvp.util.Constants;
-import org.smartregister.chw.kvp.util.VisitUtils;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import timber.log.Timber;
 
 public class KvpPrEPVisitInteractor extends BaseKvpVisitInteractor {
-    protected final LinkedHashMap<String, BaseKvpVisitAction> actionList = new LinkedHashMap<>();
-    protected Context context;
-    protected Map<String, List<VisitDetail>> details = null;
-
     @Override
-    public void calculateActions(BaseKvpVisitContract.View view, MemberObject memberObject, BaseKvpVisitContract.InteractorCallBack callBack) {
-
-        context = view.getContext();
-        getDetailsOnEdit(view, memberObject);
-
-        populateActionList(callBack);
-    }
-
-    private void getDetailsOnEdit(BaseKvpVisitContract.View view, MemberObject memberObject) {
-        if (view.getEditMode()) {
-            Visit lastVisit = KvpLibrary.getInstance().visitRepository().getLatestVisit(memberObject.getBaseEntityId(), Constants.EVENT_TYPE.KVP_PrEP_FOLLOW_UP_VISIT);
-
-            if (lastVisit != null) {
-                details = VisitUtils.getVisitGroups(KvpLibrary.getInstance().visitDetailsRepository().getVisits(lastVisit.getVisitId()));
-            }
-        }
-    }
-
-    private void populateActionList(BaseKvpVisitContract.InteractorCallBack callBack) {
+    protected void populateActionList(BaseKvpVisitContract.InteractorCallBack callBack) {
         final Runnable runnable = () -> {
             try {
                 evaluateVisitType(details);
@@ -64,10 +35,6 @@ public class KvpPrEPVisitInteractor extends BaseKvpVisitInteractor {
         };
 
         appExecutors.diskIO().execute(runnable);
-    }
-
-    public BaseKvpVisitAction.Builder getBuilder(String title) {
-        return new BaseKvpVisitAction.Builder(context, title);
     }
 
     private void evaluateVisitType(Map<String, List<VisitDetail>> details) throws BaseKvpVisitAction.ValidationException {
