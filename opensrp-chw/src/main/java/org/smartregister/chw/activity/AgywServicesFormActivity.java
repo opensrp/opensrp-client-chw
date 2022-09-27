56b5352e7819d60f5e991eb1882ae0bf49cc3a5e
Billy <billypatrick767@gmail.com>
2022-09-27 17:08:30 +0300

    added: method to start forms activity

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/AgywServicesFormActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/AgywServicesFormActivity.java
@@ -1,7 +1,18 @@
 package org.smartregister.chw.activity;
 
+import android.app.Activity;
+import android.content.Intent;
+
+import org.smartregister.chw.agyw.util.Constants;
 import org.smartregister.chw.core.activity.CoreAgywServicesFormsActivity;
 
 public class AgywServicesFormActivity extends CoreAgywServicesFormsActivity {
+    public static void startMe(Activity activity, String formName, String baseEntityId, int age) {
+        Intent intent = new Intent(activity, AgywServicesFormActivity.class);
+        intent.putExtra(Constants.ACTIVITY_PAYLOAD.AGYW_FORM_NAME, formName);
+        intent.putExtra(Constants.ACTIVITY_PAYLOAD.BASE_ENTITY_ID, baseEntityId);
+        intent.putExtra(Constants.ACTIVITY_PAYLOAD.AGE, age);
 
+        activity.startActivity(intent);
+    }
 }
