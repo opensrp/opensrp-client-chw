23019f4c15961d3c58c1dce0f282f587c9f98f7a
vend <junaid.warsi@venturedive.com>
2023-02-16 17:39:38 +0500

    added the missing code from the merged PR 1855 issue ref#296

--- opensrp-chw/src/main/java/org/smartregister/chw/sync/ChwSyncIntentService.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/sync/ChwSyncIntentService.java
@@ -1,8 +1,22 @@
 package org.smartregister.chw.sync;
 
+import org.smartregister.chw.dao.EventDao;
 import org.smartregister.sync.intent.SyncIntentService;
 
 public class ChwSyncIntentService extends SyncIntentService {
+
+    @Override
+    protected void handleSync() {
+        // fetch the last downloaded serverVersion before any unsyced data
+        Long serverVersion = EventDao.getMinimumVerifiedServerVersion();
+        if (serverVersion != null)
+            org.smartregister.util.Utils.getAllSharedPreferences().saveLastSyncDate(serverVersion);
+
+        // flag all contentious events as unsynced
+        EventDao.markEventsForReUpload();
+        super.handleSync();
+    }
+
 
     @Override
     public int getEventPullLimit() {
@@ -12,6 +26,6 @@ public class ChwSyncIntentService extends SyncIntentService {
 
     @Override
     protected Integer getEventBatchSize(){
-        return 50;
+        return 250;
     } // Should this be configurable?
 }
